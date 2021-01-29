68318effb5f548bd88f71f1f817bbd727ae47e1e
TJ Ward <jward3@email.unc.edu>
2021-01-28 21:13:07 -0500

    Add GCEP DefinitionStepManager

---
+++ app/Domain/Application/Service/Steps/GcepDefinitionStepManager.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Domain\Application\Service\Steps;
+
+use App\Domain\Application\Models\Application;
+
+class GcepDefinitionStepManager extends AbstractStepManager
+{
+    public function __construct(private Application $application)
+    {
+    }
+
+    public function getUnmetRequirements():array
+    {
+        return [];
+    }
+
+    public function meetsAllRequirements():bool
+    {
+        return true;
+    }
+
+    public function isCurrentStep():bool
+    {
+        return $this->application->current_step == 1 
+            && $this->application->ep_type_id == config('expert_panels.types.gcep.id');
+    }
+
+    public function isLastStep():bool
+    {
+        return true;
+    }
+
+    // public function getDocuments():Collection;
+    // public function getLogEntries():Collection;
+    
+}
