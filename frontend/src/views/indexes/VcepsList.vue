9f9cd7cbbb6c402793a63d46b40d582197ab1c86
TJ Ward <jward3@email.unc.edu>
2021-02-08 09:23:06 -0500

    set fields on VcepsList

--- frontend/src/views/indexes/VcepsList.vue
+++ frontend/src/views/indexes/VcepsList.vue
@@ -1,5 +1,9 @@
 <template>
     <div>
+        <div class="mb-1">
+            <label>Filter: <input type="text" class="sm" v-model="filter" placeholder="filter"></label>
+        </div>
+        <data-table :data="data" :fields="fields" :filter-term="filter"></data-table>
         <application-list></application-list>
     </div>
 </template>
@@ -15,11 +19,48 @@ export default {
     },
     data() {
         return {
-            
+            fields: [
+                {
+                    name: 'id',
+                    label: 'ID',
+                    type: Number,
+                    sortable: true
+                },
+                {
+                    name: 'name',
+                    label: 'Name',
+                    type: String,
+                    sortable: true
+                },
+                {
+                    name: 'email',
+                    label: 'email',
+                    type: String,
+                    sortable: true
+                },
+                {
+                    name: 'birthday',
+                    label: 'DOB',
+                    type: Date,
+                    sortable: false
+                }
+            ],
+            data: [],
         }
     },
     computed: {
+        filter: {
+            set(value) {
+                let currentQuery = this.$route.query;
+                let currentPath = this.$route.path;
 
+                this.$router.replace({path: currentPath, query: {...currentQuery, ...{filter: value}}})
+            },
+            get() {
+                return this.$route.query.filter
+            },
+            immediate: true
+        }
     },
     methods: {
 
