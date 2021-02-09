52261368e045a583ba10ccf1560d3f61847a2c56
TJ Ward <jward3@email.unc.edu>
2021-02-09 11:19:35 -0500

    Add ApplicationsTableSeeder

---
+++ database/seeders/ApplicationsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Domain\Application\Jobs\InitiateApplication;
+use App\Domain\Application\Models\Application;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class ApplicationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach(Application::factory(2)->make() as $app) {
+            $job = new InitiateApplication($app->uuid, $app->working_name, $app->cdwg_id, $app->ep_type_id, Carbon::now());
+            \Bus::dispatch($job);
+        }
+    }
+}
