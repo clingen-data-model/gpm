dc69ad2ee55e5564fba456dec30cf461c17d5b06
TJ Ward <jward3@email.unc.edu>
2021-02-09 11:23:39 -0500

    Fetch and display VCEPs in VCEP index

--- frontend/src/views/indexes/VcepsList.vue
+++ frontend/src/views/indexes/VcepsList.vue
@@ -3,16 +3,18 @@
         <div class="mb-1">
             <label>Filter: <input type="text" class="sm" v-model="filter" placeholder="filter"></label>
         </div>
-        <data-table :data="data" :fields="fields" :filter-term="filter"></data-table>
-        <application-list></application-list>
+        <data-table :data="data" :fields="fields" :filter-term="filter">
+            <template v-slot:latest_activity_log="props">
+                <pre>{{props.value.description}}</pre>
+            </template>
+        </data-table>
     </div>
 </template>
 <script>
-import ApplicationList from '../../components/applications/ApplicationList'
+import { all as getAllApplications } from '../../adapters/application_repository'
 
 export default {
     components: {
-        ApplicationList
     },
     props: {
         
@@ -33,16 +35,28 @@ export default {
                     sortable: true
                 },
                 {
-                    name: 'email',
-                    label: 'email',
+                    name: 'cdwg.name',
+                    label: 'CDWG',
                     type: String,
                     sortable: true
                 },
                 {
-                    name: 'birthday',
-                    label: 'DOB',
+                    name: 'current_step',
+                    label: 'Current Step',
+                    type: Number,
+                    sortable: true
+                },
+                {
+                    name: 'latest_log_entry.description',
+                    label: 'Last Activity',
                     type: Date,
+                },
+                {
+                    name: 'latest_pending_next_action.entry',
+                    label: 'Next Action',
+                    type: String,
                     sortable: false
+
                 }
             ],
             data: [],
@@ -63,7 +77,20 @@ export default {
         }
     },
     methods: {
-
+        async getApplications () {
+            this.data = await getAllApplications({
+                with: [
+                    'latestLogEntry',
+                    'latestPendingNextAction'
+                ],
+                where: {
+                    ep_type_id: 2
+                }
+            })
+        }
+    },
+    mounted () {
+        this.getApplications()
     }
 }
 </script>
