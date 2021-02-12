00fd341b732785b36316ebe1a60dfdeaf15443c9
TJ Ward <jward3@email.unc.edu>
2021-02-12 16:41:30 -0500

    Add progress chart component

---
+++ frontend/src/components/applications/ProgressChart.vue
@@ -0,0 +1,39 @@
+<style lang="postcss" scoped>
+    .progress-date {
+        @apply text-xs;
+        width: 10em;
+    }
+</style>
+<template>
+    <div>
+        <h4 class="pb-2">EP Progress</h4>
+        <div class="flex flex-row space-x-1">
+            <progress-step v-for="i in steps" :key="i" :step="i" :application="application"></progress-step>
+        </div>
+    </div>
+</template>
+<script>
+import { formatDate } from '../../date_utils'
+import ProgressStep from './ProgressStep'
+
+export default {
+    components: {
+        ProgressStep
+    },
+    props: {
+        application: {
+            required: true,
+            type: Object
+        }
+    },
+    computed: {
+        steps () {
+            if (this.application.ep_type_id == 2) {
+                return [1,2,3,4]
+            }
+
+            return [1]
+        },
+    }
+}
+</script>
