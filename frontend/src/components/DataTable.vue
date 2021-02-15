e49d50bdbb8b37195def37e45be1ec21a9879815
TJ Ward <jward3@email.unc.edu>
2021-02-15 15:45:09 -0500

    Add sort prop support to DataTable; Improve sort for strings.

--- frontend/src/components/DataTable.vue
+++ frontend/src/components/DataTable.vue
@@ -19,7 +19,10 @@
                     <th v-for="field in fields.filter(f => !f.hideHeader)" :key="field.name"
                         class="text-left border border-gray-300 px-3"
                         :title="field.sortable ? `Click to sort` : ``"
-                        :class="{'cursor-pointer underline hover:bg-gray-300': field.sortable, 'sorted': sort.field == field}"
+                        :class="{
+                            'cursor-pointer underline hover:bg-gray-300': field.sortable, 
+                            'sorted': sortClone.field == field
+                        }"
                         @click="field.sortable && updateSort(field)"
                         :colspan="(field.colspan ? field.colspan : 1)"
                     >
@@ -32,13 +35,13 @@
                             <div>
                                 <div v-if="field.sortable">
                                     <icon-cheveron-up icon-color="#ccc" 
-                                        v-if="sort.field != field"
+                                        v-if="sortClone.field != field"
                                     ></icon-cheveron-up>
                                     <icon-cheveron-up icon-color="#333" 
-                                        v-if="sort.field == field && !sort.desc"
+                                        v-if="sortClone.field == field && !sortClone.desc"
                                     ></icon-cheveron-up>
                                     <icon-cheveron-down icon-color="#333" 
-                                        v-if="sort.field == field && sort.desc"
+                                        v-if="sortClone.field == field && sortClone.desc"
                                     ></icon-cheveron-down>
                                 </div>
                             </div>
@@ -98,13 +101,33 @@ export default {
             required: false,
             type: [String,null],
             default: null
+        },
+        sort: {
+            required: false,
+            type: Object
         }
     },
     data() {
         return {
-            sort: {
-                field: this.fields[0],
+            sortClone: {
+                field: {},
                 desc: false
+            }
+        }
+    },
+    watch: {
+        sort: {
+            immediate: true,
+            handler: function() {
+                if (!this.sort) {
+                    this.sortClone = {
+                        field: this.fields[0],
+                        desc: false
+                    }
+                    return;
+                }
+
+                this.sortClone = {...this.sort}
             }
         }
     },
@@ -122,15 +145,12 @@ export default {
         sortedFilteredData() {
             if (this.data) {
                 let data = this.filteredData;//JSON.parse(JSON.stringify(this.data))
-                const sortType = this.sort.field.type;
+                const sortType = this.sortClone.field.type;
                 switch (sortType) {
-                    case String:
-                    case Number:
-                        return data.sort(this.textAndNumbeSort)
                     case Date:
                         return data.sort(this.dateSort)
                     default:
-                        console.error('unsupported column type: '+sortType);
+                        return data.sort(this.textAndNumberSort)
                         break;
                 }                
             } else {
@@ -160,29 +180,35 @@ export default {
             return item[attr]
         },
         updateSort(field) {
-            console.log(field);
-            const oldField = this.sort.field;
-            this.sort.field = field
-            this.sort.desc = !this.sort.desc
+            const oldField = this.sortClone.field;
+            const newSort = {
+                field: field,
+                desc: !this.sortClone.desc
+            }
             
             if (oldField != field) {
-                this.sort.desc = false
+                newSort.desc = false
             }
-            this.$emit('sorted', this.sort);
+            this.$emit('update:sort', newSort)
+            this.$emit('sorted', newSort);
         },
-        textAndNumbeSort(a, b) {
-            const coefficient = this.sort.desc ? -1 : 1;
-            const aVal = this.resolveAttribute(a, this.sort.field);
-            const bVal = this.resolveAttribute(b, this.sort.field);
+        textAndNumberSort(a, b) {
+            const coefficient = this.sortClone.desc ? -1 : 1;
+            let aVal = this.resolveAttribute(a, this.sortClone.field);
+            let bVal = this.resolveAttribute(b, this.sortClone.field);
+            if (this.sortClone.field.type == String) {
+                aVal = aVal.toLowerCase();
+                bVal = bVal.toLowerCase();
+            }
             if (aVal == bVal) {
                 return 0;
             }
             return coefficient*((aVal > bVal) ? 1 : -1);
         },
         dateSort(a, b) {
-            const coefficient = this.sort.desc ? -1 : 1;
-            const aVal = new Date(a[this.sort.field]);
-            const bVal = new Date(b[this.sort.field]);
+            const coefficient = this.sortClone.desc ? -1 : 1;
+            const aVal = new Date(a[this.sortClone.field]);
+            const bVal = new Date(b[this.sortClone.field]);
             if (aVal == bVal) {
                 console.log('dates equal')
                 return 0;
