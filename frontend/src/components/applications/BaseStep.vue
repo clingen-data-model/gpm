e21cc076484b3d0b76295867f4ff4c80d048b832
TJ Ward <jward3@email.unc.edu>
2021-02-22 16:40:13 -0500

    Customize step view when step is completed/approved

--- frontend/src/components/applications/BaseStep.vue
+++ frontend/src/components/applications/BaseStep.vue
@@ -1,45 +1,67 @@
 <template>
     <div>
         <slot name="document">
-            <div>
+            <div class="mb-6">
                 <h3 class="text-lg font-bold mb-1">{{documentName}}</h3>
-                <document-manager 
+                <document-manager
                     :application="application"
                     :document-type-id="documentType"
                     :getsReviewd="documentGetsReviewed"
                     :step="step"
+                    v-if="!application.stepIsApproved(step)"        
                 ></document-manager>
+                
+                <div v-else
+                    class="pb-3 border-b"
+                >
+                    <dictionary-row label="Final Document">
+                        <a href="" class="text-blue-500 underline">{{application.finalDocumentOfType(documentType).filename}}</a>
+                    </dictionary-row>
+                    <dictionary-row label="Date Received">
+                        {{firstDateReceived}}
+                    </dictionary-row>
+                    <dictionary-row label="Date Reviewed">
+                        {{finalDateReviewed}}
+                    </dictionary-row>
+                    <dictionary-row label="Date Approved">
+                        {{dateApproved}}
+                    </dictionary-row>
+                </div>
+
             </div>
         </slot>
 
-        <hr class="my-6 text">
-
-        <button 
-            class="btn btn-lg w-full" 
-            @click="startApproveStep"
-            :disabled="!isCurrentStep"
-            :title="isCurrentStep ? 'Approve this step' : 'You can only approve the application\'s current step'"
+        <!-- Approve step -->
+        <div  
+            class="border border-l-0 border-r-0 py-4 mb-6" 
+            v-if="!application.stepIsApproved(step)"
         >
-            {{approveButtonLabel}}
-        </button>
-        <modal-dialog v-model="showApproveForm">
-            <approve-step-form @saved="hideApproveForm" @canceled="hideApproveForm"></approve-step-form>
-        </modal-dialog>
-
-        <hr class="my-6 text">
+            <button 
+                class="btn btn-lg w-full" 
+                @click="startApproveStep"
+                :disabled="!isCurrentStep"
+                :title="isCurrentStep ? 'Approve this step' : 'You can only approve the application\'s current step'"
+            >
+                {{approveButtonLabel}}
+            </button>
+            <modal-dialog v-model="showApproveForm">
+                <approve-step-form @saved="hideApproveForm" @canceled="hideApproveForm"></approve-step-form>
+            </modal-dialog>
+        </div>
 
         <slot></slot>
 
         <slot name="log">
-        <div class="mb-6">
-            <h4 class="text-md font-bold mb-2">Step {{step}} Progress Log</h4>
-            <application-log :step="step"></application-log>
-        </div>
+            <div class="mb-6">
+                <h4 class="text-md font-bold mb-2">Step {{step}} Progress Log</h4>
+                <application-log :step="step"></application-log>
+            </div>
         </slot>
     </div>
 </template>
 <script>
 import { mapGetters } from 'vuex'
+import { formatDate } from '../../date_utils'
 import ApplicationLog from './ApplicationLog'
 import DocumentManager from './DocumentManager'
 import ApproveStepForm from './ApproveStepForm'
@@ -88,6 +110,15 @@ export default {
         }),
         isCurrentStep () {
             return this.step == this.application.current_step
+        },
+        finalDateReviewed () {
+            return formatDate(this.application.finalDocumentOfType(this.documentType).date_reviewed)
+        },
+        firstDateReceived () {
+            return formatDate(this.application.firstDocumentOfType(this.documentType).date_received)
+        },
+        dateApproved () {
+            return formatDate(this.application.approvalDateForStep(this.step))
         }
     },
     methods: {
--- frontend/src/components/applications/StepThree.vue
+++ frontend/src/components/applications/StepThree.vue
@@ -4,22 +4,53 @@
         approve-button-label="Approve Pilot and Specifications"
     >
         <template v-slot:document>
-            <div class="mb-4">
-                <h3 class="text-lg font-bold mb-1">Final Specifications</h3>
-                <document-manager 
-                    :application="application"
-                    :document-type-id="4"
-                    :getsReviewd="false"
-                ></document-manager>
+            <h3 class="text-lg font-bold mb-1">Final Specifications</h3>
+            <div v-if="!application.stepIsApproved(3)">
+                <div class="mb-4">
+                    <document-manager 
+                        :application="application"
+                        :document-type-id="4"
+                        :getsReviewd="false"
+                    ></document-manager>
+                </div>
+                <div>
+                    <h3 class="text-lg font-bold mb-1">Pilot Classifications</h3>
+                    <document-manager 
+                        :application="application"
+                        :document-type-id="5"
+                        :getsReviewd="false"
+                    ></document-manager>
+                </div>
             </div>
-            <div>
-                <h3 class="text-lg font-bold mb-1">Pilot Classifications</h3>
-                <document-manager 
-                    :application="application"
-                    :document-type-id="5"
-                    :getsReviewd="false"
-                ></document-manager>
+            <div v-else
+                class="pb-3 border-b mb-6"
+            >
+                <dictionary-row label="Final Specifications">
+                    <a href="" class="text-blue-500 underline">{{application.finalDocumentOfType(3).filename}}</a>
+                </dictionary-row>
+                <dictionary-row label="Date Received">
+                    {{formatDate(application.firstDocumentOfType(3).date_received)}}
+                </dictionary-row>
+                <dictionary-row label="Date Reviewed">
+                    {{formatDate(application.finalDocumentOfType(3).date_reviewed)}}
+                </dictionary-row>
+
+                <br>
+                <dictionary-row label="Final Pilot Classifications">
+                    <a href="" class="text-blue-500 underline">{{application.finalDocumentOfType(4).filename}}</a>
+                </dictionary-row>
+                <dictionary-row label="Date Received">
+                    {{formatDate(application.firstDocumentOfType(4).date_received)}}
+                </dictionary-row>
+                <dictionary-row label="Date Reviewed">
+                    {{formatDate(application.finalDocumentOfType(4).date_reviewed)}}
+                </dictionary-row>
+                <br>
+                <dictionary-row label="Date Approved">
+                    {{formatDate(application.approvalDateForStep(3))}}
+                </dictionary-row>
             </div>
+
         </template>
 
         
@@ -27,6 +58,7 @@
 </template>
 <script>
 import {mapGetters} from 'vuex'
+import {formatDate} from '../../date_utils'
 import BaseStep from './BaseStep'
 import DocumentManager from './DocumentManager'
 
@@ -50,7 +82,9 @@ export default {
             })
     },
     methods: {
-
+        formatDate(dateString) {
+            return formatDate(dateString)
+        }
     }
 }
 </script>
