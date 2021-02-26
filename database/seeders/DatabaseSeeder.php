cf7d10fab98cd56b4e383a0ca63d4699e29cb040
TJ Ward <jward3@email.unc.edu>
2021-02-26 17:39:17 -0500

    Add User seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $seederClasses = [
+            UsersTableSeeder::class,
             EpTypesTableSeeder::class,
             DocumentCategoriesTableSeeder::class,
         ];
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'tj ward',
+                'email' => 'jward3@email.unc.edu',
+                'password' => Hash::make('tester')
+            ]
+        ];
+
+        foreach ($users as $userData ) {
+            User::create($userData);
+        }
+
+    }
+}
