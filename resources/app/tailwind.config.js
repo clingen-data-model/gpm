98a55de9092e161a423e69f7d80b4c2e72002edd
TJ Ward <jward3@email.unc.edu>
2021-03-04 10:11:19 -0500

    Add minwidth options to tailwind config.

--- resources/app/tailwind.config.js
+++ resources/app/tailwind.config.js
@@ -15,11 +15,21 @@ module.exports = {
             '2/3': '66%',
             '3/4': '75%',
             'full': '100%'
+        },
+        minWidth: {
+            '4': '1rem',
+            '8': '2rem',
+            '16': '4rem',
+            '24': '6rem',
+            '28': '7rem',
+            '32': '8rem',
+            '40': '10rem',
+            '48': '12rem',
         }
     },
     variants: {
         extend: {
-            borderStyle: ['active', 'hover'],
+            borderStyle: ['active', 'hover', 'first', 'last'],
             padding: ['active'],
             backgroundColor: ['even', 'odd', 'active'],
             opacity: ['disabled'],
