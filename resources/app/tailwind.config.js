931678a2c0e4b420cec54a49b279ce010a35a425
TJ Ward <jward3@email.unc.edu>
2021-03-05 12:06:10 -0500

    Fix up tailwind config

--- resources/app/tailwind.config.js
+++ resources/app/tailwind.config.js
@@ -4,28 +4,38 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            maxWidth: {
+                '28': '7rem',
+                '32': '8rem',
+                '40': '10rem',
+                '48': '12rem',
+                '56': '14rem',
+                '60': '16rem',
+                '64': '20rem',
+            },
+            minHeight: {
+                '0': '0',
+                '1/4': '25%',
+                '1/2': '50%',
+                '2/3': '66%',
+                '3/4': '75%',
+                'full': '100%'
+            },
+            minWidth: {
+                '4': '1rem',
+                '8': '2rem',
+                '16': '4rem',
+                '24': '6rem',
+                '28': '7rem',
+                '32': '8rem',
+                '40': '10rem',
+                '48': '12rem',
+            },    
+        },
         container: {
             padding: '3rem'
         },
-        minHeight: {
-            '0': '0',
-            '1/4': '25%',
-            '1/2': '50%',
-            '2/3': '66%',
-            '3/4': '75%',
-            'full': '100%'
-        },
-        minWidth: {
-            '4': '1rem',
-            '8': '2rem',
-            '16': '4rem',
-            '24': '6rem',
-            '28': '7rem',
-            '32': '8rem',
-            '40': '10rem',
-            '48': '12rem',
-        }
     },
     variants: {
         extend: {
@@ -34,7 +44,8 @@ module.exports = {
             backgroundColor: ['even', 'odd', 'active'],
             opacity: ['disabled'],
             cursor: ['disabled'],
-            gradientColorStops: ['active']
+            gradientColorStops: ['active'],
+
         },
     },
     plugins: [],
