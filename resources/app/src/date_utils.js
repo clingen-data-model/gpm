ec6601d76b9af130647748bd357c91175e6853f3
TJ Ward <jward3@email.unc.edu>
2021-03-17 12:49:09 -0400

    Fix bug in date_utils.formatDate add 1 to day of month.

--- resources/app/src/date_utils.js
+++ resources/app/src/date_utils.js
@@ -2,7 +2,7 @@ const formatDate = function(date) {
     const d = new Date(date);
     const year = d.getFullYear().toString();
     const month = (d.getMonth() + 1).toString().padStart(2, '0');
-    const day = (d.getDate() + 1).toString().padStart(2, '0');
+    const day = (d.getDate()).toString().padStart(2, '0');
 
     return `${year}-${month}-${day}`
 }
