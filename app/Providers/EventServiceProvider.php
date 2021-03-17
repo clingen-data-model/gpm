c420c60fedd79d13fd5ab39c16af62fb072ad386
TJ Ward <jward3@email.unc.edu>
2021-03-17 12:46:11 -0400

    Clean up EventServiceProvider.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,19 +4,12 @@ namespace App\Providers;
 
 use App\Listeners\RecordEvent;
 use App\Listeners\TestListener;
+use App\Modules\Application\Events\ApplicationCompleted;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
-use App\Modules\Application\Events\ContactAdded;
+
 use App\Modules\Application\Events\StepApproved;
-use App\Modules\Application\Events\DocumentAdded;
-use App\Modules\Application\Events\ContactRemoved;
-use App\Modules\Application\Events\NextActionAdded;
-use App\Modules\Application\Events\DocumentReviewed;
-use App\Modules\Application\Events\NextActionCompleted;
-use App\Modules\Application\Events\ApplicationCompleted;
 use App\Modules\Application\Events\ApplicationInitiated;
-use App\Modules\Application\Events\ExpertPanelAttributesUpdated;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -28,6 +21,13 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         // NOTE That intra-module listeners are registered in the module's service provider.
+        // StepApproved::class => [
+        //     SendStepApprovedNotification::class
+        // ],
+        // ApplicationCompleted::class => [
+        //     SendApplicationCompletedNotification::class
+        // ],
+        
     ];
 
     /**
