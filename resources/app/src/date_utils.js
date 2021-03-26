654f4f04aa8cf5d67df22b7a271362dc8c6b63e9
TJ Ward <jward3@email.unc.edu>
2021-03-26 15:14:11 -0400

    Update date_utils to use toLocleDateString

--- resources/app/src/date_utils.js
+++ resources/app/src/date_utils.js
@@ -1,10 +1,20 @@
 const formatDate = function(date) {
     const d = new Date(date);
-    const year = d.getFullYear().toString();
-    const month = (d.getMonth() + 1).toString().padStart(2, '0');
-    const day = (d.getDate()).toString().padStart(2, '0');
+    // const year = d.getFullYear().toString();
+    // const month = (d.getMonth() + 1).toString().padStart(2, '0');
+    // const day = (d.getDate()).toString().padStart(2, '0');
 
-    return `${year}-${month}-${day}`
+    return d.toLocaleDateString();
 }
 
-export { formatDate }
+const formatDateTime = function(date) {
+    const d = new Date(date);
+    // const year = d.getFullYear().toString();
+    // const month = (d.getMonth() + 1).toString().padStart(2, '0');
+    // const day = (d.getDate()).toString().padStart(2, '0');
+    // const time = (d.getDate()).toString().padStart(2, '0');
+
+    return `${d.toLocaleDateString()} ${d.toLocaleTimeString([], {timeStyle: 'short'})}`;
+}
+
+export { formatDate, formatDateTime }
