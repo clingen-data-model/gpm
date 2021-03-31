0048ead9ef666fe6504e1284a7bb360e2f72d321
TJ Ward <jward3@email.unc.edu>
2021-03-31 15:01:31 -0400

    Extract alert viewer to it's own component

--- resources/app/src/App.vue
+++ resources/app/src/App.vue
@@ -23,34 +23,21 @@
       </div>
     </div>
 
-    <div class="container mx-auto my-3">
-      <router-view/>
-    </div>
+    <router-view class="container mx-auto my-3"/>
 
-    <div class="fixed right-4 bottom-4 w-64">
-      <ul>
-        <transition-group name="slide-fade" tag="li">
-          <div v-for="alert in $store.state.alerts.alerts" :key="alert.uuid"
-            class="flex justify-between rounded px-3 py-2 my-2 alert-item" 
-            :class="`alert-${alert.type}`"
-          >
-            {{alert.message}}
-            <button @click="$store.commit('removeAlert', alert.uuid)" class="hover:underline">x</button>
-          </div>
-        </transition-group>
-      </ul>
-    </div>
-
+    <alert-viewer></alert-viewer>
   </div>
 </template>
 
 <script>
 import UserMenu from './components/UserMenu'
+import AlertViewer from './components/alerts/AlertViewer'
 import { mapGetters } from 'vuex'
 
 export default {
   components: {
     UserMenu,
+    AlertViewer
   },
   data() {
     return {
---
+++ resources/app/src/components/alerts/AlertViewer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="fixed right-4 bottom-4 w-64">
+      <ul>
+        <transition-group name="slide-fade" tag="li">
+          <div v-for="alert in $store.state.alerts.alerts" :key="alert.uuid"
+            class="flex justify-between rounded px-3 py-2 my-2 alert-item" 
+            :class="`alert-${alert.type}`"
+          >
+            {{alert.message}}
+            <button @click="$store.commit('removeAlert', alert.uuid)" class="hover:underline">x</button>
+          </div>
+        </transition-group>
+      </ul>
+    </div>
+
+</template>
+<script>
+export default {
+    props: {
+        
+    },
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+}
+</script>
