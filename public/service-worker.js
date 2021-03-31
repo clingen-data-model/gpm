354863638525b2a6b45164d8995787d7ff44ef2f
TJ Ward <jward3@email.unc.edu>
2021-03-31 17:27:17 -0400

    Build js for deploy

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c88679478ffb47891008e2a1e227a648.js"
+  "/precache-manifest.6e997fd03f27fefc654d302fe6ebcc5c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "EPAM"});
