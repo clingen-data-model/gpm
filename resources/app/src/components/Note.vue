1cc8c462f1af6fd81272df69f51352994a195547
TJ Ward <jward3@email.unc.edu>
2021-04-09 10:52:30 -0400

    Add Note Component

---
+++ resources/app/src/components/Note.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="text-xs text-gray-400"><slot/></div>
+</template>
+<script>
+export default {
+    props: {
+        
+    },
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+}
+</script>
