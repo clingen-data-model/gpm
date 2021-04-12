078e38b5fddfddf378732ff9ea89c67f50973ce9
TJ Ward <jward3@email.unc.edu>
2021-04-12 09:59:37 -0400

    Fix backtrace helper bug

--- app/helpers.php
+++ app/helpers.php
@@ -12,7 +12,7 @@ if (!function_exists('renderQuery')) {
 if (!function_exists('backtrace')) {
     function backtrace($limit = 0) {
         return array_map(function ($result) {
-            return $result->file.':'.$result->line;
+            return $result['file'].':'.$result['line'];
         }, debug_backtrace(null, $limit));
     }
 }
