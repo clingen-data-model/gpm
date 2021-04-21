1742a9e8c61ed69c95ea40ceb3511b548327b53c
TJ Ward <jward3@email.unc.edu>
2021-04-21 12:38:26 -0400

    Update Log name for easier identification in slack.

--- config/logging.php
+++ config/logging.php
@@ -56,7 +56,7 @@ return [
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
+            'username' => env('APP_NAME').' Log',
             'emoji' => ':boom:',
             'level' => env('LOG_LEVEL', 'critical'),
         ],
