a6e72b5000db3cf97ad72ac7915c815d4baae2bf
TJ Ward <jward3@email.unc.edu>
2021-05-26 15:25:16 -0400

    Add TrashIconButton

---
+++ resources/app/src/components/buttons/TrashIconButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="btn btn-xs">
+        <icon-trash width="12" height="16"></icon-trash>
+    </button>
+</template>
+<script>
+import IconTrash from '../icons/IconTrash'
+
+export default {
+    components: {
+        IconTrash,
+    },
+    props: {
+    },
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+}
+</script>
