5ef42dd98e1cb67298d4e761914d3f29f52871f3
TJ Ward <jward3@email.unc.edu>
2021-06-17 13:11:57 -0400

    Fixing step approal emails and UI.

--- resources/views/applications/email/approval/initial_approval.blade.php
+++ resources/views/applications/email/approval/initial_approval.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <p>
-        Thank you for submitting your application for the {{$application->name}} {{ $application->epType->display_name}}. 
+        Thank you for submitting your application for the {{$application->long_base_name}}. 
         Your {{ ($application->ep_type_id == 1) ? 'GCEP' : 'VCEP step 1 application' }} has been reviewed and approved by the ClinGen Clinical Domain Working Group Oversight Committee co-chairs. We will be following up with additional information. 
     </p>
 
