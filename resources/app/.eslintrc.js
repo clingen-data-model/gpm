bba3bbff79e7e91183efb5b68cc0294ef420668b
TJ Ward <jward3@email.unc.edu>
2021-07-08 15:05:27 -0400

    Make eslint more sane

--- resources/app/.eslintrc.js
+++ resources/app/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
         'no-empty': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
         'no-unreachable': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
         'vue/no-unused-components': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+        'vue/no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     },
     overrides: [{
         files: [
