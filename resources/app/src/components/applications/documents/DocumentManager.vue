bfd182516d89586e9553ee1eee48f1bbc3ffc75f
TJ Ward <jward3@email.unc.edu>
2021-07-16 16:00:28 -0400

    Allow uploading of documents to approved steps.

--- resources/app/src/components/applications/documents/DocumentManager.vue
+++ resources/app/src/components/applications/documents/DocumentManager.vue
@@ -1,35 +1,38 @@
 <template>
     <div>
-        <final-document-view
-            :application="application"
-            :document-type-id="documentTypeId"
-            :step="step"
-            v-if="application.stepIsApproved(step)"
-        >
-        </final-document-view>
+        <div v-if="application.stepIsApproved(step)">
+            <final-document-view
+                :application="application"
+                :document-type-id="documentTypeId"
+                :step="step"
+            >
+            </final-document-view>
+            <button class="btn mb-2" @click="showUploadForm = true">
+                Upload a new version
+            </button>
+        </div>
 
         <div v-else>
             <button class="btn mb-2" @click="showUploadForm = true">
                 Upload a new version
             </button>
-            
             <document-list 
                 :application="application"
                 :document-type-id="documentTypeId" 
             ></document-list>
             
-            <modal-dialog v-model="showUploadForm" @closed="$refs.uploadform.clearForm()">
-                <document-upload-form 
-                    :application="application" 
-                    :document-type-id="documentTypeId" 
-                    :step="step"
-                    @canceled="showUploadForm = false" 
-                    @saved="showUploadForm = false" 
-                    ref="uploadform"
-                >
-                </document-upload-form>
-            </modal-dialog>
         </div>
+        <modal-dialog v-model="showUploadForm" @closed="$refs.uploadform.clearForm()">
+            <document-upload-form 
+                :application="application" 
+                :document-type-id="documentTypeId" 
+                :step="step"
+                @canceled="showUploadForm = false" 
+                @saved="showUploadForm = false" 
+                ref="uploadform"
+            >
+            </document-upload-form>
+        </modal-dialog>
     </div>
 </template>
 <script>
