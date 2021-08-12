e2f54fe6fcad036628807247f0ccb033299296b0
TJ Ward <jward3@email.unc.edu>
2021-08-12 10:11:45 -0400

    Add migration to update activity_log subject type for expert panels

---
+++ database/migrations/2021_08_12_135851_update_expert_panel_polymorphic_type_attributes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateExpertPanelPolymorphicTypeAttributes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('activity_log')
+            ->where('subject_type', 'App\Modules\Application\Models\Application')
+            ->update(['subject_type' =>'App\Modules\ExpertPanel\Models\ExpertPanel']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('activity_log')
+            ->where('subject_type', 'App\Modules\Application\Models\ExpertPanel')
+            ->update(['subject_type' =>'App\Modules\Application\Models\Application']);
+    }
+}
