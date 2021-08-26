0238fbbfb33a567a3b43c0e19137311ca76e8a09
TJ Ward <jward3@email.unc.edu>
2021-08-26 09:23:34 -0400

    Trying my hand at client-side end-to-end testing.

--- resources/app/tests/e2e/specs/test.js
+++ resources/app/tests/e2e/specs/test.js
@@ -2,7 +2,14 @@
 
 describe('My First Test', () => {
   it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.visit('http://localhost:8081/');
+    cy.url().should('include', '/login')
+    cy.contains('Login')
+    cy.contains('Email:')
+    cy.contains('Password:')
+    cy.get('[name=email]')
+      .type('jward3@email.unc.edu');
+    cy.get('[name=password]').type('tester');
+    cy.get('[name=login-button]').click();
   })
 })
