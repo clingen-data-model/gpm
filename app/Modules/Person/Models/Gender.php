6be84575ac2672d7b6ab2ba03b229584bf9674c7
TJ Ward <jward3@email.unc.edu>
2021-09-13 20:51:57 -0400

    Add lookup models related to person profile.

---
+++ app/Modules/Person/Models/Gender.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Modules\Person\Models;
+
+use Database\Factories\GenderFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
+class Gender extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+    ];
+
+    // Factory
+    protected static function newFactory()
+    {
+        return new GenderFactory();
+    }
+}
