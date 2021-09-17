7634cb6b40284cfe4f73e808ddde7a3a6d8f5443
TJ Ward <jward3@email.unc.edu>
2021-09-17 10:03:27 -0400

    Move alerts to top of screen and move styles to component.

--- resources/app/src/components/alerts/AlertViewer.vue
+++ resources/app/src/components/alerts/AlertViewer.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="fixed right-4 bottom-4 w-64">
+    <div class="fixed right-4 top-8 w-64 z-50">
       <ul>
         <transition-group name="slide-fade" tag="li">
           <div v-for="alert in $store.state.alerts.alerts" :key="alert.uuid"
-            class="flex justify-between rounded px-3 py-2 my-2 alert-item" 
+            class="flex justify-between rounded px-3 py-2 my-2 alert-item shadow-lg" 
             :class="`alert-${alert.type}`"
           >
             {{alert.message}}
@@ -32,3 +32,17 @@ export default {
     }
 }
 </script>
+<style lang="postcss">
+  .alert-info {
+      @apply bg-blue-200 text-blue-800 border-blue-300 border;
+  }
+  .alert-success {
+      @apply bg-green-200 text-green-800 border-green-300 border;
+  }
+  .alert-warning {
+      @apply bg-yellow-200 text-yellow-800 border-yellow-300 border;
+  }
+  .alert-error {
+      @apply bg-red-200 text-red-800 border-red-300 border;
+  }
+</style>
