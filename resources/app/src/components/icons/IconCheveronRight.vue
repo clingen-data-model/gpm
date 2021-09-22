d56cd5729e357739508c5f94135e1ddbf0fc6fd8
TJ Ward <jward3@email.unc.edu>
2021-09-22 12:19:32 -0400

    Add IconCheveronRight component

---
+++ resources/app/src/components/icons/IconCheveronRight.vue
@@ -0,0 +1,25 @@
+<template>
+    <icon-base name="chevron-down">
+        <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/>
+    </icon-base>
+</template>
+<script>
+import IconBase from './IconBase.vue'
+export default {
+  components: { IconBase },
+    props: {
+        
+    },
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+}
+</script>   
