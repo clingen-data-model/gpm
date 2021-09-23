ac639f5419cc0b2beef4028ef0a315839466d1ab
TJ Ward <jward3@email.unc.edu>
2021-09-23 17:11:19 -0400

    Add GroupEvent to prevent missing directory error.

---
+++ app/Modules/Group/Events/GroupEvent.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Modules\Group\Events;
+
+use Illuminate\Support\Carbon;
+use App\Events\RecordableEvent;
+use App\Modules\Group\Models\Group;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Broadcasting\PrivateChannel;
+use phpDocumentor\Reflection\Types\Boolean;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+abstract class GroupEvent extends RecordableEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(public Group $group)
+    {
+    }
+
+    public function hasSubject(): bool
+    {
+        return true;
+    }
+
+    public function getSubject(): Model
+    {
+        return $this->group;
+    }
+    
+    public function getLog(): string
+    {
+        return 'groups';
+    }
+
+    public function getProperties(): ?array
+    {
+        return null;
+    }
+
+    public function getLogDate(): Carbon
+    {
+        return Carbon::now();
+    }
+
+    abstract public function getLogEntry() :string;
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
