813347ea9648ae205a4974264fd5f8562c148358
TJ Ward <jward3@email.unc.edu>
2021-10-05 18:13:53 -0400

    Extract test data from GroupDetail, and show other group data.

--- resources/app/src/views/groups/GroupDetail.vue
+++ resources/app/src/views/groups/GroupDetail.vue
@@ -1,5 +1,27 @@
 <template>
     <div>
+        <header class="pb-4">
+            <h1>{{group.name}}</h1>
+            <dictionary-row label="Chairs:">
+                <template v-slot:label><strong>Chairs:</strong></template>
+                <div v-if="group.chairs.length > 0">
+                    {{group.chairs.join(', ')}}
+                </div>
+                <div class="text-gray-500" v-else>
+                    None assigned
+                </div>
+            </dictionary-row>
+            <dictionary-row label="Coordinators:">
+                <template v-slot:label><strong>Coordinators:</strong></template>
+                <div v-if="group.coordinators.length > 0">
+                    {{group.coordinators.join(', ')}}
+                </div>
+                <div class="text-gray-500" v-else>
+                    None assigned
+                </div>
+            </dictionary-row>
+        </header>
+
         <member-list :group="group"></member-list>
 
         <modal-dialog v-model="showModal" @closed="handleModalClosed" :title="this.$route.meta.title">
@@ -9,85 +31,9 @@
     </div>
 </template>
 <script>
-    import Group from "@/domain/group";
-    import config from '@/configs.json';
-    import MemberList from '@/components/groups/MemberList';
-
-    const group = new Group({name: 'Bob\'s Burgers', uuid: 'abcd-1234', id: 1, group_type_id: config.groups.types.ep.id});
-    group.addMember({
-        id: 1,
-        person_id: 1,
-        person: {
-            first_name: 'Bob',
-            last_name: 'Belcher',
-            email: 'bob@bobsburgers.com',
-        },
-        start_date: '2021-01-01T00:00:00Z',
-        end_date: null,
-        v1_contact: 1,
-        roles: [
-            { name: 'chair', id: 2 },
-            { name: 'coordinator', id: 1 },
-            { name: 'biocurator', id: 3 }
-        ],
-        coi_last_completed: '2021-09-16T13:12:00Z',
-        training_completed_at: null,
-        needs_training: true
-    });
-    group.addMember({
-        id: 2,
-        person_id: 2,
-        person: {
-            first_name: 'Linda',
-            last_name: 'Belcher',
-            email: 'linda@bobsburgers.com',
-        },
-        start_date: '2021-01-01T00:00:00Z',
-        end_date: null,
-        v1_contact: 1,
-        roles: [
-            { name: 'coordinator', id: 1 },
-            { name: 'parent', id: 16 }
-        ],
-        coi_last_completed: '2020-05-17T15:09:00Z',
-        training_completed_at: null,
-        needs_training: false
-    });
-    group.addMember({
-        id: 3,
-        person_id: 3,
-        person: {
-            first_name: 'Tina',
-            last_name: 'Belcher',
-            email: 'tina@bobsburgers.com',
-        },
-        start_date: '2021-01-01T00:00:00Z',
-        end_date: null,
-        v1_contact: 1,
-        roles: [
-            { name: 'kid', id: 17 },
-        ],
-        training_completed_at: '2020-09-19T00:00:00Z',
-        needs_training: true
-    });
-    group.addMember({
-        id: 4,
-        person_id: 4,
-        person: {
-            first_name: 'Gene',
-            last_name: 'Belcher',
-            email: 'gene@bobsburgers.com',
-        },
-        start_date: '2021-01-01T00:00:00Z',
-        end_date: null,
-        v1_contact: 1,
-        roles: [
-            { name: 'biocurator', id: 3 },
-        ],
-        training_completed_at: null,
-        needs_training: true
-    });
-
+import MemberList from '@/components/groups/MemberList';
+import { ref, computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
 
 export default {
     name: 'GroupDetail',
@@ -100,10 +46,22 @@ export default {
             required: true
         }
     },
-    data () {
+    setup (props) {
+        const store = useStore();
+        const showModal = ref(false);
+
+        onMounted(async () => {
+            await store.dispatch('groups/find', props.uuid)
+                .then(response => {
+                    store.commit('groups/setCurrentItemIdxByUuid', props.uuid)
+                })
+        });
+
         return {
-            group: group,
-            showModal: false,
+            group: computed(() => {
+                return store.getters['groups/currentItem'] || {}
+            }),
+            showModal
         }
     },
     watch: {
@@ -128,7 +86,6 @@ export default {
         },
     },
     mounted() {
-        console.log(this.$options.name)
         this.showModal = Boolean(this.$route.meta.showModal)
     }
 }
