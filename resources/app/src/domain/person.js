3a0b96fc915104838387d42ce18ee39d658127cb
TJ Ward <jward3@email.unc.edu>
2021-10-05 18:14:51 -0400

    Fix person.matchesKeyword

--- resources/app/src/domain/person.js
+++ resources/app/src/domain/person.js
@@ -19,20 +19,24 @@ class Person extends Entity {
     ]
 
     matchesKeyword(keyword) {
+        if (!keyword || keyword.length < 3) {
+            return false;
+        }
         const {first_name, last_name, email} = this.attributes;
-        const pattern = new RegExp(keyword);
-        if (first_name && first_name.match(pattern)) {
+        const pattern = new RegExp(keyword.toLowerCase());
+
+        if (first_name && first_name.toLowerCase().match(pattern)) {
             return true;
         }
-        if (last_name && last_name.match(pattern)) {
+        if (last_name && last_name.toLowerCase().match(pattern)) {
             return true;
         }
-        if (email && email.match(pattern)) {
+        if (email && email.toLowerCase().match(pattern)) {
             return true;
         }
         if (first_name && last_name) {
             const fullname = `${first_name} ${last_name}`;
-            if (fullname && fullname.match(pattern)) {
+            if (fullname && fullname.toLowerCase().match(pattern)) {
                 return true;
             }
         }
