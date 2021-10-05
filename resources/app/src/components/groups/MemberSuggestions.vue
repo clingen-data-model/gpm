2cb12a52f1ba2e9de17b97af51f7e5b91b8de537
TJ Ward <jward3@email.unc.edu>
2021-10-05 18:11:35 -0400

    Get MemberForm working.

---
+++ resources/app/src/components/groups/MemberSuggestions.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <ul>
+            <li 
+                v-for="suggestion in suggestions" 
+                :key="suggestion.uuid" class="flex justify-between my-2"
+            >
+                <slot>
+                    {{suggestion.name}} 
+                    <button 
+                        class="btn btn-xs" 
+                        @click="useSuggestion(suggestion)"
+                    >
+                        Add as member
+                    </button>
+                </slot>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ComponentName',
+    props: {
+        suggestions: {
+            required: true,
+            type: Array
+        }
+    },
+    emits: [
+        'selected',
+
+    ],
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+        useSuggestion(item) {
+            this.$emit('selected', item);
+        }
+    }
+}
+</script>
