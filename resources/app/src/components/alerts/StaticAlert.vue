64b151603d0b9c423ba12c25283e4caf3e82bad0
TJ Ward <jward3@email.unc.edu>
2021-10-07 13:12:58 -0400

    Add border styling to StaticAlert

--- resources/app/src/components/alerts/StaticAlert.vue
+++ resources/app/src/components/alerts/StaticAlert.vue
@@ -1,15 +1,15 @@
 <style lang="postcss" scoped>
     .danger {
-        @apply bg-red-200 text-red-800;
+        @apply bg-red-200 text-red-800 border-red-500;
     }
     .warning {
-        @apply bg-yellow-200 text-yellow-800;
+        @apply bg-yellow-200 text-yellow-700 border-yellow-500;
     }
     .success {
-        @apply bg-green-200 text-green-800;
+        @apply bg-green-200 text-green-800 border-green-500;
     }
     .info {
-        @apply bg-blue-200 text-blue-800;
+        @apply bg-blue-200 text-blue-800 border-blue-500;
     }
 </style>
 <template>
