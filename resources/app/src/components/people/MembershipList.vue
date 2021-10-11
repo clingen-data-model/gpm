eae20021568cf34878015c4f5b8f9c6d13bffb03
TJ Ward <jward3@email.unc.edu>
2021-10-11 15:26:32 -0400

    Add membership list including group info to person detail;

---
+++ resources/app/src/components/people/MembershipList.vue
@@ -0,0 +1,88 @@
+<template>
+    <div>
+        <data-table 
+            :fields="fields" 
+            :data="person.memberships" 
+            v-model:sort="sort"
+            :rowClickHandler="goToGroup"
+            :row-class="() => 'cursor-pointer'"
+        ></data-table>
+    </div>
+</template>
+<script>
+import Person from '@/domain/person'
+
+export default {
+    name: 'ComponentName',
+    props: {
+        person: {
+            type: Person,
+            required: true
+        }
+    },
+    data() {
+        return {
+            sort: {
+                field: 'group.name',
+                desc: false
+            },
+            fields: [
+                {
+                    name: 'group.name',
+                    type: String,
+                    sortable: true,
+                    label: 'Name'
+                },
+                {
+                    name: 'group.type.name',
+                    type: String,
+                    sortable: true,
+                    label: 'Type'
+                },
+                {
+                    name: 'roles',
+                    type: String,
+                    sortable: false,
+                    label: 'Roles',
+                    resolveValue (item) {
+                        return item.roles.join(', ');
+                    }
+                },
+                {
+                    name: 'permissions',
+                    type: String,
+                    sortable: false,
+                    label: 'Permissions',
+                    resolveValue (item) {
+                        return item.permissions.join(', ');
+                    }
+                },
+                {
+                    name: 'start_date',
+                    type: Date,
+                    sortable: true,
+                    label: 'Started'
+                },
+                {
+                    name: 'end_date',
+                    type: Date,
+                    sortable: true,
+                    label: 'Ended'
+                }
+
+
+            ]
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+        goToGroup(item) {
+            console.log(item.group.uuid);
+            this.$router.push({name: 'GroupDetail', params: {uuid: item.group.uuid}})
+        }
+
+    }
+}
+</script>
