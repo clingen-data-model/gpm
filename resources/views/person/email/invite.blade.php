c085189d111d18ffc45a0f7c53d44d5f97e682f5
TJ Ward <jward3@email.unc.edu>
2021-10-11 15:27:36 -0400

    Handle no inviter when sending invite email.

--- resources/views/person/email/invite.blade.php
+++ resources/views/person/email/invite.blade.php
@@ -3,7 +3,11 @@
 </p>
 
 <p>
-    You have been invited to join the Clinical Genome Resource in the {{$invite->inviter->name}}.
+    @if($invite->inviter) {
+        You have been invited to join the Clinical Genome Resource in the {{$invite->inviter->name}}.
+    @else
+        You have been invited to join the Clinical Genome Resource's Group and Personnel Management System.
+    @endif
 
     To accept this invitation, please visit 
     <a href="{{url('/invites/'.$invite->code)}}">{{url('/invites/'.$invite->code)}}</a>
