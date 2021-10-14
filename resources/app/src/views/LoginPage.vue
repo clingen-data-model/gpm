0f6f869997c3e62e0e67026fe02a7271c5961e04
TJ Ward <jward3@email.unc.edu>
2021-10-14 09:45:05 -0400

    Add link to onboarding to LoginPage

--- resources/app/src/views/LoginPage.vue
+++ resources/app/src/views/LoginPage.vue
@@ -1,7 +1,14 @@
 <template>
-    <card title="Login" class="lg:w-1/2 mx-auto">
-        <login-form @authenticated="redirect"></login-form>
-    </card>
+    <div>
+        <card title="Login" class="md:w-1/2 mx-auto">
+            <login-form @authenticated="redirect"></login-form>
+        </card>
+        <div class="mx-auto md:w-1/2 mt-8 p-4 bg-white border border-gray-300 rounded">
+            <router-link class="block text-center btn blue btn-lg w-full" :to="{name: 'RedeemInvite'}">
+                Redeem your invite
+            </router-link>
+        </div>
+    </div>
 </template>
 <script>
 import LoginForm from '@/components/LoginForm';
