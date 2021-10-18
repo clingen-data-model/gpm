292ce6160fded2c0adb95203bb7bb020ef85db98
TJ Ward <jward3@email.unc.edu>
2021-10-18 11:02:49 -0400

    Begin building/using modelSearchService.

--- app/Modules/Group/Http/Controllers/Api/GroupController.php
+++ app/Modules/Group/Http/Controllers/Api/GroupController.php
@@ -5,13 +5,31 @@ namespace App\Modules\Group\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Modules\Group\Models\Group;
 use App\Http\Controllers\Controller;
+use App\ModelSearchService;
 use App\Modules\Group\Http\Resources\GroupResource;
 
 class GroupController extends Controller
 {
     public function index(Request $request)
     {
-        return Group::with(['type', 'status'])->get();
+        $searchService = new ModelSearchService(
+            modelClass: Group::class,
+            defaultWith: ['type', 'status'],
+            sortFunction: function ($query, $field, $dir) {
+                if ($field == 'type') {
+                    $query->innerJoin('group_types', 'groups.group_type_id', '=', 'group_types.id');
+                    $query->ordeBy('group_types.name', $dir);
+                    return $query;
+                }
+                if ($field == 'status') {
+                    $query->innerJoin('group_statuses', 'groups.group_status_id', '=', 'group_statuses.id');
+                    $query->ordeBy('group_statuses.name', $dir);
+                    return $query;
+                }
+                return $query->orderBy($field, $dir);
+            }
+        );
+        return $searchService->search($request->all());
     }
 
     public function show($uuid, Request $request)
