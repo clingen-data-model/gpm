8787441df0db0a14dd27449c68fbca52c0cefd4d
TJ Ward <jward3@email.unc.edu>
2021-10-26 11:27:30 -0400

    Add title to PersonEdit route meta.

--- resources/app/src/router/people.js
+++ resources/app/src/router/people.js
@@ -51,7 +51,8 @@ export default [
                 },
                 meta: {
                     showModal: true,
-                    protected: true
+                    protected: true,
+                    title: 'Update Information'
                 },
                 props: true,
                 beforeEnter: [checkPermissionAndPersonOwnership]
