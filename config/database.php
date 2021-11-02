82f23148c3e4ba9281b71fc7f10ab21ce783eaf3
TJ Ward <jward3@email.unc.edu>
2021-11-02 12:02:08 -0400

    add genetracker db to database config.

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'genetracker' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('GT_DB_HOST', '127.0.0.1'),
+            'port' => env('GT_DB_PORT', '3306'),
+            'database' => env('GT_DB_DATABASE', 'gene_tracker'),
+            'username' => env('GT_DB_USERNAME', 'forge'),
+            'password' => env('GT_DB_PASSWORD', ''),
+            'unix_socket' => env('GT_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
