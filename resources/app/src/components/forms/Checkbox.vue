cd10ba5082d5686b75a0347f554cb1afeadb2510
TJ Ward <jward3@email.unc.edu>
2021-11-18 12:41:27 -0500

    Add mixin to generate unique component id.

--- resources/app/src/components/forms/Checkbox.vue
+++ resources/app/src/components/forms/Checkbox.vue
@@ -1,13 +1,14 @@
 <template>
-    <label>
+    <div class="flex space-x-2 mb-2">
         <slot name="input">
-            <input type="checkbox" v-model="val">
+            <input type="checkbox" v-model="val" :id="checkboxId" class="mt-1">
         </slot>
-        &nbsp;
-        <slot>
-            {{label}}
-        </slot>
-    </label>
+        <label :for="checkboxId">
+            <slot>
+                {{label}}
+            </slot>
+        </label>
+    </div>
 </template>
 <script>
 export default {
@@ -19,13 +20,16 @@ export default {
         },
         label: {
             required: false
-        }
+        },
     },
     emits: [
         'update:modelValue',
         'change'
     ],
     computed: {
+        checkboxId () {
+            return `checkbox-${this.uid}`;
+        },
         val: {
             get () {
                 return this.modelValue
