3bc01a70dc161661410e67462bed42ab906d1400
TJ Ward <jward3@email.unc.edu>
2021-11-29 14:37:08 -0500

    Add ApplicationSummary component.

---
+++ resources/app/src/components/groups/ApplicationSummary.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="p-4 my-4 border rounded-xl" v-if="group.isEp()">
+        <router-link v-if="group.uuid" :to="{name: 'GroupApplication', params: {uuid: group.uuid}}">
+            <button class="btn blue btn-lg">Go to application</button>
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ApplicationSummary',
+    props: {
+        group: {
+            required: true,
+            type: Object
+        }
+    },
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+}
+</script>
