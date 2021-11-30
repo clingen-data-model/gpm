1d817087c7df437f1516d61b5f843cdcd39127bd
TJ Ward <jward3@email.unc.edu>
2021-11-30 12:44:48 -0500

    Move BasicInfo fields and login into GroupForm.

--- resources/app/src/components/groups/GroupForm.vue
+++ resources/app/src/components/groups/GroupForm.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <input-row label="Type" v-if="canSetType">
-            <select v-model="workingCopy.group_type_id" class="w-full">
+            <select v-model="group.group_type_id" class="w-full">
                 <option :value="null">Select&hellip;</option>
                 <option v-for="type in groupTypes" :key="type.id" :value="type.id">
                     {{type.fullname}}
@@ -13,66 +13,127 @@
         </dictionary-row>
         
         <transition name="slide-fade-down" mode="out-in">
-            <basic-info-form 
-                :group="group" ref="infoForm"
-                v-if="workingCopy.group_type_id > 2 && group.expert_panel"
-            />
+            <div v-if="group.group_type_id > 2 && group.expert_panel">
+                <input-row 
+                    label="Long Base Name" 
+                    v-model="group.expert_panel.long_base_name" 
+                    placeholder="Long base name"
+                    :errors="errors.long_base_name"
+                    input-class="w-full"
+                ></input-row>
+                <input-row 
+                    label="Short Base Name" 
+                    v-model="group.expert_panel.short_base_name" 
+                    placeholder="Short base name"
+                    :errors="errors.short_base_name"
+                    input-class="w-full"
+                ></input-row>
+                <div v-if="hasAnyPermission(['groups-manage'])">
+                    <input-row 
+                        label="Affiliation ID" 
+                        v-model="group.expert_panel.affiliation_id" 
+                        :placeholder="affiliationIdPlaceholder"
+                        :errors="errors.affiliation_id"  
+                        input-class="w-full"
+                    >
+                        <template v-slot:label>
+                            Affiliation ID
+                            <note>admin-only</note>
+                        </template>
+                    </input-row>
+                </div>
+                <dictionary-row label="Affiliation ID" v-else>
+                    <span v-if="group.expert_panel.affiliation_id">{{group.expert_panel.affiliation_id}}</span>
+                    <span v-else class="text-gray-400">{{'Not yet assigend'}}</span>
+                </dictionary-row>
+            </div>
             <div v-else>
-                <input-row v-model="workingCopy.name" placeholder="Name" label="Name" input-class="w-full"/>
-                <input-row label="Status">
-                    <select v-model="workingCopy.group_status_id" class="w-full">
-                        <option :value="null"></option>
-                        <option v-for="status in groupStatuses" :key="status.id" :value="status.id">
-                            {{titleCase(status.name)}}
-                        </option>
-                    </select>
-                </input-row>
+                <input-row 
+                    v-model="group.name" 
+                    placeholder="Name" 
+                    label="Name" 
+                    input-class="w-full"
+                    :errors="errors.name"
+                />
             </div>
         </transition>
-        <input-row label="Parent group" :errors="errors.parent_id">
-            <select v-model="workingCopy.parent_id" class="w-full">
-                <option :value="null">Select...</option>
-                <option :value="0">None</option>
-                <option v-for="group in groups" :key="group.id" :value="group.id">
-                    {{group.displayName}}
-                </option>
-            </select>
-        </input-row>
-        <button-row submit-text="Save" @submitted="save" @canceled="cancel"></button-row>
+        <div v-if="hasPermission('groups-manage')">
+            <input-row :errors="errors.group_status_id">
+                <template v-slot:label>
+                    Status:
+                    <note>admin-only</note>
+                </template>
+                <select v-model="group.group_status_id" class="w-full"
+                    
+                >
+                    <option :value="null"></option>
+                    <option v-for="status in groupStatuses" :key="status.id" :value="status.id">
+                        {{titleCase(status.name)}}
+                    </option>
+                </select>
+            </input-row>
+
+            <input-row :errors="errors.parent_id"  >
+                <template v-slot:label>
+                    Parent group:
+                    <note>admin-only</note>
+                </template>
+                <select v-model="group.parent_id" class="w-full">
+                    <option :value="null">Select...</option>
+                    <option :value="0">None</option>
+                    <option v-for="parent in groups" :key="parent.id" :value="parent.id">
+                        {{parent.displayName}}
+                    </option>
+                </select>
+            </input-row>
+        </div>
     </div>
 </template>
 <script>
 import is_validation_error from '@/http/is_validation_error'
 import Group from '@/domain/group'
-import BasicInfoForm from '@/components/expert_panels/BasicInfoForm'
 import configs from '@/configs'
 import formFactory from '@/forms/form_factory'
 
 export default {
     name: 'GroupForm',
-    components: {
-        BasicInfoForm,
-    },
-    props: {
-        group: {
-            type: Object,
-            required: false,
-            default: () => ({})
-        }
-    },
+    emits: [
+        'canceled',
+        'saved'
+    ],
     data() {
         return {
-            workingCopy: {},
             groupTypes: [
                 {id: 1, fullname: 'Working Group'},
                 {id: 2, fullname: 'Clinical Domain Working Group'},
                 {id: 3, fullname: 'GCEP'},
                 {id: 4, fullname: 'VCEP'},
             ],
-            groupStatuses: configs.groups.statuses
+            groupStatuses: configs.groups.statuses,
+            newGroup: new Group(),
         }
     },
     computed: {
+        group: {
+            get() {
+                const group = this.$store.getters['groups/currentItem'];
+                if (group) {
+                    console.log('got a group from the store')
+                    return group;
+                }
+                console.log('use this.newGroup b/c no group in the store.')
+                return this.newGroup;
+            },
+            set (value) {
+                console.log('set', value)
+                try {
+                    this.$store.commit('groups/addItem', value);
+                } catch (e) {
+                    console.log('no id so update newgroup')
+                    this.newGroup = value;
+                }
+            }
+        },
         groups () {
             return this.$store.getters['groups/all'];
         },
@@ -80,42 +141,40 @@ export default {
             return this.hasPermission('groups-manage') && !this.group.id 
         },
         typeDisplayName () {
-            if (!this.workingCopy.type) {
-                return 1;
+            if (!this.group.type) {
+                return "🐇🥚";
             }
-            return (this.workingCopy.type.id < 3) 
-                ? this.workingCopy.type.name.toUpperCase() 
-                : this.workingCopy.expert_panel.type.name.toUpperCase();
-        }
-    },
-    watch: {
-        group: function (to) {
-            this.syncWorkingCopy(to);
+            return (this.group.type.id < 3) 
+                ? this.group.type.name.toUpperCase() 
+                : this.group.expert_panel.type.name.toUpperCase();
+        },
+        affiliationIdPlaceholder () {
+            return 50000
+        },
+        cdwgs () {
+            return this.$store.getters['cdwgs/all']
+        },
+        namesDirty () {
+            return this.group.expert_panel.isDirty('long_base_name')
+                || this.group.expert_panel.isDirty('short_base_name');
+        },
+        affiliationIdDirty () {
+            return this.group.expert_panel.isDirty('affiliation_id');
         }
     },
     methods: {
-        initWorkingCopy () {
-            this.workingCopy = new Group();
-        },
-        syncWorkingCopy (group) {
-            if (Object.prototype.hasOwnProperty.call(group, 'attributes')) {
-                this.workingCopy = group.clone();
-            } else {
-               this.workingCopy = new Group(group)
-            }
-            this.workingCopy.group_type_id = group.expertPanel ? group.expertPanel.expert_panel_type_id+2 : group.group_type_id;
-        },
         async save() {
             try {
                 this.resetErrors();
-                if (this.workingCopy.id) {
+                if (this.group.id) {
                     await this.updateGroup();
                     this.$emit('saved');
 
-                    this.$store.dispatch('groups/find', this.workingCopy.uuid);
+                    this.$store.dispatch('groups/find', this.group.uuid);
                     this.$store.commit('pushSuccess', 'Group info updated.');
                     return;
                 } 
+
                 const newGroup = await this.createGroup()
                                     .then(response => response.data.data);
                 this.$emit('saved');
@@ -128,12 +187,16 @@ export default {
             }
         },
         createGroup () {
-            return this.$store.dispatch('groups/create', this.workingCopy);
+            let {name, parent_id, group_type_id, group_status_id} = this.group.attributes;
+            if (name === null && this.group.expert_panel) {
+                name = this.group.expert_panel.long_base_name;
+            }
+            return this.$store.dispatch('groups/create', {name, parent_id, group_type_id, group_status_id});
         },
         updateGroup () {
             const promises = [];
             promises.push(this.saveGroupData());
-            if (this.workingCopy.expert_panel) {
+            if (this.group.expert_panel) {
                 promises.push(this.saveEpData());
             }
             
@@ -141,57 +204,87 @@ export default {
         },
         saveGroupData () {
             const promises = [];
-            if (this.isDirty('parent_id')) {
+            console.log(this.group.isDirty)
+            if (this.group.isDirty('parent_id')) {
                 promises.push(this.saveParent());
             }
 
-            if (this.isDirty('name')) {
+            if (this.group.isDirty('name')) {
+                console.log('name is dirty.  save new name.')
                 promises.push(this.saveName())
             }
 
-            if (this.isDirty('group_status_id')) {
+            if (this.group.isDirty('group_status_id')) {
                 promises.push(this.saveStatus())
             }
 
             return Promise.all(promises);
         },
         async saveEpData() {
-            await this.$refs.infoForm.save();
+            const promises = []
+            if (this.namesDirty) {
+                console.log('update names')
+                const {long_base_name, short_base_name} = this.group.expert_panel;
+                promises.push(this.submitFormData({
+                    method: 'put',
+                    url: `/api/groups/${this.group.uuid}/expert-panel/name`, 
+                    data: { long_base_name, short_base_name }
+                }));
+            }
+
+            if (this.affiliationIdDirty) {
+                promises.push(this.submitFormData({
+                    method: 'put',
+                    url: `/api/groups/${this.group.uuid}/expert-panel/affiliation-id`, 
+                    data: { affiliation_id: this.affiliationId }
+                }));
+            }
+
+            return await Promise.all(promises);
         },
 
         isDirty (attribute) {
-            return this.group[attribute] != this.workingCopy[attribute]
+            return this.group[attribute] != this.group[attribute]
         },
         
         saveParent () {
             return this.submitFormData({
                 method: 'put',
                 url: `/api/groups/${this.group.uuid}/parent`, 
-                data: { parent_id: this.workingCopy.parent_id }
+                data: { parent_id: this.group.parent_id }
             })
         },
         saveName () {
+            console.log('saveName', this.group.uuid, this.grou);
             return this.submitFormData({
                 method: 'put',
                 url: `/api/groups/${this.group.uuid}/name`,
-                data: {name: this.workingCopy.name}
+                data: {name: this.group.name}
             })
         },
         saveStatus () {
             return this.submitFormData({
                 method: 'put',
                 url: `/api/groups/${this.group.uuid}/status`,
-                data: {status_id: this.workingCopy.group_status_id}
+                data: {status_id: this.group.group_status_id}
             })
         },
+        resetData () {
+            if (this.group.uuid) {
+                this.$store.dispatch('groups/find', this.group.uuid);
+            }
+        },
         cancel() {
-            this.syncWorkingCopy(this.group);
-            this.$refs.infoForm.syncData(this.group);
+            console.log('cancel!');
+            if (this.group.uuid) {
+                this.resetData();
+            }
             this.$emit('canceled');
         }
     },
     beforeMount() {
-        this.$store.dispatch('groups/getItems')
+        this.$store.dispatch('groups/getItems');
+        this.$store.dispatch('cdwgs/getAll');
     },
     setup (props, context) {
         const {errors, submitFormData, resetErrors} = formFactory(props, context)
