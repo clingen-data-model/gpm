72b559a7f2d2b63e4fad163a018db08a75826fca
TJ Ward <jward3@email.unc.edu>
2021-12-03 14:33:19 -0500

    Add http/index for more convenient importing.

---
+++ resources/app/src/http/index.js
@@ -0,0 +1,11 @@
+import api from './api'
+import is_validation_error from './is_validation_error'
+import is_auth_error from './is_auth_error'
+import query_string_from_params from './query_string_from_params'
+
+export { 
+    api,
+    is_validation_error,
+    is_auth_error,
+    query_string_from_params
+}
