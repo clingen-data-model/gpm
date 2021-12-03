138c2a3fed0dd48c652231ffdc752d3a45c1c32d
TJ Ward <jward3@email.unc.edu>
2021-12-03 14:35:58 -0500

    use camelcase instead of snake case for http method exports.

--- resources/app/src/http/index.js
+++ resources/app/src/http/index.js
@@ -1,11 +1,11 @@
 import api from './api'
-import is_validation_error from './is_validation_error'
-import is_auth_error from './is_auth_error'
-import query_string_from_params from './query_string_from_params'
+import isValidationError from './is_validation_error'
+import isAuthError from './is_auth_error'
+import queryStringFromParams from './query_string_from_params'
 
 export { 
     api,
-    is_validation_error,
-    is_auth_error,
-    query_string_from_params
+    isValidationError,
+    isAuthError,
+    queryStringFromParams
 }
