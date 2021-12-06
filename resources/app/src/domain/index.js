fb9b93d6c929974fc92485a72ef16fe61424dec5
TJ Ward <jward3@email.unc.edu>
2021-12-06 08:26:55 -0500

    Add domain.index

---
+++ resources/app/src/domain/index.js
@@ -0,0 +1,11 @@
+import Group from './group';
+import Person from './person';
+import GroupMember from './group_member';
+import User from './user'
+
+export {
+    Group,
+    Person,
+    GroupMember,
+    User
+}
