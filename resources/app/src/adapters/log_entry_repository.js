cc167cc69249c2e6e921a350fcc7baec60a047f0
TJ Ward <jward3@email.unc.edu>
2021-12-17 11:45:23 -0500

    Update _entries when saving, updating, or deleting.

--- resources/app/src/adapters/log_entry_repository.js
+++ resources/app/src/adapters/log_entry_repository.js
@@ -14,14 +14,27 @@ export const fetchEntries = async (apiUrl) => {
 
 export const saveEntry = async (apiUrl, entryData) => {
     return await api.post(apiUrl, entryData)
-        .then(response => response.data);
+        .then(response => {
+            _entries.value.push(response.data);
+            return response.data
+        });
 }
 
 export const updateEntry = async (apiUrl, entryData) => {
     return await api.put(apiUrl, entryData)
-        .then(response => response.data);
+        .then(response => {
+            const entryIdx = _entries.value.findIndex(entry => entry.id == response.data.id);
+            _entries.value[entryIdx] = response.data;
+            return response.data
+        });
 }
 
 export const deleteEntry = async (apiUrl, entryId) => {
-    return await api.delete(`${apiUrl}/${entryId}`);
+    return await api.delete(`${apiUrl}/${entryId}`)
+        .then(response => {
+            const entryIdx = _entries.value.findIndex(entry => entry.id == entryId);
+            _entries.value.splice(entryIdx, 1);
+
+            return response;
+        });
 }
