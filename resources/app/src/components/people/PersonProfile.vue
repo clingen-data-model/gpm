af0ed0a30f472e633388c73b1ea5fdc533466529
TJ Ward <jward3@email.unc.edu>
2021-12-20 16:17:25 -0500

    Increase white space in PersonProfile.

--- resources/app/src/components/people/PersonProfile.vue
+++ resources/app/src/components/people/PersonProfile.vue
@@ -10,7 +10,7 @@
         </edit-icon-button>
 
 
-        <dictionary-row 
+        <dictionary-row class="pb-2" label-class="w-40" 
             v-for="key in ['name', 'email']"
             :key="key"
             :label="titleCase(key)"
@@ -20,16 +20,16 @@
 
         <section class="mt-4 border-t pt-4">
             <h3>Profile</h3>
-            <dictionary-row label="Institution">{{person.institutionName}}</dictionary-row>
-            <dictionary-row label="Credentials">{{person.credentials}}</dictionary-row>
-            <dictionary-row label="Primary Occupation">{{person.primaryOccupationName}}</dictionary-row>
-            <dictionary-row label="Biography">{{person.biography}}</dictionary-row>
-            <dictionary-row label="ORCID">{{person.orcid_id}}</dictionary-row>
-            <dictionary-row label="Hypothes.is ID">{{person.hypothesis_id}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" label="Institution">{{person.institutionName}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" label="Credentials">{{person.credentials}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" label="Primary Occupation">{{person.primaryOccupationName}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" label="Biography">{{person.biography}}</dictionary-row>
+            <!-- <dictionary-row class="pb-2" label-class="w-40" label="ORCID">{{person.orcid_id}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" label="Hypothes.is ID">{{person.hypothesis_id}}</dictionary-row> -->
         </section>
 
         <section class="mt-4 border-t pt-4">
-            <dictionary-row label="Address">
+            <dictionary-row class="pb-2" label-class="w-40" label="Address">
                 <p v-if="person.street1">{{person.street1}}</p>
                 <p v-if="person.street2">{{person.street2}}</p>
                 <p>
@@ -39,28 +39,36 @@
                 </p>
                 <p v-if="person.country">{{person.country.name}}</p>
             </dictionary-row>
-            <dictionary-row label="Phone">{{person.phone}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" label="Phone">{{person.phone}}</dictionary-row>
         </section>
 
         <section class="mt-4 border-t pt-4" v-if="userIsPerson(person) || hasPermission('people-manage')">
-            <h3>Demographics</h3>
-            <dictionary-row label="Primary Occupation">{{person.primaryOccupationName}}</dictionary-row>
-            <dictionary-row label="Race">{{person.raceName}}</dictionary-row>
-            <dictionary-row label="Ethnicity">{{person.ethnicityName}}</dictionary-row>
-            <dictionary-row label="Gender">{{person.genderName}}</dictionary-row>
+            <header class="flex items-center space-x-2">
+                <h3>
+                    Demographics
+                </h3>
+                <popper hover arrow>
+                    <template v-slot:content>Only you and administrators can see this information</template>
+                    <icon-info class="text-gray-500"></icon-info>
+                </popper>
+            </header>    
+            <dictionary-row class="pb-2" label-class="w-40" label="Primary Occupation">{{person.primaryOccupationName}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" label="Race">{{person.raceName}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" label="Ethnicity">{{person.ethnicityName}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" label="Gender">{{person.genderName}}</dictionary-row>
         </section>
 
         <section class="mt-4 border-t pt-4" v-if="hasPermission('people-manage')">
             <h3>Metadata</h3>
-            <dictionary-row label="Uuid">{{person.uuid}}</dictionary-row>
-            <dictionary-row label="Numeric ID">{{person.id}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" label="Uuid">{{person.uuid}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" label="Numeric ID">{{person.id}}</dictionary-row>
 
-            <dictionary-row v-if="person.user_id" label="User ID">{{person.user_id}}</dictionary-row>
-            <dictionary-row v-else label="Invite Code">
+            <dictionary-row class="pb-2" label-class="w-40" v-if="person.user_id" label="User ID">{{person.user_id}}</dictionary-row>
+            <dictionary-row class="pb-2" label-class="w-40" v-else label="Invite Code">
                 {{person.invite ? person.invite.code : null}}
             </dictionary-row>
             
-            <dictionary-row 
+            <dictionary-row class="pb-2" label-class="w-40" 
                 v-for="key in ['created_at', 'updated_at']"
                 :key="key"
                 :label="key"
@@ -102,7 +110,6 @@ export default {
         person: {
             type: Person,
             required: true,
-            // default: () => new Person()
         }
     },
     data () {
