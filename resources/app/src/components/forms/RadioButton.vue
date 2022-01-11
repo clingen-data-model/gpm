31429d3ee18dbec3976a2bf48507c5c6c7c6ac9b
TJ Ward <jward3@email.unc.edu>
2022-01-10 19:17:14 -0500

    Add RadioButton component.

---
+++ resources/app/src/components/forms/RadioButton.vue
@@ -0,0 +1,53 @@
+<template>
+    <label class="mb-2">
+        <slot name="input">
+            <input 
+                type="radio" 
+                v-model="val" 
+                :id="radioId" 
+                class="mt-1" 
+                :disabled="$attrs.disabled" 
+                :value="value"
+                :checked="$attrs.checked">
+        </slot>
+        <div>
+            <slot>
+                {{label}}
+            </slot>
+        </div>
+    </label>
+</template>
+<script>
+export default {
+    name: 'Checkbox',
+    props: {
+        modelValue: {
+            required: false,
+            default: false
+        },
+        label: {
+            required: false
+        },
+        value: {
+            required: true,
+        }
+    },
+    emits: [
+        'update:modelValue',
+        'change'
+    ],
+    computed: {
+        radioId () {
+            return `radiobutton-${this.uid}`;
+        },
+        val: {
+            get () {
+                return this.modelValue
+            },
+            set (val) {
+                this.$emit('update:modelValue', val)
+            }
+        }
+    }
+}
+</script>
