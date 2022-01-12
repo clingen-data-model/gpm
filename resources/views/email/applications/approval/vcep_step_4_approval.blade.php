4876a103939ad95a90e5695fae0e7f981c09c191
TJ Ward <jward3@email.unc.edu>
2022-01-12 09:18:33 -0500

    Update step 4 approval email.

--- resources/views/email/applications/approval/vcep_step_4_approval.blade.php
+++ resources/views/email/applications/approval/vcep_step_4_approval.blade.php
@@ -2,17 +2,14 @@
 
 @section('content')
     <p>
-        Congratulations on being formally approved by the ClinGen Rule Specification Review Committee and the CDWG Oversight Committee as an official ClinGen Variant Curation Expert Panel (VCEP)! The ClinGen website should be updated to reflect your new status and an announcement was circulated in the ClinGen quarterly newsletter. We will also make an announcement on the <a href="https://twitter.com/ClinGenResource">ClinGen Twitter newsfeed</a>.
-    </p>
-
-    {{-- <p>
-        A copy of your Expert Panel application, including rule specifications, will be posted to the ClinVar website. The SOP for registering and submitting to ClinVar as a 3-star approved VCEP is attached.
-    </p> --}}
-    <p>
-        A copy of your Expert Panel application, including rule specifications, will be posted to the ClinVar website. You can download the SOP for registering and submitting to ClinVar as a 3-star approved VCEP <a href="{{url('/downloads/VCEP%20SOP%20-%20Registering%20and%20Submitting%20to%20ClinVar.pdf')}}">here</a>.
+        Congratulations on being formally approved by the CDWG Oversight Committee as an official ClinGen Variant Curation Expert Panel (VCEP)! The ClinGen website will be updated to reflect your new status and an announcement will be circulated in the ClinGen quarterly newsletter. 
     </p>
 
     <p>
-        We are delighted to welcome you as a ClinGen-approved VCEP. We encourage you to participate in the monthly ClinGen all consortium calls, and we also want to ensure that your VCEP is represented on the twice-yearly Clinical Domain Leadership call.
+        Please refer to the <a href="https://www.clinicalgenome.org/docs/clingen-variant-curation-expert-panel-vcep-protocol/">ClinGen VCEP Protocol</a> sections 2.5 “Post Approval Requirements” and 3. Variant “Prioritization and Reanalysis” for information on next steps. You may now publish your variant curations to the ClinGen Evidence Repository and submit to ClinVar. Please review the <a href="https://gpm.clinicalgenome.org/downloads/VCEP%20SOP%20-%20Registering%20and%20Submitting%20to%20ClinVar.pdf">SOP for registering and submitting to ClinVar as a 3-star approved VCEP</a>.
+    </p>
+
+    <p>
+        We are delighted to welcome you as a ClinGen-approved VCEP. We encourage you to participate in the monthly ClinGen All Consortium calls and ensure that your VCEP is represented on the twice-yearly Clinical Domain Leadership by requesting that coordinator(s) and chair(s) are added to the <a href="clingen_cdwg_leadership@list.nih.gov">clingen_cdwg_leadership@list.nih.gov</a> listserv on the <a href="https://sites.google.com/broadinstitute.org/clinical-genome-resource/education-coordination-and-training-working-group/coordinator-resource/listserv-requests?authuser=0">ClinGen Google site</a>.
     </p>
 @endsection
