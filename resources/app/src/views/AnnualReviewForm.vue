ce66c04e3d4297fc59900d8ae0e53282e7304903
TJ Ward <jward3@email.unc.edu>
2022-01-17 11:17:07 -0500

    Add new components to AnnualReviewForm

--- resources/app/src/views/AnnualReviewForm.vue
+++ resources/app/src/views/AnnualReviewForm.vue
@@ -1,126 +1,8 @@
-<template>
-    <div>
-        <router-link class="note"
-            :to="{name: 'GroupDetail', params: {uuid: group.uuid}}"
-            v-if="group.uuid"
-        >
-            {{group.displayName}}
-        </router-link>
-
-        <h1>{{group.displayName}} - Annual Review for {{year}}</h1>
-        <app-section title="Submitter Information">
-            <submitter-information v-model="annualReview" :errors="errors" />
-        </app-section>
-
-        <transition name="slide-fade-down">
-            <div v-if="group.isVcep() || group.isGcep() && annualReview.ep_activity == 'active' ">
-                <app-section title="Membership">
-                    <p>
-                        Please list the entire membership of the Expert Panel.
-                    </p>
-                    <p v-if="group.isVcep()">
-                        Note: If changes are made to an Expert Panel Co-chair(s) or coordinator, please report them directly to the <a href="cdwg_oversightcommittee@clinicalgenome.org">Clinical Domain Working Group Oversight Committee</a> when they occur. All current EP members must complete a Conflict of Interest (COI) survey each year. If all members of your EP have filled out the EPAM generated COI survey, some of the information will be auto populated.
-                    </p>
-
-                    <member-list />
-
-                    <input-row 
-                        vertical 
-                        label="Please attest that your membership is up to date" 
-                        :errors="errors.membership_attestation"
-                    >
-                        <div class="ml-4">
-                            <radio-button v-model="annualReview.membership_attestation" value="I have reviewed and made the appropriate updates to membership as needed.">
-                                I have reviewed and made the appropriate updates to membership as needed.
-                            </radio-button>
-                            <radio-button v-model="annualReview.membership_attestation" value="I have reviewed and there are no changes needed.">
-                                I have reviewed and there are no changes needed.
-                            </radio-button>
-                        </div>
-                    </input-row>
-                </app-section>
-
-                <template v-if="group.isGcep()">
-                    <app-section title="Use of GCI and GeneTracker Systems">
-                        <gci-gt-use v-model="annualReview" :errors="errors" />
-                    </app-section>
-
-                    <app-section title="Summary of total numbers of genes curated">
-                        <gene-curation-totals v-model="annualReview" :errors="errors" />
-                    </app-section>
-
-                    <app-section title="Changes to plans for ongoing curation">
-                        <gcep-ongoing-plans-update-form v-model="annualReview" :errors="errors" @updated="saveOngoingPlans"></gcep-ongoing-plans-update-form>
-                    </app-section>
-
-                    <app-section title="Gene Re-curation/Re-review">
-                        <gcep-rereview-form v-model="annualReview" :errors="errors"></gcep-rereview-form>
-                    </app-section>
-                </template>
-
-                <app-section v-if="group.isVcep()" title="Use of Variant Curation Interface (VCI)">
-                    <vci-use v-model="annualReview" :errors="errors"></vci-use>
-                </app-section>
-
-                <app-section title="Goals for next year">
-                    <goals-form v-model="annualReview" :errors="errors" />
-                </app-section>
-
-
-                <app-section title="Additional Funding">
-                    <funding-form v-model="annualReview" :errors="errors" />
-                </app-section>
-
-                <app-section title="Webpage Updates">
-                    <input-row :errors="errors.website_attestation" vertical>
-                        <template v-slot:label>
-                            <p>
-                                Please review your ClinGen EP webpage, including Expert Panel Status, description, membership, COI and relevant documentation, including publications. See the <a href="https://docs.google.com/document/d/1GeyR1CBqlzLHOdlPLJt0uA29Z-2ysmTX1dtH9PDmqRo/edit?usp=sharing">Coordinator Resource Document</a> for instructions on how to update web pages.
-                            </p>
-                        </template>
-                        <checkbox 
-                            label="I attest that the information on the webpage is up-to-date and accurate." 
-                            v-model="annualReview.website_attestation"
-                        />
-                    </input-row>
-                </app-section>
-
-                <template v-if="group.isVcep() && group.expert_panel.defIsApproved">
-                    <app-section title="Progress on Rule Specification">
-                        <specification-progress v-model="annualReview" :errors="errors" />
-                    </app-section>
-
-                    <app-section title="Summary of total number of variants curated">
-                        <vcep-totals v-model="annualReview" :errors="errors" />
-                    </app-section>
-                </template>
-
-                <template v-if="group.isVcep() && group.expert_panel.pilotSpecificationsIsApproved">
-                    <app-section title="Changes to plans for variant curation workflow">
-                        <vcep-ongoing-plans-update-form v-model="annualReview" :errors="errors"></vcep-ongoing-plans-update-form>
-                    </app-section>
-                </template>
-
-                <hr>
-                <button class="btn btn-lg" @click="submit">Submit annual update</button>
-                <br>
-            </div>
-        </transition>
-
-
-
-        <teleport to='body'>
-            <modal-dialog v-model="showModal" @closed="handleModalClosed" :title="this.$route.meta.title">
-                <router-view ref="modalView" @saved="hideModal" @canceled="hideModal"></router-view>
-            </modal-dialog>
-        </teleport>
-    </div>
-</template>
 <script>
+import ApplicationSection from '@/components/expert_panels/ApplicationSection'
 import SubmitterInformation from '@/components/annual_review/SubmitterInformation'
 import GciGtUse from '@/components/annual_review/GciGtUse'
 import GeneCurationTotals from '@/components/annual_review/GeneCurationTotals'
-import ApplicationSection from '@/components/expert_panels/ApplicationSection'
 import MemberList from '@/components/groups/MemberList'
 import {debounce} from 'lodash'
 import VcepOngoingPlansUpdateForm from '@/components/annual_review/VcepOngoingPlansUpdateForm'
@@ -132,6 +14,9 @@ import GcepOngoingPlansUpdateForm from '@/components/annual_review/GcepOngoingPl
 import VciUse from '@/components/annual_review/VciUse'
 import SpecificationProgress from '@/components/annual_review/SpecificationProgress'
 import VcepTotals from '@/components/annual_review/VcepTotals'
+import VariantReanalysis from '@/components/annual_review/VariantReanalysis'
+import MemberDesignationUpdate from '@/components/annual_review/MemberDesignationUpdate'
+import VcepPlansForSpecifications from '@/components/annual_review/VcepPlansForSpecifications'
 
 export default {
     name: 'AnnualReviewForm',
@@ -150,6 +35,9 @@ export default {
         VciUse,
         SpecificationProgress,
         VcepTotals,
+        VariantReanalysis,
+        MemberDesignationUpdate,
+        VcepPlansForSpecifications,
     },
     props: {
         uuid: {
@@ -237,8 +125,171 @@ export default {
     }
 }
 </script>
-<style lang="postcss" scoped>
-    .application-section {
-        padding-bottom: 0;
+<template>
+    <div class="annual-review">
+        <router-link class="note"
+            :to="{name: 'GroupDetail', params: {uuid: group.uuid}}"
+            v-if="group.uuid"
+        >
+            {{group.displayName}}
+        </router-link>
+
+        <h1>{{group.displayName}} - Annual Review for {{year}}</h1>
+        <app-section title="Submitter Information">
+            <submitter-information v-model="annualReview" :errors="errors" />
+        </app-section>
+
+        <transition name="slide-fade-down">
+            <div v-if="group.isVcep() || group.isGcep() && annualReview.ep_activity == 'active' ">
+                <app-section title="Membership">
+                    <p>
+                        Please list the entire membership of the Expert Panel.
+                    </p>
+                    <p v-if="group.isVcep()">
+                        Note: If changes are made to an Expert Panel Co-chair(s) or coordinator, please report them directly to the <a href="cdwg_oversightcommittee@clinicalgenome.org">Clinical Domain Working Group Oversight Committee</a> when they occur. All current EP members must complete a Conflict of Interest (COI) survey each year. If all members of your EP have filled out the EPAM generated COI survey, some of the information will be auto populated.
+                    </p>
+
+                    <member-list />
+
+                    <input-row 
+                        vertical 
+                        label="Please attest that your membership is up to date" 
+                        :errors="errors.membership_attestation"
+                    >
+                        <div class="ml-4">
+                            <radio-button v-model="annualReview.membership_attestation" value="I have reviewed and made the appropriate updates to membership as needed.">
+                                I have reviewed and made the appropriate updates to membership as needed.
+                            </radio-button>
+                            <radio-button v-model="annualReview.membership_attestation" value="I have reviewed and there are no changes needed.">
+                                I have reviewed and there are no changes needed.
+                            </radio-button>
+                        </div>
+                    </input-row>
+                </app-section>
+
+                <template v-if="group.isGcep()">
+                    <app-section title="Use of GCI and GeneTracker Systems">
+                        <gci-gt-use v-model="annualReview" :errors="errors" />
+                    </app-section>
+
+                    <app-section title="Summary of total numbers of genes curated">
+                        <gene-curation-totals v-model="annualReview" :errors="errors" />
+                    </app-section>
+
+                    <app-section title="Changes to plans for ongoing curation">
+                        <gcep-ongoing-plans-update-form v-model="annualReview" :errors="errors" @updated="saveOngoingPlans"></gcep-ongoing-plans-update-form>
+                    </app-section>
+
+                    <app-section title="Gene Re-curation/Re-review">
+                        <gcep-rereview-form v-model="annualReview" :errors="errors"></gcep-rereview-form>
+                    </app-section>
+                </template>
+
+                <app-section v-if="group.isVcep()" title="Use of Variant Curation Interface (VCI)">
+                    <vci-use v-model="annualReview" :errors="errors"></vci-use>
+                </app-section>
+
+                <app-section title="Goals for next year">
+                    <goals-form v-model="annualReview" :errors="errors" />
+                </app-section>
+
+
+                <app-section title="Additional Funding">
+                    <funding-form v-model="annualReview" :errors="errors" />
+                </app-section>
+
+                <app-section title="Webpage Updates">
+                    <input-row :errors="errors.website_attestation" vertical>
+                        <template v-slot:label>
+                            <p>
+                                Please review your ClinGen EP webpage, including Expert Panel Status, description, membership, COI and relevant documentation, including publications. See the <a href="https://docs.google.com/document/d/1GeyR1CBqlzLHOdlPLJt0uA29Z-2ysmTX1dtH9PDmqRo/edit?usp=sharing">Coordinator Resource Document</a> for instructions on how to update web pages.
+                            </p>
+                        </template>
+                        <checkbox 
+                            label="I attest that the information on the webpage is up-to-date and accurate." 
+                            v-model="annualReview.website_attestation"
+                        />
+                    </input-row>
+                </app-section>
+
+                <template v-if="group.isVcep() && group.expert_panel.defIsApproved">
+                    <dev-component>Begin questions for specifcation-ed VCEPS</dev-component>
+                    <app-section title="Progress on Rule Specification">
+                        <specification-progress v-model="annualReview" :errors="errors" />
+                    </app-section>
+
+                    <app-section title="Summary of total number of variants curated">
+                        <vcep-totals v-model="annualReview" :errors="errors" />
+                    </app-section>
+                    <dev-component>End questions for specifcation-ed VCEPS</dev-component>
+                </template>
+
+                <template v-if="group.isVcep() && group.expert_panel.pilotSpecificationsIsApproved">
+                    <dev-component>Begin Questions for sustained curation</dev-component>
+                    <app-section title="Changes to plans for variant curation workflow">
+                        <input-row 
+                            v-model="annualReview.variant_workflow_changes"
+                            type="radio-group"
+                            label="Has the Expert Panel has made any changes to its workflow?"
+                            :errors="errors.variant_workflow_changes"
+                            :options="[
+                                {value: 'Yes'},
+                                {value: 'No' }, 
+                            ]"
+                            vertical
+                        ></input-row>
+                        <transition name="slide-fade-down">
+                            <input-row 
+                                v-if="annualReview.variant_workflow_changes == 'Yes'"
+                                v-model="annualReview.variant_workflow_changes_details"
+                                type="large-text"
+                                label="Please explain"
+                                :errors="errors.variant_workflow_changes_details"
+                                vertical
+                                class="ml-4"
+                            ></input-row>
+                        </transition>
+                    </app-section>
+                    
+                    <app-section title="Variant Re-review">
+                        <variant-reanalysis v-model="annualReview" :errors="errors" />
+                    </app-section>
+                    
+                    <app-section title="Sustained Variant Curation">
+                        <vcep-ongoing-plans-update-form v-model="annualReview" :errors="errors" />
+                    </app-section>
+                    
+                    <app-section title="Member Designation">
+                        <member-designation-update v-model="annualReview" :errors="errors" />
+                    </app-section>
+
+                    <vcep-plans-for-specifications 
+                        v-model="annualReview" 
+                        :errors="errors" 
+                    />
+
+                    <dev-component>End Questions for sustained curation</dev-component>
+                </template>
+
+                <hr>
+                <button class="btn btn-lg" @click="submit">Submit annual update</button>
+                <br>
+            </div>
+        </transition>
+
+        <teleport to='body'>
+            <modal-dialog v-model="showModal" @closed="handleModalClosed" :title="this.$route.meta.title">
+                <router-view ref="modalView" @saved="hideModal" @canceled="hideModal"></router-view>
+            </modal-dialog>
+        </teleport>
+    </div>
+</template>
+<style lang="postcss">
+    .annual-review .input-row .label-container{
+        font-size: 1.05em;
+        margin-bottom: .5rem;
+    }
+    .annual-review .application-section {
+        padding-bottom: .25rem;
     }
 </style>
