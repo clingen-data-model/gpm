7be8eda750fc685db25dc3ede4be98c3887ad166
TJ Ward <jward3@email.unc.edu>
2022-02-01 09:11:22 -0500

    Upgrade tailwind.

--- resources/app/tailwind.config.js
+++ resources/app/tailwind.config.js
@@ -45,6 +45,14 @@ module.exports = {
                 lg: '3rem'
             }
         },
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+            'print': {'raw': 'print'}
+        },
     },
     variants: {
         extend: {
