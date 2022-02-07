485833ff7a0daadcbdd646ee0c2cbca2584521b4
TJ Ward <jward3@email.unc.edu>
2022-02-07 13:46:34 -0500

    Allow pending param in Tasks index search.

--- app/Tasks/Http/Controllers/Api/TaskController.php
+++ app/Tasks/Http/Controllers/Api/TaskController.php
@@ -11,7 +11,25 @@ class TaskController
     public function index(Request $request)
     {
         $searchService = new ModelSearchService(
-            modelClass: Task::class
+            modelClass: Task::class,
+            whereFunction: function ($query, $where) {
+                foreach ($where as $key => $value) {
+                    if ($key == 'pending') {
+                        $query->pending();
+                        continue;
+                    }
+                    if ($key == 'completed') {
+                        $query->completed();
+                        continue;
+                    }
+                    if (is_array($value)) {
+                        $query->whereIn($key, $value);
+                    } else {
+                        $query->where($key, $value);
+                    }
+                }
+                return $query;
+            }
         );
 
         return $searchService->search($request->all());
