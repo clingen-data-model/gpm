77d4e947658a64c24127aec7069b8f656b1ef83d
TJ Ward <jward3@email.unc.edu>
2022-02-18 14:37:31 -0500

    Working on UI to resend.

--- resources/app/src/views/MailLog.vue
+++ resources/app/src/views/MailLog.vue
@@ -25,35 +25,70 @@
                     </li>
                 </ul>
             </template>
+            <template v-slot:cell-actions="{item}">
+                <div>
+                    <button class="btn btn-xs" @click="resentEmail(item)">Resend</button>
+                </div>
+            </template>
         </data-table>
 
-        <modal-dialog v-model="showDetail">
-            <dictionary-row label="To" label-class="font-bold" class="mb-1 border-b">
-                <div class="flex-none">
-                    <div v-for="(name, address) in currentEmail.to" :key="address">
+        <teleport to="body">
+            <modal-dialog v-model="showDetail">
+                <dictionary-row label="To" label-class="font-bold" class="mb-1 border-b">
+                    <div class="flex-none">
+                        <div v-for="(name, address) in currentEmail.to" :key="address">
+                            {{address}}
+                        </div>
+                    </div>
+                </dictionary-row>
+                <dictionary-row label="From" label-class="font-bold" class="mb-1 border-b">
+                    <div v-for="(name, address) in currentEmail.from" :key="address">
                         {{address}}
                     </div>
+                </dictionary-row>
+                <dictionary-row label="Cc" label-class="font-bold" class="mb-1 border-b">
+                    <ul>
+                        <li v-for="(name, address) in currentEmail.cc" :key="address">
+                            {{address}}
+                        </li>
+                    </ul>
+                </dictionary-row>
+                <dictionary-row label="Subject" label-class="font-bold" class="mb-1 border-b">
+                    {{currentEmail.subject}}
+                </dictionary-row>
+                <dictionary-row label="Body" label-class="font-bold" class="mb-1">
+                    <div v-html="currentEmail.body" class="email-body w-3/4"></div>
+                </dictionary-row>
+                <div class="mt-2 border-t pt-2 text-right">
+                    <button class="btn " @click="resendEmail(currentEmail)">Resend</button>
                 </div>
-            </dictionary-row>
-            <dictionary-row label="From" label-class="font-bold" class="mb-1 border-b">
-                <div v-for="(name, address) in currentEmail.from" :key="address">
-                    {{address}}
-                </div>
-            </dictionary-row>
-            <dictionary-row label="Cc" label-class="font-bold" class="mb-1 border-b">
-                <ul>
-                    <li v-for="(name, address) in currentEmail.cc" :key="address">
-                        {{address}}
-                    </li>
-                </ul>
-            </dictionary-row>
-            <dictionary-row label="Subject" label-class="font-bold" class="mb-1 border-b">
-                {{currentEmail.subject}}
-            </dictionary-row>
-            <dictionary-row label="Body" label-class="font-bold" class="mb-1">
-                <div v-html="currentEmail.body" class="email-body w-3/4"></div>
-            </dictionary-row>
-        </modal-dialog>
+            </modal-dialog>
+            <modal-dialog title="Resend Email">
+                <dictionary-row label="To">
+                    <ul v-if="group.hasContacts">
+                        <li v-for="to in currentEmail.to" :key="contact.email">
+                            {{contact.email}}&gt;</router-link>
+                    </ul>
+                </dictionary-row>
+                <dictionary-row label="Cc">
+                    <div v-if="email.cc.length > 0">
+                        <truncate-expander :value="ccAddresses" :truncate-length="100"></truncate-expander>
+                    </div>
+                    <div class="text-gray-500" v-else>None</div>
+                </dictionary-row>
+                <input-row label="Subject">
+                    <input type="text" v-model="email.subject" class="w-full">
+                </input-row>
+                <input-row label="Body">
+                    <rich-text-editor  v-model="email.body"></rich-text-editor>
+                </input-row>
+                <input-row label="Attachments">
+                    <input type="file" multiple ref="attachmentsField">
+                </input-row>
+                <note v-if="emailCced">ClinGen Services will be carbon copied on this email.</note>
+
+            </modal-dialog>
+        </teleport>
     </div>
 </template>
 <script>
@@ -108,6 +143,11 @@ export default {
                         return item.created_at;                        
                     },
                     type: String
+                },
+                {
+                    name: 'actions',
+                    label: '',
+                    sortable: false
                 }
             ],
             data: [],
@@ -131,6 +171,12 @@ export default {
         showMailDetail (item) {
             this.currentEmail = item;
             this.showDetail = true;
+        },
+        initResend (item) {
+            this.currentEmail = item;
+        },
+        resend () {
+            
         }
     },
     mounted() {
