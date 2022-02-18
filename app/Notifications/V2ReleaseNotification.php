38d4adb981fd0f8672fe868663713d91037ff332
TJ Ward <jward3@email.unc.edu>
2022-02-17 19:54:21 -0500

    Just making changes I've already made.

--- app/Notifications/V2ReleaseNotification.php
+++ app/Notifications/V2ReleaseNotification.php
@@ -41,7 +41,7 @@ class V2ReleaseNotification extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->subject('Activate your ClinGen Group & Personnel Management account.')
+                    ->subject('Activate your ClinGen Group & Personnel Management account (Corrected).')
                     ->view('email.v2.release_notification', compact('notifiable'));
     }
 
