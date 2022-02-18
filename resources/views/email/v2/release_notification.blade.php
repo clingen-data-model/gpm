1f73be239b1859c625bb5b9d44279aa29c145812
TJ Ward <jward3@email.unc.edu>
2022-02-17 19:46:28 -0500

    Add aplogy for bunk email sent earlier.

--- resources/views/email/v2/release_notification.blade.php
+++ resources/views/email/v2/release_notification.blade.php
@@ -14,18 +14,20 @@
     @else
         You have been invited to join the Clinical Genome Resource's Group and Personnel Management System (GPM).
     @endif
+</p>
 
     <p>The GPM helps ClinGen keep track of expert panel details such as application status, group membership, member profiles, and conflict of interest disclosures.</p>
 
-    Read the <a href="https://docs.google.com/document/d/1adqRiW8UYTOKpIfBPWDcA-tO7I9Q5UGGYHVpTP9n-6E/edit">Frequently Asked Questions document</a> to learn about activiting common activities in the GPM.
+    <p>Read the <a href="https://docs.google.com/document/d/1adqRiW8UYTOKpIfBPWDcA-tO7I9Q5UGGYHVpTP9n-6E/edit">Frequently Asked Questions document</a> to learn about activiting common activities in the GPM.</p>
 
-    To activate your account, please visit 
-    <a href="{{url('/invites/'.$notifiable->invite->code)}}">{{url('/invites/'.$notifiable->invite->code)}}</a>
+    <p></p>To activate your account, please visit 
+        <a href="{{url('/invites/'.$notifiable->invite->code)}}">{{url('/invites/'.$notifiable->invite->code)}}</a>
     and follow the instructions.
-</p>
+    </p>
 
 <p>
     Thanks,<br>
     The ClinGen Team
 </p>
 
+<p>P.S. You may have received an email with the same subject but no body text. If so, we apologize for any confusion this may have caused. Computers are hard.</p>
