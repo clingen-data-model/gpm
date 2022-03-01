704f8d16a0a61e7631789ee50afad6e51dfe5f3e
TJ Ward <jward3@email.unc.edu>
2022-03-01 09:08:06 -0500

    Add back-end to mark institutions approved.

---
+++ app/Modules/Person/Actions/InstitutionMarkApproved.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Modules\Person\Actions;
+
+use Lorisleiva\Actions\ActionRequest;
+use App\Modules\Person\Models\Institution;
+use Lorisleiva\Actions\Concerns\AsController;
+
+class InstitutionMarkApproved
+{
+    use AsController;
+
+    public function handle(Institution $institution): Institution
+    {
+        $institution->update(['approved' => true]);
+
+        return $institution->load("country");
+    }
+    
+    public function authorize(ActionRequest $request): bool
+    {
+        return $request->user()->hasPermissionTo('people-manage');
+    }
+}
---
+++ tests/Feature/End2End/Person/InstitutionMarkApprovedTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\End2End\Person;
+
+use Tests\TestCase;
+use App\Models\Permission;
+use Laravel\Sanctum\Sanctum;
+use App\Modules\Person\Models\Institution;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InstitutionMarkApprovedTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setup():void
+    {
+        parent::setup();
+        Permission::factory()->create(['name' => 'people-manage']);
+        $this->institution = Institution::factory()->create(['approved' => false]);
+        $this->user = $this->setupUser(permissions: ['people-manage']);
+        Sanctum::actingAs($this->user);
+    }
+    
+    /**
+     * @test
+     */
+    public function unprivileged_user_cannot_approve_institution()
+    {
+        $this->user->revokePermissionTo('people-manage');
+        
+        $this->makeRequest()
+            ->assertStatus(403);
+    }
+
+    /**
+     * @test
+     */
+    public function privileged_use_can_approve_institution()
+    {
+        $this->makeRequest()
+            ->assertStatus(200)
+            ->assertJson([
+                'id' => $this->institution->id,
+                'name' => $this->institution->name,
+                'approved' => true
+            ]);
+
+        $this->assertDatabaseHas('institutions', [
+            'id' => $this->institution->id,
+            'approved' => 1
+        ]);
+    }
+    
+    private function makeRequest()
+    {
+        return $this->json('put', '/api/institutions/'.$this->institution->id.'/approved');
+    }
+}
