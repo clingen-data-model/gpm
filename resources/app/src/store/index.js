8f54415dfc75daf7ad6e87149df70183a2829c51
TJ Ward <jward3@email.unc.edu>
2022-03-16 14:01:22 -0400

    Appease the linter and build.

--- resources/app/src/store/index.js
+++ resources/app/src/store/index.js
@@ -32,7 +32,6 @@ axios.interceptors.response.use(
             case 401:
                 store.commit('setAuthenticated', false)
                 return error;
-                break;
             case 403:
                 if (error.response.data.includes('The request to access this resource was rejected.')) {
                     const matches = error.response.data.match(/Reference this support identifier:\s*(\d+)/)
@@ -42,20 +41,17 @@ axios.interceptors.response.use(
                     store.commit('pushError', 'You do not have permission to complete that action.  If you think this is an error please contact support.')
                 }
                 return error;
-                break;
             case 404:
                 if (error.config.headers['X-Ignore-Missing'] !== 1) {
                     store.commit('pushError', 'We couldn\'t find something you\'re looking for.');
                 }
                 return error;
-                break;
 //             case 422:
 //                 store.commit('pushError', 'There was a problem with your submission');
 //                 break
             case 500:
                 store.commit('pushError', 'We\'ve encountered a problem trying to complete your request.  Support has been notified and we will be in touch.');
                 return error;
-                break;
         }
         return Promise.reject(error);
     }
