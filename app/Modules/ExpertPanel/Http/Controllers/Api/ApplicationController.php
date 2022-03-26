b2d76478bb78bdfc9614e80e9bca4bd6a61e6603
TJ Ward <jward3@email.unc.edu>
2022-03-26 08:50:37 -0400

    Use ModelSearchService in ApplicaitonController

--- app/Modules/ExpertPanel/Http/Controllers/Api/ApplicationController.php
+++ app/Modules/ExpertPanel/Http/Controllers/Api/ApplicationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\ExpertPanel\Http\Controllers\Api;
 
+use App\ModelSearchService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -27,61 +28,82 @@ class ApplicationController extends Controller
      */
     public function index(Request $request)
     {
-        $query = ExpertPanel::query()
-                    ->select('expert_panels.*');
-
-        if ($request->has('sort')) {
-            $field = $request->sort['field'];
-            $dir = $request->sort['dir'] ?? 'asc';
-
-            if ($field == 'name') {
-                $query->leftJoin('groups', 'expert_panels.group_id', '=', 'groups.id');
-                $query->orderBy('groups.name', $dir);
-            } elseif ($field == 'cdwg.name') {
-                $query->leftJoin('groups', 'expert_panels.group_id', '=', 'groups.id');
-                $query->leftJoin('groups as parents', 'groups.parent_id', '=', 'parents.id');
-                $query->orderBy('parents.name', $dir);
-            } elseif ($field == 'latestLogEntry.created_at') {
-                $subQuery = DB::table('activity_log')
-                                ->select('subject_id', DB::raw('MAX(created_at) as latest_activity_at'))
-                                ->where('subject_type', ExpertPanel::class)
-                                ->groupBy('subject_id');
-
-                $query->leftJoinSub($subQuery, 'latest_activity', function ($join) {
-                    $join->on('latest_activity.subject_id', '=', 'expert_panels.id');
-                })
-                ->addSelect('latest_activity.latest_activity_at as latest_activity_at')
-                ->orderBy('latest_activity_at', $dir);
-            } else {
-                $query->orderBy($field, $dir);
-            }
-        }
-
-        if ($request->has('with')) {
-            $relations = $request->with;
-            
-            if (is_string($relations)) {
-                $relations = array_map(fn ($i) => trim($i), explode(',', $request->with));
-            }
-
-            $query->with($relations);
-        }
-
-        if ($request->has('where')) {
-            foreach ($request->where as $key => $value) {
-                if ($key == 'since') {
-                    $query->where('updated_at', '>', Carbon::parse($value));
-                    continue;
+        $search = new ModelSearchService(
+            modelClass: ExpertPanel::class,
+            defaultSelect: [
+                'expert_panels.uuid',
+                'expert_panels.id',
+                'expert_panels.long_base_name',
+                'expert_panels.affiliation_id',
+                'expert_panels.expert_panel_type_id',
+                'expert_panels.group_id',
+                'expert_panels.current_step',
+                'step_1_approval_date',
+                'step_1_received_date',
+                'step_2_approval_date',
+                'step_3_approval_date',
+                'step_4_received_date',
+                'step_4_approval_date',
+                'coi_code',
+                'date_completed',
+                'deleted_at'
+            ],
+            defaultWith: [
+                'group' => function ($q) {
+                    $q->select('id', 'parent_id', 'name', 'group_type_id');
+                },
+                'group.members' => function ($q) {
+                    $q->select('id', 'group_id', 'person_id')
+                        ->isActive()
+                        ->whereHas('roles', function ($q) {
+                            $q->where('name', 'coordinator');
+                        });
+                },
+                'group.members.person' => function ($q) {
+                    $q->select('id', 'uuid', 'first_name', 'last_name', 'email');
+                },
+                'group.parent' => function ($q) {
+                    $q->select('id', 'parent_id', 'name', 'group_type_id');
+                },
+                'nextActions',
+                'nextActions.assignee',
+                'group.latestLogEntry' => function ($q) {
+                    $q->select('created_at', 'description', 'subject_id', 'subject_type');
                 }
-                $query->where($key, $value);
+            ],
+            whereFunction: function ($query, $where) {
+                foreach ($where as $key => $val) {
+                    // if ($key == 'showDeleted') {}
+                    $query->where($key, $val);
+                }
+
+                return $query;
+            },
+            sortFunction: function ($query, $field, $dir) {
+                if ($field == 'name') {
+                    $query->orderBy('long_base_name', $dir);
+                } elseif ($field == 'cdwg.name') {
+                    $query->leftJoin('groups', 'expert_panels.group_id', '=', 'groups.id');
+                    $query->leftJoin('groups as parents', 'groups.parent_id', '=', 'parents.id');
+                    $query->orderBy('parents.name', $dir);
+                } elseif ($field == 'latestLogEntry.created_at') {
+                    $subQuery = DB::table('activity_log')
+                                    ->select('subject_id', DB::raw('MAX(created_at) as latest_activity_at'))
+                                    ->where('subject_type', ExpertPanel::class)
+                                    ->groupBy('subject_id');
+
+                    $query->leftJoinSub($subQuery, 'latest_activity', function ($join) {
+                        $join->on('latest_activity.subject_id', '=', 'expert_panels.id');
+                    })
+                    ->addSelect('latest_activity.latest_activity_at as latest_activity_at')
+                    ->orderBy('latest_activity_at', $dir);
+                } else {
+                    $query->orderBy($field, $dir);
+                }
             }
-        }
+        );
 
-        if ($request->has('showDeleted')) {
-            $query->withTrashed();
-        }
-
-        return ExpertPanelResource::collection($query->get());
+        return ExpertPanelResource::collection($search->search($request->all()));
     }
 
     /**
