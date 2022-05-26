fb226dc9823856f0c656bb8ad144a5a1dff064b2
TJ Ward <jward3@email.unc.edu>
2022-05-26 15:06:20 -0400

    Paginate system mail log.

--- resources/app/src/components/mail/MailDetail.vue
+++ resources/app/src/components/mail/MailDetail.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <dictionary-row label="To" label-class="font-bold" class="mb-1 border-b">
+        <dictionary-row label="To" label-class="font-bold w-36" class="mb-1 border-b">
             <div class="flex-none">
                 <div v-for="recipient in mail.to" :key="recipient.address">
                     {{recipient.address}}
                 </div>
             </div>
         </dictionary-row>
-        <dictionary-row label="From" label-class="font-bold" class="mb-1 border-b">
+        <dictionary-row label="From" label-class="font-bold w-36" class="mb-1 border-b">
             <div v-for="recipient in mail.from" :key="recipient.address">
                 {{recipient.address}}
             </div>
         </dictionary-row>
-        <dictionary-row label="Cc" label-class="font-bold" class="mb-1 border-b">
+        <dictionary-row label="Cc" label-class="font-bold w-36" class="mb-1 border-b">
             <note v-if="!mail.cc">--</note>
             <ul v-else>
                 <li v-for="recipient in mail.cc" :key="recipient.address">
@@ -20,7 +20,7 @@
                 </li>
             </ul>
         </dictionary-row>
-        <dictionary-row label="Bcc" label-class="font-bold" class="mb-1 border-b">
+        <dictionary-row label="Bcc" label-class="font-bold w-36" class="mb-1 border-b">
             <note v-if="!mail.bcc">--</note>
             <ul v-else>
                 <li v-for="recipient in mail.bcc" :key="recipient.address">
@@ -28,10 +28,10 @@
                 </li>
             </ul>
         </dictionary-row>
-        <dictionary-row label="Subject" label-class="font-bold" class="mb-1 border-b">
+        <dictionary-row label="Subject" label-class="font-bold w-36" class="mb-1 border-b">
             {{mail.subject}}
         </dictionary-row>
-        <dictionary-row label="Body" label-class="font-bold" class="mb-1">
+        <dictionary-row label="Body" label-class="font-bold w-36" class="mb-1">
             <div v-html="mail.body" class="email-body w-3/4"></div>
         </dictionary-row>
         <div class="mt-2 border-t pt-2 text-right">
