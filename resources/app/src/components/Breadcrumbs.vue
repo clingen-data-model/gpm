3ef79df4c9016531a4ac775ba13b71038babe369
TJ Ward <jward3@email.unc.edu>
2022-07-21 13:44:38 -0400

    Add class to breadcrumps component root.

--- resources/app/src/components/Breadcrumbs.vue
+++ resources/app/src/components/Breadcrumbs.vue
@@ -1,5 +1,5 @@
 <template>
-    <ul class="note">
+    <ul>
         <li v-for="(item, idx) in items" :key="idx" class="inline">
             <router-link :to="item.route" class="note">{{item.label}}</router-link>
             <span v-if="idx !== items.length-1" class="note"> &gt; </span>
