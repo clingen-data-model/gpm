0b37148e13df24d04ab1fe92449f34856bb036fd
TJ Ward <jward3@email.unc.edu>
2022-07-26 11:02:23 -0400

    Fix logic to display approve or request revisions button.

--- resources/app/src/components/dashboard/ApplicationActivity.vue
+++ resources/app/src/components/dashboard/ApplicationActivity.vue
@@ -2,6 +2,7 @@
     import {ref, computed, onMounted} from 'vue'
     import {useRouter} from 'vue-router'
     import {api} from '@/http'
+import {hasPermission} from '../../auth_utils';
 
     const router = useRouter();
 
@@ -39,7 +40,7 @@
             label: '',
             sortable: false
         }
-    ]
+    ];
 
     const sort = ref({field: 'name', desc: false})
 
@@ -55,6 +56,23 @@
                 uuid: group.uuid
             }
         });
+    }
+
+    const hasMadeJudgementOn = group => {
+        return group.submission.judgements
+            && group.submission.judgements.some(j => j.person_id == props.user.person.id);
+    }
+
+    const judgementFor = group => {
+        if (!group.submission.judgements) return {};
+        
+        return group.submission.judgements.find(j => j.person_id == props.user.person.id);
+    }
+
+    const judgementColor = group => {
+        if (!group.submission.judgements) return 'gray';
+        const decToColor = {'approved': 'green', 'approve-after-revisions': 'blue', 'request-revisions': 'yellow'}
+        return decToColor[judgementFor(group).decision];
     }
 
     onMounted(() => {
@@ -75,9 +93,41 @@
             row-class="cursor-pointer"
         >
             <template v-slot:cell-actions="{item}">
-                <router-link :to="{name: 'ApplicationDetail', params: {uuid: item.uuid}}">
-                    {{hasPermission('ep-applications-manage') ? 'View' : 'Review'}}
-                </router-link>
+                <div class="flex space-x-2">
+                    <div v-if="hasMadeJudgementOn(item)">
+                        <popper hover arrow placement="right">
+                            <badge :color="judgementColor(item)">
+                                <icon-checkmark class="text-white inline-block" width="12" height="12"></icon-checkmark>
+                            </badge>
+
+                            <template v-slot:content>
+                                <div>
+                                    <h3 class="mb-2">Your Decision</h3>
+                                    <badge class="inline-block" :color="judgementColor(item)" size="xs">
+                                        {{judgementFor(item).decision}}
+                                    </badge>
+                                </div>
+                                <div v-if="judgementFor(item).notes" class="mt-2" style="max-width: 300px">
+                                    <strong>Notes:</strong> {{judgementFor(item).notes}}
+                                </div>
+                                <hr>
+                                <router-link
+                                    :to="{name: 'ApplicationDetail', params: {uuid: item.uuid}}" 
+                                    :class="{'btn btn-xs': hasPermission('ep-applications-approve')}"
+                                >
+                                    {{'Review'}}
+                                </router-link>
+
+                            </template>
+                        </popper>
+                    </div>
+                    <router-link v-else
+                        :to="{name: 'ApplicationDetail', params: {uuid: item.uuid}}" 
+                        class="btn btn-xs"
+                    >
+                        {{hasPermission('ep-applications-manage') ? 'View' : 'Review'}}
+                    </router-link>
+                </div>
             </template>
         </data-table>
     </div>        
