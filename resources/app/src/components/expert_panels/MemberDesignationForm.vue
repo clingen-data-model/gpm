0cce710fb15e772b034636b64dc94363f29361a9
TJ Ward <jward3@email.unc.edu>
2022-09-20 11:57:38 -0400

    Reorde vue file objects.

--- resources/app/src/components/expert_panels/MemberDesignationForm.vue
+++ resources/app/src/components/expert_panels/MemberDesignationForm.vue
@@ -1,60 +1,3 @@
-<style scoped>
-    th {
-        @apply bg-white border-0
-    }
-    th[colgroup=biocurator],
-    td[colgroup=biocurator]
-    {
-        @apply bg-blue-50;
-    }
-</style>
-<template>
-    <div>
-        <table class="text-sm">
-            <thead>
-                <tr>
-                    <th colspan="2">&nbsp;</th>
-                    <th colspan="1" colgroup="biocurator">&nbsp;</th>
-                    <th colspan="2" colgroup="biocurator" class="text-center">Training</th>
-                    <th colspan="2">&nbsp;</th>
-                </tr>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th colgroup="biocurator" class="w-24">Biocurator</th>
-                    <th colgroup="biocurator" class="w-20">Level 1</th>
-                    <th colgroup="biocurator" class="w-20">Level 2</th>
-                    <th>Biocurator Trainer</th>
-                    <th>
-                        Core Approval Member
-                        <popover
-                            arrow
-                            content="Core approval members are responsible for ongoing final approval of variant classifications."
-                            hover
-                        >
-                            <icon-info class="inline-block cursor-pointer" :width="16" :height="16" />
-                        </popover>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <member-designation-row
-                    :member="member"
-                    v-for="member in group.members"
-                    :key="member.id"
-                    :ref="`memberRow${member.id}`"
-                    :readonly="readonly"
-                    @updated="handleMemberUpdate"
-                />
-            </tbody>
-        </table>
-        <!-- <div class="flex items-center mt-2 pt-2 border-t">
-            <icon-info :width="14" :height="14" />
-            &nbsp;
-            <div>Core approval members are responsible for ongoing final approval of variant classifications.</div>
-        </div> -->
-    </div>
-</template>
 <script>
 import MemberDesignationRow from './MemberDesignationRow.vue'
 export default {
@@ -103,3 +46,60 @@ export default {
     },
 }
 </script>
+<template>
+    <div>
+        <table class="text-sm">
+            <thead>
+                <tr>
+                    <th colspan="2">&nbsp;</th>
+                    <th colspan="1" colgroup="biocurator">&nbsp;</th>
+                    <th colspan="2" colgroup="biocurator" class="text-center">Training</th>
+                    <th colspan="2">&nbsp;</th>
+                </tr>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th colgroup="biocurator" class="w-24">Biocurator</th>
+                    <th colgroup="biocurator" class="w-20">Level 1</th>
+                    <th colgroup="biocurator" class="w-20">Level 2</th>
+                    <th>Biocurator Trainer</th>
+                    <th>
+                        Core Approval Member
+                        <popover
+                            arrow
+                            content="Core approval members are responsible for ongoing final approval of variant classifications."
+                            hover
+                        >
+                            <icon-info class="inline-block cursor-pointer" :width="16" :height="16" />
+                        </popover>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <member-designation-row
+                    :member="member"
+                    v-for="member in group.members"
+                    :key="member.id"
+                    :ref="`memberRow${member.id}`"
+                    :readonly="readonly"
+                    @updated="handleMemberUpdate"
+                />
+            </tbody>
+        </table>
+        <!-- <div class="flex items-center mt-2 pt-2 border-t">
+            <icon-info :width="14" :height="14" />
+            &nbsp;
+            <div>Core approval members are responsible for ongoing final approval of variant classifications.</div>
+        </div> -->
+    </div>
+</template>
+<style scoped>
+    th {
+        @apply bg-white border-0
+    }
+    th[colgroup=biocurator],
+    td[colgroup=biocurator]
+    {
+        @apply bg-blue-50;
+    }
+</style>
