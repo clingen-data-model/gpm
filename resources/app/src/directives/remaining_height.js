6b3b954f813fcdc4791fe2b7b9d93e48a2ac12fa
TJ Ward <jward3@email.unc.edu>
2022-10-11 12:33:22 -0400

    Add vue2 support fro remaining height. document.

--- resources/app/src/directives/remaining_height.js
+++ resources/app/src/directives/remaining_height.js
@@ -1,5 +1,27 @@
+/**
+ * Simple remaining height directive for vuejs.
+ *
+ * Registration:
+ * import remainingHeight from 'path/to/js'
+ * V2: Vue.directive(remainingHeight)
+ * V3: vue.createApp().directive(remainingHeight)
+ *
+ * Usage:
+ * <div v-remaining-height="500">...</div>
+ *
+ * Accepts a integer value to set the offset.
+ */
 export default {
+    // For Vue3
     mounted (el, binding) {
+        const rect = el.getBoundingClientRect();
+        const elementOffset = rect.top;
+        let offset = (binding.value) ? binding.value : (elementOffset+36);
+        el.style.height = `calc(100vh - ${offset}px)`
+        el.style.overflow = 'auto'
+    },
+    // For Vue2
+    bind (el, binding) {
         const rect = el.getBoundingClientRect();
         const elementOffset = rect.top;
         let offset = (binding.value) ? binding.value : (elementOffset+36);
