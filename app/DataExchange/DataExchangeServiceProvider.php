65250f71a5df30c43de3f16d00c6007828c46b53
TJ Ward <jward3@email.unc.edu>
2022-10-12 16:34:55 -0400

    Update dx event listeners

--- app/DataExchange/DataExchangeServiceProvider.php
+++ app/DataExchange/DataExchangeServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\DataExchange;
 use ReflectionClass;
 use Illuminate\Console\Command;
 use Lorisleiva\Actions\Facades\Actions;
+use App\DataExchange\Actions\MessagePush;
 use App\DataExchange\Listeners\PushMessage;
 use App\DataExchange\Contracts\MessagePusher;
 use App\DataExchange\Contracts\MessageStream;
@@ -12,15 +13,15 @@ use App\DataExchange\Kafka\KafkaMessageStream;
 use App\DataExchange\Contracts\MessageProcessor;
 use App\DataExchange\Actions\IncomingMessageStore;
 use App\DataExchange\Actions\IncomingMessageProcess;
-use App\DataExchange\MessageFactories\MessageFactoryInterface;
 use App\DataExchange\MessagePushers\MessagePusherFactory;
+use App\DataExchange\MessageFactories\MessageFactoryInterface;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class DataExchangeServiceProvider extends ServiceProvider
 {
     protected $listen = [
         \App\DataExchange\Events\Created::class => [
-            PushMessage::class
+            MessagePush::class
         ],
         \App\DataExchange\Events\Received::class => [
             IncomingMessageStore::class,
@@ -35,10 +36,6 @@ class DataExchangeServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
-
-        if ($this->app->runningInConsole()) {
-            $this->loadCommands(__DIR__.'/Commands');
-        }
 
         $this->bindMessageClasses();
 
@@ -55,23 +52,4 @@ class DataExchangeServiceProvider extends ServiceProvider
         $this->app->bind(MessageProcessor::class, IncomingMessageProcess::class);
     }
 
-    /**
-     * Register all of the commands in the given directory.
-     *
-     * @param  array|string  $paths
-     * @return void
-     */
-    protected function loadCommands($paths)
-    {
-        $commands = array_filter(
-                        getClassesInPaths($paths, $this->app->getNamespace()),
-                        function ($class) {
-                            if (is_subclass_of($class, Command::class) &&
-                                ! (new ReflectionClass($class))->isAbstract()) {
-                                $commands[] = $class;
-                            }
-                        }
-                    );
-        $this->commands($commands);
-    }
 }
