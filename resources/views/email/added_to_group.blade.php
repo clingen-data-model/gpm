f61c064a2ece918263a00839c8b213529250e513
TJ Ward <jward3@email.unc.edu>
2022-10-25 22:20:08 -0400

    Fix added_to_group email template.

--- resources/views/email/added_to_group.blade.php
+++ resources/views/email/added_to_group.blade.php
@@ -5,10 +5,10 @@
 
 <p>
 @if ($group->isEp)
-    Please complete your 
-    <a href="{{url('/coi/'.$group->expertPanel->coi_code)}}">
+    Please complete your
+    <a href="{{url('/coi/'.$group->coi_code)}}">
         Conflict of Interest Disclosure
-    </a> 
+    </a>
     for this group.
 @else
     You can see more details in the <a href="{{url('/')}}">ClinGen Group &amp; Personnel Management System</a>.
@@ -22,19 +22,19 @@
 
     @if ($group->isEp)
         <p>
-            Before you can complete the COI for {{$group->displayName}} you'll need to 
-            <a href="{{$notifiable->invite->url}}">activate your ClinGen GPM account</a>.      
+            Before you can complete the COI for {{$group->displayName}} you'll need to
+            <a href="{{$notifiable->invite->url}}">activate your ClinGen GPM account</a>.
             After activating your account you'll see alerts for your pending COI disclosure(s) on your dashboard.
         </p>
     @else
         <p>
-           Before you can access {{$group->displayName}} in the GPM you'll need to 
+           Before you can access {{$group->displayName}} in the GPM you'll need to
             <a href="{{$notifiable->invite->url}}">activate your ClinGen GPM account</a>.
         </p>
     @endif
 
     <p>
-        For more information about the GPM, account activation, and COI disclosures see the 
+        For more information about the GPM, account activation, and COI disclosures see the
         <a href="https://docs.google.com/document/d/1adqRiW8UYTOKpIfBPWDcA-tO7I9Q5UGGYHVpTP9n-6E/edit">GPM FAQ</a>.
     </p>
 
@@ -42,18 +42,18 @@
 
 <p>
 @if ($group->hasCoordinator)
-    If you have questions about your assignment to {{$group->displayName}} 
-    please contact 
+    If you have questions about your assignment to {{$group->displayName}}
+    please contact
     @if ($group->coordinators->count() > 1)
         one of the coordinators:
         <ul>
-        @foreach($group->coordinators as $coordinator) 
+        @foreach($group->coordinators as $coordinator)
             <li><a href="mailto:{{$coordinator->person->email}}">{{$coordinator->person->name}}</a></li>
         @endforeach
         </ul>
     @else
     the coordinator, <a href="mailto:{{$group->coordinators->first()->person->email}}">{{$group->coordinators->first()->person->name}}</a>
-    @endif 
+    @endif
 @endif
 
 </p>
