c49c621af1847f82751573a44b3b429b5f448da7
Bradford Powell <bpow@drpowell.org>
2023-03-13 16:46:02 -0400

    add report for COI statements that are due

---
+++ app/Actions/ReportCOIDueMake.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Actions;
+use Lorisleiva\Actions\ActionRequest;
+use App\Modules\Person\Models\Person;
+use Lorisleiva\Actions\Concerns\AsController;
+
+class ReportCOIDueMake extends ReportMakeAbstract
+{
+
+    public $commandSignature = 'reports:coi-due';
+    public function handle(): array
+    {
+        return Person::query()
+            ->isActivatedUser()
+            ->hasPendingCois()
+            ->with('membershipsWithPendingCoi', 'membershipsWithPendingCoi.group')
+            ->get()
+            ->map(function($p) {
+                return [
+                    'first_name' => $p->first_name,
+                    'last_name' => $p->last_name,
+                    'email' => $p->email,
+                    'membership' => $p->membershipsWithPendingCoi->map(function ($m) { return $m->group->name; })
+                ];
+        })
+        ->toArray();
+    }
+}
