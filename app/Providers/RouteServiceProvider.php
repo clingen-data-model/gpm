af8a007e968c47eac69e6073b7c7fb420236a81a
Shift <shift@laravelshift.com>
2023-06-23 23:52:58 +0000

    Adopt class based routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -20,15 +20,6 @@ class RouteServiceProvider extends ServiceProvider
     public const HOME = '/home';
 
     /**
-     * The controller namespace for the application.
-     *
-     * When present, controller route declarations will automatically be prefixed with this namespace.
-     *
-     * @var string|null
-     */
-    // protected $namespace = 'App\\Http\\Controllers';
-
-    /**
      * Define your route model bindings, pattern filters, etc.
      *
      * @return void
@@ -41,7 +32,6 @@ class RouteServiceProvider extends ServiceProvider
             // API Routes
             Route::prefix('api')
                 ->middleware('api')
-                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             // Admin Routes
@@ -50,7 +40,6 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/dev.php'));
 
             Route::middleware('web')
-                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
