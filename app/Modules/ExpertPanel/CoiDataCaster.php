a6fa09d3fd40e0fb10fcff5bdbaeef1d78597654
Shift <shift@laravelshift.com>
2023-10-15 18:00:24 +0000

    Update method signatures for Laravel contracts

--- app/Modules/ExpertPanel/CoiDataCaster.php
+++ app/Modules/ExpertPanel/CoiDataCaster.php
@@ -2,17 +2,18 @@
 
 namespace App\Modules\ExpertPanel;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use InvalidArgumentException;
 
 class CoiDataCaster implements CastsAttributes
 {
-    public function get($model, $key, $value, $attributes): CoiData
+    public function get(Model $model, string $key, mixed $value, array $attributes): CoiData
     {
         return new CoiData(json_decode($value));
     }
 
-    public function set($model, $key, $value, $attributes): string
+    public function set(Model $model, string $key, mixed $value, array $attributes): string
     {
         // if (! $value instanceof CoiData) {
         //     throw new InvalidArgumentException('The given value is not an CoiData object.');
