810f7f10181170f39ef786cd18d27998907a5ad4
TJ Ward <yugen@ibiblio.org>
2023-12-11 12:07:51 -0500

    Remove what were failing, but duplicate tests.

--- tests/Feature/End2End/ExpertPanels/SimpleCoiEndpointTest.php
+++ tests/Feature/End2End/ExpertPanels/SimpleCoiEndpointTest.php
@@ -10,7 +10,9 @@ use Laravel\Sanctum\Sanctum;
 use App\Modules\User\Models\User;
 use Illuminate\Http\UploadedFile;
 use App\Modules\Group\Models\Group;
+use App\Modules\Person\Models\Person;
 use App\Modules\ExpertPanel\Models\Coi;
+use App\Modules\Group\Actions\MemberAdd;
 use App\Modules\Group\Models\GroupMember;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Modules\ExpertPanel\Models\ExpertPanel;
@@ -23,12 +25,18 @@ class SimpleCoiEndpointTest extends TestCase
 {
     use RefreshDatabase;
 
+    private $group;
+    private $groupMember;
+
     public function setup():void
     {
         parent::setup();
         $this->setupForGroupTest();
 
         $this->group = Group::factory()->create();
+        $user = $this->setupUserWithPerson();
+        $this->groupMember = GroupMember::factory()->create(['group_id' => $this->group, 'person_id' => $user->person]);
+        $this->actingAs($user);
     }
 
     /**
@@ -37,8 +45,8 @@ class SimpleCoiEndpointTest extends TestCase
     public function can_get_group_from_coi_code()
     {
         $this->json('GET', '/api/coi/'.$this->group->coi_code.'/group')
-        ->assertStatus(200)
-        ->assertJson($this->group->toArray());
+            ->assertStatus(200)
+            ->assertJson(['id' => $this->group->id, 'name' => $this->group->name]);
     }
 
     /**
@@ -47,86 +55,12 @@ class SimpleCoiEndpointTest extends TestCase
     public function returns_404_if_group_not_found_for_code()
     {
         $this->json('GET', '/api/coi/some-fake-code/group')
-        ->assertStatus(404);
+            ->assertStatus(404);
     }
 
-    /**
-     * @test
-     */
-    public function validates_coi_response_data()
+    private function signInAsMember()
     {
-        $this->json('POST', '/api/coi/'.$this->group->coi_code, [])
-            ->assertStatus(422)
-            ->assertJsonFragment(['group_member_id' => ['Storing a COI requires a group member id']])
-            ->assertJsonFragment(['work_fee_lab' => ['This field is required.']])
-            ->assertJsonFragment(['contributions_to_gd_in_ep' => ['This field is required.']])
-            ->assertJsonFragment(['independent_efforts' => ['This field is required.']])
-            ->assertJsonFragment(['coi' => ['This field is required.']]);
-
-        $this->json('POST', '/api/coi/'.$this->group->coi_code, ['contributions_to_gd_in_ep' => 1])
-            ->assertStatus(422)
-            ->assertJsonFragment(['contributions_to_genes' => ['This field is required.']]);
-    }
-
-    /**
-     * @test
-     */
-    public function stores_valid_coi_response()
-    {
-        $groupMember = GroupMember::factory()->create();
-        $data = [
-            'group_member_id' => $groupMember->id,
-            'work_fee_lab' => 0,
-            'contributions_to_gd_in_ep' => 1,
-            'contributions_to_genes' => 'beans',
-            'independent_efforts' => 'None',
-            'coi' => 'no coi',
-        ];
-
-
-        $this->json('POST', '/api/coi/'.$this->group->coi_code, $data)
-        ->assertStatus(200);
-
-        unset($data['group_member_id']);
-
-        $this->assertDatabaseHas('cois', [
-            'group_member_id' => $groupMember->id,
-            'group_id' => $this->group->id,
-            'data->work_fee_lab' => 0,
-            'data->contributions_to_gd_in_ep' => 1,
-            'data->contributions_to_genes' => 'beans',
-            'data->independent_efforts' => 'None',
-            'data->coi' => 'no coi'
-        ]);
-    }
-
-    /**
-     * @test
-     */
-    public function stores_legacy_coi()
-    {
-        $document = Document::factory()->create([
-            'owner_id' => $this->group->id,
-            'owner_type' => get_class($this->group),
-            'document_type_id' => config('documents.types.coi.id')
-        ]);
-
-        $data = [
-            "download_url" => "http://localhost:8080/documents/".$document->uuid,
-            "document_uuid" => $document->uuid
-        ];
-        $this->json('POST', '/api/coi/'.$this->group->coi_code, $data)
-            ->assertStatus(200);
-
-
-        $expectedData = array_merge($data, [
-            'email' => 'Legacy Coi',
-            'first_name' => 'Legacy',
-            'last_name' => 'Coi'
-        ]);
-        $this->assertDatabaseHas('cois', [
-            'group_id' => $this->group->id,
-            'data' => json_encode($expectedData)
-        ]);
+        $user = $this->loginWithPerson();
+        (new (app(MemberAdd::class)))()->handle($this->group, $user->person);
     }
 }
