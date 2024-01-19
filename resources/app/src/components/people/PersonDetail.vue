6b0626cf21e8e92c1b1441ae9dca93a5d16d407b
tmbattey-2021 <mellbattey@gmail.com>
2024-01-18 23:03:56 -0500

    Demographics api successful.  Updates first name field.  Need to add new fields to the person object definiton.

--- resources/app/src/components/people/PersonDetail.vue
+++ resources/app/src/components/people/PersonDetail.vue
@@ -118,6 +118,7 @@ import { logEntries, fetchEntries } from "@/adapters/log_entry_repository";
 import TabsContainer from '../TabsContainer.vue'
 import MembershipList from './MembershipList.vue'
 import PersonProfile from '@/components/people/PersonProfile.vue'
+//import DemographicsForm from '@/components/people/DemographicsForm.vue'
 import PersonDemographics from '@/components/people/PersonDemographics.vue'
 import PersonMergeForm from '@/components/people/PersonMergeForm.vue'
 import CoiList from '@/components/people/CoiList.vue'
@@ -132,6 +133,7 @@ export default {
         TabsContainer,
         MembershipList,
         PersonProfile,
+        //DemographicsForm,
         PersonDemographics,
         PersonMergeForm,
         CoiList,
