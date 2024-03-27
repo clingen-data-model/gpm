377dca9d04e26ea95c57243fa03db8f8b39242e2
tmbattey-2021 <tmbattey@proton.me>
2024-03-26 20:55:48 -0400

    initial commit of routing for initial demographics form

---
+++ resources/app/src/router/people_new.js
@@ -0,0 +1,113 @@
+import router from '.'
+import store from '../store/index'
+import { checkPermissionAndPersonOwnership } from './route_guards'
+
+const PersonDetail = () => import (/* person-detail */ '@/components/people/PersonDetail.vue')
+const PersonEdit = () => import (/* person-detail */ '@/views/PersonEdit.vue')
+const PeopleList = () => import (/* people-index */ '@/views/PeopleList.vue')
+const OnboardingWizard = () => import (/* onboarding-wizard */ '@/views/OnboardingWizard.vue')
+const RequiredProfileUpdate = () => import (/* required-profile-update */ '@/views/RequiredProfileUpdate.vue')
+const DemographicsForm = () => import (/* person-detail */ '@/components/people/DemographicsFormOptionsFinal.vue')
+//const DemographicsForm = () => import (/* person-detail */ '@/views/OnboardingDemographicsForm.vue')
+
+export default [
+    { name: 'people-index',
+            path: '/people',
+            component: PeopleList,
+            meta: {
+                protected: true
+            },
+    },
+    { name: 'PersonDetail',
+        path: '/people/:uuid',
+        component: PersonDetail,
+        props: true,
+        meta: {
+            protected: true
+        },
+        children: [
+            { name: 'PersonEdit',
+                path: 'edit',
+                components: {
+                    default: PersonDetail,
+                    modal: PersonEdit
+                },
+                meta: {
+                    showModal: true,
+                    protected: true,
+                    title: 'Update Information'
+                },
+                props: true,
+                beforeEnter: [checkPermissionAndPersonOwnership]
+            }
+        ]
+    },
+    {
+        name: 'InviteWithCode',
+        path: '/invites/:code',
+        component: OnboardingWizard,
+        beforeEnter: () => {
+            if (store.getters.currentUser.id !== null) {
+                router.replace({name: 'Dashboard'});
+            }
+            return true;
+        },
+        meta: {
+            protected: false
+        },
+        props: true
+    },
+    { name: 'RedeemInvite',
+        path: '/invites',
+        component: OnboardingWizard,
+        beforeEnter: () => {
+            if (store.getters.currentUser.id !== null) {
+                router.replace({name: 'Dashboard'});
+            }
+            return true;
+        },
+        meta: {
+            protected: false
+        }
+    },
+    { name: 'InitialProfileForm',
+        path: '/onboarding/profile',
+        component: () => import (/* onboarding */ '@/views/OnboardingProfileForm.vue')
+    },
+
+   { name: 'DemograaphicsForm',
+        path: '/demographics',
+        component: () => import (/* onboarding */ '@/views/OnboardingDemographicsForm.vue')
+      // component: () => import (/* onboarding */ '@/components/people/DemographicsFormOptionsFinal.vue')
+    },
+
+  //  {   name: 'DemographicsForm',
+   //     path: '/demographics/',
+   //     path: '/demographics/:uuid',
+   //     props: true,
+       // component: DemographicsForm
+       // component: DemographicsForm
+     //  component: () =>
+     // ( /* webpackChunkName: "application-summary" */ '@/views/OnboardingDemographicsForm.vue'),
+     //( /* webpackChunkName: "application-summary" */ '@/components/people/DemographicsFormOptionsFinal.vue'),
+    //  },
+
+   //    {   name: 'DemographicsForm',
+    //    path: '/demographics/:uuid',
+  //      path: '/demographics/:uuid',
+   //     props: true,
+       // component: DemographicsForm
+       // component: DemographicsForm
+   //    component: () =>
+    // ( /* webpackChunkName: "application-summary" */ '@/views/OnboardingDemographicsForm.vue'),
+    //   },
+
+
+    { name: 'MandatoryProfileUpdate',
+      path: '/required-profile-update',
+      component: RequiredProfileUpdate,
+      meta: {
+        protected: true
+      }
+    },
+]
---
+++ resources/app/src/views/OnboardingDemographicsForm.vue
@@ -0,0 +1,35 @@
+<script setup>
+    import {useStore} from 'vuex'
+    import {useRouter} from 'vue-router'
+    import DemographicsForm from '../components/people/DemographicsFormOptionsFinal.vue';
+
+    const store = useStore();
+    const router = useRouter();
+
+    const props = defineProps({
+        redirectTo: {
+            type: Object,
+            default: () => ({name: 'Dashboard'})
+        }
+    })
+
+    const handleSave = async () => {
+        await store.dispatch('forceGetCurrentUser');
+
+        router.replace(props.redirectTo)
+    }
+
+</script>
+<template>
+    <div>
+        <card title="Please fill out your Demographics profile">
+            <DemographicsForm
+                :uuid="store.getters.currentuser.person.uuid"
+                :person="store.getters.currentUser.person"
+                :allowCancel="false"
+                :showTitle="false"
+                @saved="handleSave"
+            />
+        </card>
+    </div>
+</template>
