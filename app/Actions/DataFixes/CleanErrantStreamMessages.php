0a0c71633130cc7f589e379fd3f8f1a3f2443e9c
TJ Ward <yugen@ibiblio.org>
2024-05-02 09:32:41 -0400

    Get errant messages based on attributes rather than id.

--- app/Actions/DataFixes/CleanErrantStreamMessages.php
+++ app/Actions/DataFixes/CleanErrantStreamMessages.php
@@ -8,10 +8,28 @@ class CleanErrantStreamMessages
 {
     public function handle()
     {
+        
         // Delete errant member_retired for Aurora Pujol on group Peroxisomal Disorders VCEP
-        StreamMessage::find(4034)->delete();
+        $aPujolMemberRetired = StreamMessage::where([
+            'message->event_type' => 'member_retired',
+            'message->data->expert_panel->affiliation_id' => '50049',
+            'message->data->members[0]->email' => 'apujol@IDIBELL.CAT',
+        ])->first();
+
+        if ($aPujolMemberRetired) {
+            $aPujolMemberRetired->delete();
+        }
+
 
         // Delete errant member_removed for Bo Yuan on group SCID VCEP
-        StreamMessage::find(2640)->delete();
+        $bYuanMemberRemoved = StreamMessage::where([
+            'message->event_type' => 'member_removed',
+            'message->data->expert_panel->affiliation_id' => '50091',
+            'message->data->members[0]->email' => 'by2@bcm.edu',
+        ])->first();
+
+        if ($bYuanMemberRemoved) {
+            $bYuanMemberRemoved->delete();
+        }
     }
 }
