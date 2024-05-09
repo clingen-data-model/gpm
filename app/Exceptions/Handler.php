9bc1a3784b49c0a7ab2f20f50c445c9aae416cff
tmbattey-2021 <tmbattey@proton.me>
2024-05-09 13:50:26 -0400

    changed location of edit button. added error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -37,4 +37,31 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    // app/Exceptions/Handler.php
+    public function render($request, Throwable $exception)
+{
+    if ($exception instanceof ModelNotFoundException) {
+        return response()->json([
+            'message' => 'Resource not found'
+        ], 404);
+    }
+
+    if ($exception instanceof AuthenticationException) {
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
+    }
+
+    if ($exception instanceof \Exception) {
+        return response()->json([
+            'message' => 'Internal Server Error',
+            'error' => $exception->getMessage(), // You can omit this line in production
+            'code' => 500
+        ], 500);
+    }
+
+    return parent::render($request, $exception);
+}
+
 }
