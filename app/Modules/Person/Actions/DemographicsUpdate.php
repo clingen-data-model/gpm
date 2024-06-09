108a46a5fce8e1188217070abee6d303d8d3768a
Bradford Powell <bpow@unc.edu>
2024-06-09 13:55:43 -0400

    removed some unused code, marked some TODO/FIXMEs, fixed some spacing

--- app/Modules/Person/Actions/DemographicsUpdate.php
+++ app/Modules/Person/Actions/DemographicsUpdate.php
@@ -2,113 +2,55 @@
 
 namespace App\Modules\Person\Actions;
 
-use DateTimeZone;
-use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
 use App\Modules\Person\Models\Person;
 use Illuminate\Support\Facades\Event;
 use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsObject;
 use App\Modules\Person\Events\ProfileUpdated;
-use App\Modules\Person\Policies\PersonPolicy;
 use Lorisleiva\Actions\Concerns\AsController;
-use App\Modules\Person\Actions\PersonExpertisesSync;
-use App\Modules\Person\Actions\PersonCredentialsSync;
-use App\Modules\Person\Http\Requests\ProfileUpdateRequest;
-use App\Modules\Person\Http\Requests\DemographicsUpdateRequest;
+
+// FIXME: should actually handle validation with rules, etc. A lot of this was just copied from PersonUpdate without modificaiton
 
 class DemographicsUpdate
 {
     use AsObject;
     use AsController;
 
-    public function __construct(
-        private PersonCredentialsSync $personSyncCredentials,
-        private PersonExpertisesSync $personSyncExpertises
-    )
-    {
-    }
-
-
     public function handle(Person $person, array $data)
     {
         $person->update($data);
 
-        if ($person->user_id) {
-            $person->user->update([
-                'name' => $person->first_name. ' '.$person->last_name,
-                'email' => $person->email
-            ]);
-        }
-
-        if (isset($data['credential_ids'])) {
-            $person = $this->personSyncCredentials->handle($person, $data['credential_ids']);
-        }
-
-        if (isset($data['expertise_ids'])) {
-            $person = $this->personSyncExpertises->handle($person, $data['expertise_ids']);
-        }
-
         Event::dispatch(new ProfileUpdated($person, $data));
 
 
-       return response('The demographics update was successful.', 200)
-              ->header('Content-Type', 'text/plain');
+        return response('The demographics update was successful.', 200)
+            ->header('Content-Type', 'text/plain');
     }
 
     public function asController(ActionRequest $request, Person $person)
     {
         $demoData = $request->only(['id']);
+        // TODO: should this check for advanced logic regarding admins/impersonation?
         if ($request->user()->can('update', $person)) {
             $demoData = $request->all();
         }
         $person = $this->handle($person, $demoData);
 
-     //   $person->setDemographicHiddenFields($request->user());
-
-
-        //$person->load(
-         //   'institution',
-        //    'credentials',
-         //   'expertises',
-         //   'primaryOccupation',
-         //   'race',
-          //  'ethnicity',
-          //  'gender',
-          //  'country'
-        //);
         return $person;
     }
 
     public function authorize(ActionRequest $request): bool
     {
+        // TODO: should this check for advanced logic regarding admins/impersonation?
         return $request->user()->can('update', $request->person)
             || $request->user()->can('updateNameAndEmail', $request->person);
     }
 
     public function rules(ActionRequest $request)
     {
+        // FIXME: this relates to code that is part of PersonUpdate, but not DemographicsUpdate
         $rules = [
-           // 'email' => [
-           //     'required',
-           //     'email',
-           //     Rule::unique('people', 'email')
-           //         ->ignore($request->person->id),
-            //    Rule::unique('users', 'email')
-           //         ->ignore($request->person->user_id),
-          //  ],
-          //  'first_name' => ['required','max:255'],
-          //  'last_name' => ['required','max:255'],
-           // 'institution_id' => ['exists:institutions,id'],
-           // 'credential_ids' => ['array'],
-           // 'credential_ids.*' => ['exists:credentials,id'],
-           // 'expertise_ids' => ['array'],
-            //'expertise_ids.*' => ['exists:expertises,id'],
-            // 'race_id' => ['exists:races,id'],;
-            // 'primary_occupation_id' => ['exists:primary_occupations,id'],
-            // 'gender_id' => ['exists:genders,id'],
-           // 'country_id' => ['exists:countries,id'],
-            //'timezone' => [Rule::in(DateTimeZone::listIdentifiers())],
             'street1' => ['nullable','max:255'],
             'street2' => ['nullable','max:255'],
             'city' => ['nullable','max:255'],
