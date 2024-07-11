f22d4ec1657bfb82631ecb86e7a2c9bbf71e6653
Bradford Powell <bpow@drpowell.org>
2024-07-11 17:22:50 -0400

    fix class name typo and remove unused imports

--- app/DataExchange/Actions/CspecDataSyncProcessor.php
+++ app/DataExchange/Actions/CspecDataSyncProcessor.php
@@ -5,19 +5,14 @@ namespace App\DataExchange\Actions;
 use Illuminate\Support\Facades\Log;
 use Lorisleiva\Actions\Concerns\AsJob;
 use App\Modules\ExpertPanel\Models\ExpertPanel;
-use App\Modules\ExpertPanel\Models\RulesetStatus;
 use App\DataExchange\Models\IncomingStreamMessage;
-use App\Modules\ExpertPanel\Actions\SpecificationSync;
-use App\Modules\ExpertPanel\Models\SpecificationStatus;
-use App\Modules\ExpertPanel\Actions\SpecificationCreate;
-use App\Modules\ExpertPanel\Actions\SpecificationRulesetCreate;
-use App\Modules\ExpertPanel\Actions\SpecificationAndRulsetsSync;
+use App\Modules\ExpertPanel\Actions\SpecificationAndRulesetsSync;
 
 class CspecDataSyncProcessor
 {
     use AsJob;
 
-    public function __construct(private SpecificationAndRulsetsSync $syncSpecificationAndRulesets)
+    public function __construct(private SpecificationAndRulesetsSync $syncSpecificationAndRulesets)
     {
     }
 
@@ -28,7 +23,7 @@ class CspecDataSyncProcessor
 
         $expertPanel = ExpertPanel::findByAffiliationId($cspecDoc->affiliationId);
         if (!$expertPanel) {
-            Log::error('Received proposal-submitted event about EP with unkown affiliation id '.$cspecDoc->affiliationId);
+            Log::error('Received proposal-submitted event about EP with unknown affiliation id '.$cspecDoc->affiliationId);
             return;
         }
 
