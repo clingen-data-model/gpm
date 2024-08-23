f634440256b9d4967901b3d212d9a3cc01719ce0
tmbattey-2021 <tmbattey@proton.me>
2024-08-23 18:45:46 -0400

    remove phone and address from person events DEX message

--- app/Modules/Person/Events/Traits/PublishesEvent.php
+++ app/Modules/Person/Events/Traits/PublishesEvent.php
@@ -35,25 +35,18 @@ trait PublishesEvent
                 'first_name' => $this->person->first_name,
                 'last_name' => $this->person->last_name,
                 'email' => $this->person->email,
-                'phone' => $this->person->phone,
                 'institution' => $this->getInstitutionMessage(),
                 'credentials' => $this->person->credentials,
                 'biography' => $this->person->biography,
                 'profile_photo' => $this->person->ProfilePhotoUrl,
                 'orcid_id' => $this->person->orcid_id,
                 'hypothesis_id' => $this->person->hypothesis_id,
-                'address' => [
-                    'street1' => $this->person->street1,
-                    'street2' => $this->person->street2,
-                    'city' => $this->person->city,
-                    'state' => $this->person->state,
-                    'zip' => $this->person->zip,
-                    'country' => $this->person->country ? $this->person->country->name : null
-                ],
                 'timezone' => $this->person->timezone
             ]
         ];
     }
+
+
 
 
     private function getInstitutionMessage(): ?array
