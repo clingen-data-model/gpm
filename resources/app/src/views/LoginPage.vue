e960de36940cbf1e3c2db9748669c4f9e04317c5
tmbattey-2021 <tmbattey@proton.me>
2024-10-11 11:08:31 -0400

    revised code positions redeem invite link to immediately follow the reset password link

--- resources/app/src/views/LoginPage.vue
+++ resources/app/src/views/LoginPage.vue
@@ -2,20 +2,13 @@
     <div>
         <card title="Login" class="md:w-1/2 mx-auto">
             <login-form @authenticated="redirect"></login-form>
-            <div class="mt-4 text-left">
-                <router-link class="text-blue-500 underline" :to="{name: 'RedeemInvite'}">
-                    Redeem your invite
-                </router-link>
-            </div>
         </card>
     </div>
 </template>
 
-
-
 <script>
 import LoginForm from '@/components/LoginForm.vue';
-import { mapGetters } from 'vuex'
+import { mapGetters } from 'vuex';
 
 export default {
     name: "LoginPage",
@@ -32,35 +25,23 @@ export default {
     },
     methods: {
         redirect() {
-            let route = {name: 'Dashboard'};
+            let route = { name: 'Dashboard' };
             if (this.$route.query.redirect) {
                 route = this.$route.query.redirect;
             }
             if (this.$route.redirectedFrom && this.$route.redirectedFrom.name !== 'login') {
-                route = this.$route.redirectedFrom
+                route = this.$route.redirectedFrom;
             }
-            this.$router.push(route)
+            this.$router.push(route);
         },
-        redirectIfAuthed(){
+        redirectIfAuthed() {
             if (this.isAuthed) {
-                this.redirect()
+                this.redirect();
             }
         }
     },
     mounted() {
         this.redirectIfAuthed();
     }
-}
+};
 </script>
-
-<style scoped>
-.router-link {
-    color: #888; /* Even lighter gray color */
-    font-size: 0.85em; /* Slightly smaller font size */
-    text-decoration: none; /* No underline by default */
-}
-
-.router-link:hover {
-    text-decoration: underline; /* Underline on hover */
-}
-</style>
