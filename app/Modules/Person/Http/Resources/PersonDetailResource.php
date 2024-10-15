298c629e6217e60e2637821ef0b7d9f93cf632a3
tmbattey-2021 <tmbattey@proton.me>
2024-10-15 17:59:38 -0400

    coded solution to view Sonja profile

--- app/Modules/Person/Http/Resources/PersonDetailResource.php
+++ app/Modules/Person/Http/Resources/PersonDetailResource.php
@@ -18,11 +18,35 @@ class PersonDetailResource extends JsonResource
     {
         $data = parent::toArray($request);
         $user = $request->user();
-        if ($user->id === $this->user->id || $user->hasAnyRole(['admin', 'super-admin'])) {
+
+
+       if (
+            (is_null($user->id) && is_null($this->user->id)) || 
+            //($user->id === $this->user->id) || 
+            $user->hasAnyRole(['admin', 'super-admin'])
+        ) {
+        //    // Proceed with your logic when either the IDs match or the user has the required roles
             foreach (Person::$contact_details_private_fields as $field) {
-                $data[$field] = $this->$field;
-            }
-        }
+              $data[$field] = $this->$field;
+       }
+    }
+
+       //Current production
+       //if ($user->id === $this->user->id || $user->hasAnyRole(['admin', 'super-admin'])) {
+      //  foreach (Person::$contact_details_private_fields as $field) {
+        //    $data[$field] = $this->$field;
+        //}
+        
+    
+
+    //solution we discussed today
+    //if (is_null($this->user->id) || ($user->id === $this->user->id) || $user->hasAnyRole(['admin', 'super-admin'])) {
+        // Your logic here
+      //  foreach (Person::$contact_details_private_fields as $field) {
+      //      $data[$field] = $this->$field;
+   // }
+//}
+
         $data['memberships'] = $this->whenLoaded('memberships', MembershipResource::collection($this->memberships));
         return $data;
     }
