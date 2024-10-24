3c9a7b278fdd18af558ca37a4e543213a2192934
Bradford Powell <bpow@drpowell.org>
2024-10-24 10:46:21 -0400

    reformatting source (no semantic changes)

--- resources/app/src/components/ImageCropper.vue
+++ resources/app/src/components/ImageCropper.vue
@@ -16,7 +16,8 @@
                     <h4>Tips on cropping images:</h4>
                     <ul class="list-disc pl-6 text-xs">
                         <li>Drag within the cropper to move the cropped region.</li>
-                        <li>Resize the cropped region by clicking on the edges or corners of the cropper and dragging.</li>
+                        <li>Resize the cropped region by clicking on the edges or corners of the cropper and dragging.
+                        </li>
                         <li>Drag the image outside of the cropper to move.</li>
                         <li>
                             Use the
@@ -27,7 +28,8 @@
                         </li>
                     </ul>
 
-                    <note class="pl-6 mt-3">The cropper must be entirely on the image so you may need to reduce the cropped are or zoom out to move the image.</note>
+                    <note class="pl-6 mt-3">The cropper must be entirely on the image so you may need to reduce the
+                        cropped are or zoom out to move the image.</note>
                 </template>
                 <icon-question height="14" widht="14" />
             </popper>
@@ -38,91 +40,87 @@
             </div>
             <div>
                 <h4>Preview</h4>
-                <img
-                    class="border border-gray-700 overflow-hidden drop-shadow"
-                    style="width: 100px; height: 100px"
-                    ref="previewEl"
-                    :src="previewUrl"
-                >
+                <img class="border border-gray-700 overflow-hidden drop-shadow" style="width: 100px; height: 100px"
+                    ref="previewEl" :src="previewUrl">
             </div>
         </div>
     </div>
 </template>
 <script setup>
-    import 'cropperjs/dist/cropper.css'
-    import {debounce} from 'lodash-es'
-    import Cropper from 'cropperjs';
-    import {ref, onMounted, watch, onUnmounted} from 'vue';
+import 'cropperjs/dist/cropper.css'
+import { debounce } from 'lodash-es'
+import Cropper from 'cropperjs';
+import { ref, onMounted, watch, onUnmounted } from 'vue';
 
-    const props = defineProps({
-        imageSrc: String
-    })
-    const emit = defineEmits(['cropped']);
+const props = defineProps({
+    imageSrc: String
+})
+const emit = defineEmits(['cropped']);
 
-    const imageEl = ref(null);
-    const previewUrl = ref(null);
-    const cropperInst = ref(null);
+const imageEl = ref(null);
+const previewUrl = ref(null);
+const cropperInst = ref(null);
 
-    const emitCropped = debounce(() => {
-        if (cropperInst.value && cropperInst.value.getCroppedCanvas()) {
-            cropperInst.value.getCroppedCanvas().toBlob(blob => {
-                emit('cropped', blob);
-            });
-        }
-    }, 200)
-
-    const updatePreview = debounce(() => {
+const emitCropped = debounce(() => {
+    if (cropperInst.value && cropperInst.value.getCroppedCanvas()) {
         cropperInst.value.getCroppedCanvas().toBlob(blob => {
-            if (blob) {
-                previewUrl.value = URL.createObjectURL(blob)
-            }
-        })
-    }, 500)
-
-    const sizeCropper = () => {
-        const imageData = cropperInst.value.getImageData();
-        cropperInst.value.setCropBoxData({
-            width: imageData.width,
-            height: imageData.height,
+            emit('cropped', blob);
         });
     }
+}, 200)
 
-    const setupCropper = () => {
-        cropperInst.value = new Cropper(imageEl.value, {
-            viewMode: 1,
-            dragMode: 'move',
-            aspectRatio: 1 / 1,
-            backround: false,
-            zoomOnWheel: false,
-            ready () {
-                sizeCropper();
-            },
-            crop () {
-                updatePreview();
-                emitCropped();
-            }
-        })
-    }
-
-    const zoomIn = () => {
-        cropperInst.value.zoom(0.1);
-    }
-
-    const zoomOut = () => {
-        cropperInst.value.zoom(-0.1);
-    }
-
-    watch(() => props.imageSrc, (to) => {
-        cropperInst.value.replace(to);
-        sizeCropper();
-    });
-
-    onMounted( () => {
-        setupCropper()
-    });
-
-    onUnmounted( () => {
-        cropperInst.value.destroy();
-        URL.revokeObjectURL(previewUrl.value);
+const updatePreview = debounce(() => {
+    cropperInst.value.getCroppedCanvas().toBlob(blob => {
+        if (blob) {
+            previewUrl.value = URL.createObjectURL(blob)
+        }
     })
+}, 500)
+
+const sizeCropper = () => {
+    const imageData = cropperInst.value.getImageData();
+    cropperInst.value.setCropBoxData({
+        width: imageData.width,
+        height: imageData.height,
+    });
+}
+
+const setupCropper = () => {
+    cropperInst.value = new Cropper(imageEl.value, {
+        viewMode: 1,
+        dragMode: 'move',
+        aspectRatio: 1 / 1,
+        backround: false,
+        zoomOnWheel: false,
+        ready() {
+            sizeCropper();
+        },
+        crop() {
+            updatePreview();
+            emitCropped();
+        }
+    })
+}
+
+const zoomIn = () => {
+    cropperInst.value.zoom(0.1);
+}
+
+const zoomOut = () => {
+    cropperInst.value.zoom(-0.1);
+}
+
+watch(() => props.imageSrc, (to) => {
+    cropperInst.value.replace(to);
+    sizeCropper();
+});
+
+onMounted(() => {
+    setupCropper()
+});
+
+onUnmounted(() => {
+    cropperInst.value.destroy();
+    URL.revokeObjectURL(previewUrl.value);
+})
 </script>
