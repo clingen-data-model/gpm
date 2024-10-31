50d52ed43b98221f5085609ce83ef785d74df269
tmbattey-2021 <tmbattey@proton.me>
2024-10-31 19:46:49 -0400

    changed layout of members table within an application

--- resources/app/src/components/expert_panels/ReviewMembership.vue
+++ resources/app/src/components/expert_panels/ReviewMembership.vue
@@ -1,171 +1,201 @@
 <script setup>
-    import { computed, ref, watch } from 'vue'
-    import axios from 'axios'
-    import {hasPermission} from '../../auth_utils';
-    import CredentialsView from '../people/CredentialsView.vue';
-    import ExpertisesView from '../people/ExpertisesView.vue';
-    import { formatDate } from '@/date_utils'
+import { computed, ref, watch } from 'vue'
+import axios from 'axios'
+import { hasPermission } from '../../auth_utils';
+import CredentialsView from '../people/CredentialsView.vue';
+import ExpertisesView from '../people/ExpertisesView.vue';
+import { formatDate } from '@/date_utils'
 
-    const props = defineProps({
-        members: {
-            required: true,
-            type: Array
-        },
+const props = defineProps({
+    members: {
+        required: true,
+        type: Array
+    },
+});
+
+
+const fields = ref(['name', 'credentials', 'expertise', 'institution']);
+if (hasPermission('ep-applications-manage')) {
+    fields.value.push('coi_completed');
+}
+
+
+const sortDescending = ref(false); // false for ascending, true for descending
+
+
+const members = ref([]);
+
+// Watch for changes in props.members and apply sorting and aggregation
+watch(() => props.members, async (to) => {
+    if (!to) {
+        return;
+    }
+
+    // Copy the incoming members array
+    members.value = [...to];
+
+
+
+
+}, { immediate: true });
+
+// Aggregate roles for each member
+const aggregateRoles = (membersList) => {
+    const aggregatedMembers = {};
+
+    membersList.forEach(member => {
+        if (!aggregatedMembers[member.id]) {
+            aggregatedMembers[member.id] = {
+                ...member,
+                roles: [...member.roles],
+            };
+        } else {
+            aggregatedMembers[member.id].roles = [
+                ...new Set([...aggregatedMembers[member.id].roles, ...member.roles]),
+            ];
+        }
     });
 
-    const loadPubmed = computed(() => hasPermission('ep-applications-approve'))
+    return Object.values(aggregatedMembers);
+};
 
-    const fields = ref(['name', 'credentials', 'expertise', 'institution']);
-    if (hasPermission('ep-applications-manage')) {
-        fields.value.push('coi_completed');
+// Computed property to create table rows with aggregated roles
+const rolePriority = {
+    chair: 1,
+    coordinator: 2,
+    biocurator: 3,
+    expert: 4,
+};
+
+// Function to get the highest-priority role for sorting
+const getPrimaryRolePriority = (roles) => {
+    for (const role of roles) {
+        if (rolePriority[role.name.toLowerCase()] !== undefined) {
+            return rolePriority[role.name.toLowerCase()];
+        }
     }
+    return Infinity; // Return a large number for roles that don't match any in the hierarchy
+};
 
-    const members = ref([]);
-    const chairs = computed(() => tableRows.value.filter(m => m.roles.includes('chair')));
-    const experts = computed(() => tableRows.value.filter(m => m.roles.includes('expert')));
+const totalMembers = computed(() => {
+    return tableRows.value.length;
+});
 
-    const memberGroups = computed(() => [
-        {
-            title: 'Leadership',
-            members: chairs.value
-        },
-        {
-            title: 'Coordination',
-            members: tableRows.value.filter(m => m.roles.includes('coordinator'))
-        },
-        {
-            title: 'Biocuration',
-            members: tableRows.value.filter(m => m.roles.includes('biocurator'))
-        },
-        {
-            title: 'Expertise',
-            members: experts.value
-        }
-    ])
+const tableRows = computed(() => {
+    const allMembers = aggregateRoles(members.value);
 
-    const delay = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
-    const getPublications = async member => {
-        const baseUri = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils';
-        const searchUrl = `${baseUri}/esearch.fcgi?db=pubmed&term=${member.last_name},+${member.first_name}[author]&retmode=json&retmax=0`;
+    // Sort members by role priority, then by last name according to sortDescending
+    allMembers.sort((a, b) => {
+        const priorityA = getPrimaryRolePriority(a.roles);
+        const priorityB = getPrimaryRolePriority(b.roles);
 
-        return axios.get(searchUrl)
-            .then(rsp => {
-                member.pubCount = rsp.data.esearchresult.count
-            })
-            .catch(async error => {
-                if (error.response.status == 429) {
-                    await delay();
-                    getPublications(member);
-                }
-            });
-    }
-
-    watch(() => props.members, async to => {
-        if (!to) {
-            return;
+        // Sort by role priority first
+        if (priorityA !== priorityB) {
+            return priorityA - priorityB;
         }
 
-        members.value = [...to];
+        // If priorities are equal, sort by last name based on sortDescending
+        const nameComparison = a.person.last_name.localeCompare(b.person.last_name);
+        return sortDescending.value ? -nameComparison : nameComparison;
+    });
 
-        members.value.sort((a, b) => {
-            if (a.roles.includes('chair') && !b.roles.includes('chair')) {
-                return -1;
-            }
-            if (!a.roles.includes('chair') && b.roles.includes('chair')) {
-                return 1;
-            }
+    return allMembers.map(m => {
+        const retVal = {
+            id: m.id,
+            first_name: m.person.first_name,
+            last_name: m.person.last_name,
+            name: m.person.name,
+            institution: m.person.institution ? m.person.institution.name : null,
+            legacy_credentials: m.person.legacy_credentials,
+            legacy_expertise: m.legacy_expertise,
+            roles: m.roles.map(role => role.name).join(', '),  // Extract role names and join them
+            person: m.person
+        };
 
-            if (a.roles.includes('expert') && !b.roles.includes('expert')) {
-                return -1;
-            }
-            if (!a.roles.includes('expert') && b.roles.includes('expert')) {
-                return 1;
-            }
-
-            return 0;
-        })
-
-        for(let idx in members.value.filter(m => m.roles.includes('chair') || m.roles.includes('expert'))) {
-            if (loadPubmed.value) {
-                getPublications(members.value[idx])
-                await delay(500);
-            }
+        if (hasPermission('ep-applications-manage')) {
+            retVal.coi_completed = formatDate(m.coi_last_completed);
         }
-    }, {immediate: true})
 
-    const tableRows = computed( () => {
-        return props.members.map(m => {
-            const retVal = {
-                id: m.id,
-                first_name: m.person.first_name,
-                last_name: m.person.last_name,
-                name: m.person.name,
-                institution: m.person.institution ? m.person.institution.name : null,
-                // credentials: m.person.credentials,
-                legacy_credentials: m.person.legacy_credentials,
-                legacy_expertise: m.legacy_expertise,
-                roles: m.roles.map(r => r.name).join(', '),
-                person: m.person
-            }
-            if (hasPermission('ep-applications-manage')) {
-                retVal.coi_completed = formatDate(m.coi_last_completed);
-            }
+        return retVal;
+    });
+});
 
-            return retVal;
-        });
-    })
+
+
+
 
 </script>
+
 <template>
-    <div>
-        <table>
-            <template v-for="g in memberGroups" :key="g.title">
-                <thead>
-                    <tr>
-                        <th colspan="6" class="bg-white border-0 pl-0 pb-1 pt-3">
-                            <span class="text-lg">{{g.title}}</span>
-                            &nbsp;
-                            <badge size="xxs">{{g.members.length}}</badge>
-                        </th>
-                    </tr>
-                </thead>
-                <template v-if="g.members.length > 0">
-                    <thead>
-                        <tr class="text-sm">
-                            <th>Name</th>
-                            <th>Credentials</th>
-                            <th>Expertise</th>
-                            <th>Institution</th>
-                            <th>Publications</th>
-                        </tr>
-                    </thead>
-                    <tbody class="text-sm">
-                        <tr v-for="m in g.members" :key="m.id">
-                            <td>{{m.person.name}}</td>
-                            <td>
-                                <CredentialsView :person="m.person" />
-                            </td>
-                            <td>
-                                <ExpertisesView :person="m.person" :legacyExpertise="m.legacy_expertise" />
-                            </td>
-                            <td>{{m.institution}}</td>
-                            <td>
-                                <div v-if="m.pubCount">
-                                    <popper v-if="m.pubCount > 0" content="Go to PubMed results." hover arrow placement="left">
-                                        <a :href="`https://pubmed.ncbi.nlm.nih.gov/?term=${m.last_name},+${m.first_name}%5BAuthor%5D`"
-                                            target="pubmed"
-                                            >
-                                            <badge size="xxs">{{m.pubCount}}</badge>
-                                        </a>
-                                    </popper>
-                                    <badge v-else size="xxs">{{m.pubCount}}</badge>
-                                </div>
-                                <button v-else class="btn btn-xs" @click="getPublications(m)">Get</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </template>
-            </template>
+
+
+
+<div>
+    <!-- Membership Total Display -->
+    <div class="membership-total">
+        <p>Membership Total = {{ totalMembers }}</p>
+    </div>
+
+    <!-- Table Display with Flexbox -->
+    <div class="table-container">
+        <table class="flex-table">
+            <thead>
+                <tr class="text-sm">
+                    <th class="flex-column" @click="sortDescending = !sortDescending" style="cursor: pointer; display: flex; align-items: center;">
+                        Name
+                        <span v-if="sortDescending">▼</span>
+                        <span v-else>▲</span>
+                    </th>
+                    <th class="flex-column">Roles</th>
+                    <th class="flex-column">Credentials</th>
+                    <th class="flex-column">Expertise</th>
+                    <th class="flex-column">Institution</th>
+                </tr>
+            </thead>
+            <tbody class="text-sm">
+                <tr v-for="m in tableRows" :key="m.id">
+                    <td class="flex-column">{{ m.name }}</td>
+                    <td class="flex-column">{{ m.roles }}</td>
+                    <td class="flex-column">
+                        <CredentialsView :person="m.person" />
+                    </td>
+                    <td class="flex-column">
+                        <ExpertisesView :person="m.person" :legacyExpertise="m.legacy_expertise" />
+                    </td>
+                    <td class="flex-column">{{ m.institution }}</td>
+                </tr>
+            </tbody>
         </table>
     </div>
+</div>
+
 </template>
+
+<style>
+/* Flexbox table layout */
+.table-container {
+    display: flex;
+    width: 100%;
+}
+
+.flex-table {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+.flex-column {
+    flex: 1;
+    padding: 8px;
+}
+
+.flex-table thead tr {
+    display: flex;
+    justify-content: space-between;
+}
+
+.flex-table tbody tr {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
