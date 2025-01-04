c676da7c34bcec99208f1046c41f8bfed515ab3b
Bradford Powell <bpow@drpowell.org>
2025-01-04 15:19:34 -0500

    GroupResource does not need to access group as sub-object

--- app/Modules/Group/Http/Resources/GroupResource.php
+++ app/Modules/Group/Http/Resources/GroupResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Modules\Group\Http\Resources;
 
-use App\Http\Resources\RoleResource;
 use App\Http\Resources\ExpertPanelResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Modules\Group\Http\Resources\MemberResource;
@@ -25,9 +24,9 @@ class GroupResource extends JsonResource
         $data['status'] = $this->whenLoaded('status', new GroupStatusResource($this->status));
         $data['members'] = $this->whenLoaded('members', MemberResource::collection($this->members));
         $data['coordinators'] = $this->whenLoaded('coordinators', MemberResource::collection($this->members));
-        $data['is_vcep'] = $this->group->isVcep;
-        $data['is_gcep'] = $this->group->isGcep;
-        $data['is_scvcep'] = $this->group->isScvcep;
+        $data['is_vcep'] = $this->isVcep;
+        $data['is_gcep'] = $this->isGcep;
+        $data['is_scvcep'] = $this->isScvcep;
         // $data['parent'] = $this->whenLoaded('parent', GroupResource::collection($this->parent));
 
 
