a883e78a39d86180a741a4a5dd2d5e7ff509e0a9
TJ Ward <yugen@ibiblio.org>
2025-01-05 10:09:31 -0500

    Refresh db for all tests.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,12 +17,14 @@ use Database\Seeders\EpTypesTableSeeder;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Modules\ExpertPanel\Actions\ContactAdd;
 use App\Modules\ExpertPanel\Models\ExpertPanel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use WithFaker;
+    use RefreshDatabase;
     // Helper methods
 
     public function setup():void
