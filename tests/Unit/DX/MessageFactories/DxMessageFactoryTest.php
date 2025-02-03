644e362a5d302d95d315cb1092bed3a4e9d5464e
Bradford Powell <bpow@drpowell.org>
2025-02-03 12:59:00 -0500

    update test for newer message format

--- tests/Unit/DX/MessageFactories/DxMessageFactoryTest.php
+++ tests/Unit/DX/MessageFactories/DxMessageFactoryTest.php
@@ -34,6 +34,8 @@ use App\DataExchange\MessageFactories\DxMessageFactory;
 class DxMessageFactoryTest extends TestCase
 {
     use RefreshDatabase;
+    private ExpertPanel $expertPanel;
+    private DxMessageFactory $factory;
 
     public function setup():void
     {
@@ -148,7 +150,7 @@ class DxMessageFactoryTest extends TestCase
     /**
      * @test
      */
-    public function it_creates_a_member_retireded_event()
+    public function it_creates_a_member_retired_event()
     {
         $event = new MemberRetired($this->expertPanel->group->members->first());
 
@@ -161,7 +163,7 @@ class DxMessageFactoryTest extends TestCase
     /**
      * @test
      */
-    public function it_creates_a_member_unretireded_event()
+    public function it_creates_a_member_unretired_event()
     {
         $event = new MemberUnretired($this->expertPanel->group->members->first());
 
@@ -232,11 +234,9 @@ class DxMessageFactoryTest extends TestCase
 
     private function assertExpertPanelInMessage($message)
     {
-        $this->assertEquals([
-            'id' => $this->expertPanel->group->uuid,
-            'name' => $this->expertPanel->group->displayName,
-            'type' => $this->expertPanel->group->fullType->name,
-            'affiliation_id' => $this->expertPanel->affiliation_id
-         ], $message['data']['expert_panel']);
+        $g = $message['data']['group'];
+        $this->assertEquals($g['id'], $this->expertPanel->group->uuid);
+        $this->assertEquals($g['name'], $this->expertPanel->group->name);
+        $this->assertEquals($g['affiliation_id'], $this->expertPanel->affiliation_id);
     }
 }
