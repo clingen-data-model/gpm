371b77157993efe68158adfad6d0d7fc4d61a111
Bradford Powell <bpow@drpowell.org>
2025-02-04 19:41:20 -0500

    remove unused "use"s, preparing for refactoring

--- app/Modules/ExpertPanel/Events/ExpertPanelAttributesUpdated.php
+++ app/Modules/ExpertPanel/Events/ExpertPanelAttributesUpdated.php
@@ -3,14 +3,11 @@
 namespace App\Modules\ExpertPanel\Events;
 
 use App\Events\PublishableEvent;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
 use App\Modules\ExpertPanel\Models\ExpertPanel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class ExpertPanelAttributesUpdated extends ExpertPanelEvent implements PublishableEvent
 {
