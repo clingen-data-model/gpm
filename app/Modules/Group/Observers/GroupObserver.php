304dd96bee7adcb0e6073d2ad16675df6ce952d7
Bradford Powell <bpow@drpowell.org>
2025-02-06 20:15:29 -0500

    use generic GroupAttributesUpdated to update status when ApplicationCompleted

---
+++ app/Modules/Group/Observers/GroupObserver.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Group;
+
+class GroupObserver
+{
+    /**
+     * Handle the Group "created" event.
+     */
+    public function created(Group $group): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Group "updated" event.
+     */
+    public function updated(Group $group): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Group "deleted" event.
+     */
+    public function deleted(Group $group): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Group "restored" event.
+     */
+    public function restored(Group $group): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Group "force deleted" event.
+     */
+    public function forceDeleted(Group $group): void
+    {
+        //
+    }
+}
