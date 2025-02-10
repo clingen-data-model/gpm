5422d29a3522fa6692245117de7c8b459320284e
Bradford Powell <bpow@drpowell.org>
2025-02-10 13:50:28 -0500

    fixup group representation for dx

--- app/Modules/Group/Events/GroupEvent.php
+++ app/Modules/Group/Events/GroupEvent.php
@@ -70,26 +70,26 @@ abstract class GroupEvent extends RecordableEvent implements PublishableEvent
     private function groupRepresentation(Group $g): array {
         // FIXME: this will stack overflow if someone is foolish enough to create a circular reference
         $item = [
-            'id' => $this->group->uuid,
-            'name' => $this->group->name,
-            'description' => $this->group->description,
-            'status' => $this->group->groupStatus->name,
-            'type' => $this->group->type->name,
+            'id' => $g->uuid,
+            'name' => $g->name,
+            'description' => $g->description,
+            'status' => $g->groupStatus->name,
+            'type' => $g->type->name,
         ];
-        if ($this->group->parent != null) {
-            $item['parent_group'] = $this->group->parent->representationForDataExchange();
+        if ($g->parent != null) {
+            $item['parent_group'] = $this->groupRepresentation($g->parent);
         }
-        if ($this->group->isEp) {
-            $item['ep_id'] = $this->group->expertPanel->uuid;
-            $item['affiliation_id'] = $this->group->expertPanel->affiliation_id;
-            $item['scope_description'] = $this->group->expertPanel->scope_description;
-            $item['short_name'] = $this->group->expertPanel->short_base_name;
+        if ($g->isEp) {
+            $item['ep_id'] = $g->expertPanel->uuid;
+            $item['affiliation_id'] = $g->expertPanel->affiliation_id;
+            $item['scope_description'] = $g->expertPanel->scope_description;
+            $item['short_name'] = $g->expertPanel->short_base_name;
             // TODO: not sure about these fields, they appear to be unused
             // $item['long_base_name'] = $this->group->expertPanel->long_base_name;
             // $item['hypothesis_group'] = $this->group->expertPanel->hypothesis_group;
             // $item['membership_description'] = $this->group->expertPanel->membership_description;
             if ($this->group->fullType->name === 'vcep') {
-                $item['cspec_url'] = $this->group->expertPanel->affiliation_id;
+                $item['cspec_url'] = $g->expertPanel->affiliation_id;
             }
         }
         return $item;
