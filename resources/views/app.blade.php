f4b266cffa214082cc0de49fcfea3a8ba41594f2
Bradford Powell <bpow@drpowell.org>
2025-02-13 15:47:25 -0500

    WIP moving to laravel-vite

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,1 +1,24 @@
-<!doctype html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon.ico"><title>ClinGen GPM</title><script defer="defer" src="/js/chunk-vendors.3d2f3b9f.js"></script><script defer="defer" src="/js/app.04a834fd.js"></script><link href="/css/app.9e0e23a9.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but ClinGen GPM doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div></body></html>
+<!doctype html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/favicon.ico">
+    <title>ClinGen GPM</title>
+    @vite(['resources/css/app.css'])
+</head>
+
+<body>
+    <noscript>
+        <strong>We're sorry but the ClinGen Group and Personnel Management (GPM) system
+            doesn't work properly without JavaScript enabled. Please enable it
+            to continue.
+        </strong>
+    </noscript>
+    <div id="app" v-cloak></div>
+    @vite(['resources/js/app.js'])
+</body>
+
+</html>
