78771e7b11de40dd298c00436caa5f43a5c161bf
Bradford Powell <bpow@drpowell.org>
2025-02-13 22:21:19 -0500

    vite config, kind-of-important...

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin'
+import path from 'path'
+
+const routes = () => import('resources/js/router/index.js')
+
+export default defineConfig({
+    plugins: [
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                }
+            }
+        }),
+        laravel({
+            input: ['resources/js/app.js', 'resources/css/app.css'],
+            refresh: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            '~': path.resolve(__dirname, 'node_modules'),
+            '@': path.resolve(__dirname, 'resources/js'),
+            // '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
+        },
+        // FIXME: should remove this when have added .vue extension to all component references
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    }
+})
