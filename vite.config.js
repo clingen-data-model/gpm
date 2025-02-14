a14c6f43725149cf1b1fed5747bf76ea024221d6
Bradford Powell <bpow@drpowell.org>
2025-02-14 15:48:09 -0500

    make sourcemaps, why not?

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import path from 'path'
 const routes = () => import('resources/js/router/index.js')
 
 export default defineConfig({
+    build: {
+        sourcemap: true,
+    },
     plugins: [
         vue({
             template: {
