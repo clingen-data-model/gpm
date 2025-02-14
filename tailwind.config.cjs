5db093f5933c1ff1b99d50bc17872aa590fc2904
Bradford Powell <bpow@drpowell.org>
2025-02-13 22:20:57 -0500

    bring tailwindcss and postcss into the vite setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -61,7 +61,7 @@ module.exports = {
             'lg': '1024px',
             'xl': '1280px',
             '2xl': '1536px',
-            'print': {'raw': 'print'}
+            'print': { 'raw': 'print' }
         },
     },
     variants: {
