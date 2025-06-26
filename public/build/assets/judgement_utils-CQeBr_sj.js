6c74d585e2e8103b749e8f1bc72c55410622c7d9
Bradford Powell <bpow@drpowell.org>
2025-06-26 16:00:33 -0400

    js bundles from prior commits

---
+++ public/build/assets/judgement_utils-CQeBr_sj.js
@@ -0,0 +1,2 @@
+const o={approve:"green","approve-after-revisions":"blue","request-revisions":"yellow"},r=e=>e.decision?o[e.decision]:"gray";export{r as j};
+//# sourceMappingURL=judgement_utils-CQeBr_sj.js.map
