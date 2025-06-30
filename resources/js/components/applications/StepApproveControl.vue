9dc96ddacdedc1a1529fd0f493ef9bf47627b376
setiadha <setiadha@unc.edu>
2025-06-29 23:01:21 -0400

    disable approval button when affiliation ID is not present

--- resources/js/components/applications/StepApproveControl.vue
+++ resources/js/components/applications/StepApproveControl.vue
@@ -16,11 +16,27 @@
     const attrs = useAttrs();
     const showApproveForm = ref(false);
     const isCurrentStep = computed(() => props.step === Number.parseInt(props.group.expert_panel.current_step))
-    const buttonTitle = computed(() => isCurrentStep.value
-                                            ? 'Approve this step'
-                                            : 'You can only approve the application\'s current step')
+    const buttonTitle = computed(() => {
+							if (isAffiliationMissing.value) {
+								return 'Cannot approve: Affiliation ID is missing';
+							}
+							return isCurrentStep.value
+								? 'Approve this step'
+								: 'You can only approve the application\'s current step';
+						});
+
+	const isAffiliationMissing = computed(() => {
+		if (props.step !== 1) return false;
+		const id = props.group?.expert_panel?.affiliation_id;
+		return !id || !id.toString().trim();
+	});
 
     const startApproveStep = () => {
+      	if (isAffiliationMissing.value) {
+			alert('Cannot approve: Affiliation ID is missing.');
+			return;
+		}
+
         showApproveForm.value = true;
     };
 
@@ -36,25 +52,28 @@
 </script>
 
 <template>
-  <div>
-    <button
-      :disabled="!isCurrentStep"
-      :title="buttonTitle"
-      class="btn btn-lg w-full"
-      v-bind="attrs"
-      @click="startApproveStep"
-    >
-      <slot />
-    </button>
+	<div>
+		<button
+			:disabled="!isCurrentStep || isAffiliationMissing"
+			:title="buttonTitle"
+			class="btn btn-lg w-full"
+			v-bind="attrs"
+			@click="startApproveStep"
+			>
+			<slot />
+		</button>
+		<p v-if="isAffiliationMissing && props.step === 1" class="text-red-600 text-sm mt-1">
+			You must enter an Affiliation ID before approving this step.
+		</p>
 
-    <teleport to="body">
-      <modal-dialog v-model="showApproveForm" size="xl" @closed="$refs.approvestepform.clearForm()">
-        <ApproveStepForm
-          ref="approvestepform"
-          @saved="handleApproved"
-          @canceled="hideApproveForm"
-        />
-      </modal-dialog>
-    </teleport>
-  </div>
+		<teleport to="body">
+			<modal-dialog v-model="showApproveForm" size="xl" @closed="$refs.approvestepform.clearForm()">
+				<ApproveStepForm
+				ref="approvestepform"
+				@saved="handleApproved"
+				@canceled="hideApproveForm"
+				/>
+			</modal-dialog>
+		</teleport>
+	</div>
 </template>
