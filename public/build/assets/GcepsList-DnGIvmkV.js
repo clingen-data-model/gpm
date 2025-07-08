a4735f8615214a75184787af511d74f85951b74a
Bradford Powell <bpow@drpowell.org>
2025-07-08 14:15:34 -0400

    js bundling from prior commit

---
+++ public/build/assets/GcepsList-DnGIvmkV.js
@@ -0,0 +1,2 @@
+import{A as o}from"./ApplicationsTable-NSbCdOiG.js";import{_ as t,c as p,f as s,a,b as n}from"./app-CfiFud4u.js";import"./router_aware_sort_and_filter-Ckd6VGZC.js";const c={components:{ApplicationsTable:o},props:{},data(){return{}}};function r(i,l,_,m,d,f){const e=a("ApplicationsTable");return n(),p("div",null,[s(e,{"ep-type-id":1})])}const $=t(c,[["render",r]]);export{$ as default};
+//# sourceMappingURL=GcepsList-DnGIvmkV.js.map
