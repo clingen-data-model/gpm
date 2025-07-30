f3681f0f596d262125b61aec0d80cd730adc0f06
setiadha <setiadha@unc.edu>
2025-07-29 22:13:26 -0400

    Reupload GPM-500 + testing + Laravel Telescope --dev

--- config/services.php
+++ config/services.php
@@ -46,4 +46,16 @@ return [
         'client_secret' => env('AFFILIATION_API_CLIENT_SECRET'),
     ],
 
+    'gt_api' => [
+        'client_id'     => env('GT_CLIENT_API_ID'),
+        'client_secret' => env('GT_CLIENT_API_SECRET'),
+        'oauth_url'     => env('GT_CLIENT_BASE_URL') . '/oauth/token',
+        'base_url'      => env('GT_CLIENT_BASE_URL') . env('GT_CLIENT_API'),
+    ],
+
+    'affiliation_api' => [
+        'base_url' => env('AFFILIATION_API_BASE_URL'),
+        'client_secret' => env('AFFILIATION_API_CLIENT_SECRET'),
+    ],
+
 ];
