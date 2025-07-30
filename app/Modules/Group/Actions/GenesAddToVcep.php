6471664a052d7575fec03858ef2d36934a73c619
setiadha <setiadha@unc.edu>
2025-07-29 23:53:08 -0400

    bugs adding gene to VCEP

--- app/Modules/Group/Actions/GenesAddToVcep.php
+++ app/Modules/Group/Actions/GenesAddToVcep.php
@@ -29,13 +29,14 @@ class GenesAddToVcep
         if (!$group->isVcepOrScvcep) {
             throw ValidationException::withMessages(['group' => 'The group is not a VCEP.']);
         }
-
+        
         $genes = collect(array_map(function ($gene) {
+            // $gene ["hgnc_id" => 11335, "mondo_id" => "MONDO:0015655"]
             return new Gene([
                 'hgnc_id' => $gene['hgnc_id'],
-                'gene_symbol' => $this->hgncLookup->findSymbolById($gene['hgnc_id']),
+                'gene_symbol' => $this->hgncLookup->findSymbolById($gene['hgnc_id']) ?? null,
                 'mondo_id' => $gene['mondo_id'],
-                'disease_name' => $this->mondoLookup->findNameByOntologyId($gene['mondo_id'])
+                'disease_name' => $this->mondoLookup->findNameByOntologyId($gene['mondo_id']) ?? null,
             ]);
         }, $genes));
 
