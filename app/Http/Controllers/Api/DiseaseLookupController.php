79050f1bc1e57353106914acb02e68b19bb97655
setiadha <setiadha@unc.edu>
2025-08-01 12:20:45 -0400

    Added curations

--- app/Http/Controllers/Api/DiseaseLookupController.php
+++ app/Http/Controllers/Api/DiseaseLookupController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
