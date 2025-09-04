f17413b1f21c55bf71972b06fcbb94c9183773d2
setiadha <setiadha@unc.edu>
2025-09-04 17:10:22 -0400

    Adjusted dates, misspelling, inactive date, member's name and email field.

--- app/Modules/Group/Http/Resources/GroupExternalResource.php
+++ app/Modules/Group/Http/Resources/GroupExternalResource.php
@@ -5,6 +5,7 @@ namespace App\Modules\Group\Http\Resources;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Modules\Group\Models\Group;
+use Illuminate\Support\Facades\DB;
 
 class GroupExternalResource extends JsonResource
 {
@@ -30,11 +31,13 @@ class GroupExternalResource extends JsonResource
             'status' => $this->groupStatus->name,
             'status_date' => $this->groupStatus->updated_at,
             'type' => $this->type->name,
+            'coi' => $this->expertPanel && $this->expertPanel->affiliation_id ? url('/coi/'.$this->expertPanel->affiliation_id) : null,
             'members' => $this->members()->with(['person', 'person.credentials', 'roles', 'latestCoi'])->get()->map(function ($member) {
                 $p = $member->person;
                 $personData = [
-                    'first_name' => $p->first_name,
-                    'last_name' => $p->last_name,
+                    'name'        => trim($p->first_name.' '.$p->last_name),
+                    // 'first_name' => $p->first_name, 'last_name' => $p->last_name,
+                    'email'       => $p->email, 
                     'credentials' => $p->credentials->map(function ($credential) {
                         return $credential->name;
                     }),
@@ -42,7 +45,7 @@ class GroupExternalResource extends JsonResource
                     'roles' => $member->roles->map(function ($role) {
                         return $role->display_name;
                     }),
-                    'last_coi_completion_date' => $member->latestCoi?->completed_at,
+                    // 'last_coi_completion_date' => $member->latestCoi?->completed_at,
                 ];
                 if ($p->profile_photo) {
                     $personData['profile_photo'] = URL::to('/profile-photos/' . $p->profile_photo);
@@ -56,17 +59,25 @@ class GroupExternalResource extends JsonResource
             $data += [
                 'expert_panel' => [
                     'uuid' => $ep->uuid,
-                    'affilitaion_id' => $ep->affiliation_id,
+                    'affiliation_id' => $ep->affiliation_id,
                     'name' => $ep->long_base_name,
                     'short_name' => $ep->short_base_name,
-                    'scope_description' => $ep->scope_description,
+                    // 'scope_description' => $ep->scope_description,
                     'membership_description' => $ep->membership_description,
                     'type' => $ep->type->name,
-                    'step_1_approval_date' => $ep->step_1_approval_date,
-                    'step_2_approval_date' => $ep->step_2_approval_date,
-                    'step_3_approval_date' => $ep->step_3_approval_date,
-                    'step_4_approval_date' => $ep->step_4_approval_date,
+
+                    // VCEP
+                    'vcep_define_group'         => ($this->isVcep || $this->isScvcep) ? $ep->step_1_approval_date : null,
+                    'vcep_classification_rules' => ($this->isVcep || $this->isScvcep) ? $ep->step_2_approval_date : null,
+                    'vcep_pilot_rules'          => ($this->isVcep || $this->isScvcep) ? $ep->step_3_approval_date : null,
+                    'vcep_approval'             => ($this->isVcep || $this->isScvcep) ? $ep->step_4_approval_date : null,
+
+                    // GCEP dates
+                    'gcep_define_group'         => $this->isGcep ? $ep->step_1_received_date : null,
+                    'gcep_approval'             => $this->isGcep ? $ep->step_1_approval_date : null,
+
                     'date_completed' => $ep->date_completed,
+                    'inactive_date' => $this->group_status_id === 5 ? $this->deriveInactiveDate($this->id) : null,
                     'current_step' => $ep->current_step,
                 ],
             ];
@@ -78,10 +89,26 @@ class GroupExternalResource extends JsonResource
                 'parent' => [
                     'uuid' => $this->parent->uuid,
                     'name' => $this->parent->name,
+                    'type'   => optional($this->parent->type)->name,
+                    'status' => optional($this->parent->status)->name,
                 ],
             ];
         }
         
         return $data;
+    }
+
+    protected function deriveInactiveDate($groupID): ?string
+    {
+        $subjectType = 'App\\Modules\\Group\\Models\\Group';
+
+        $ts = DB::table('activity_log')
+            ->where('subject_type', $subjectType)
+            ->where('subject_id', $groupID)
+            ->where('description', 'like', '%inactive%')
+            ->latest('created_at')
+            ->value('created_at');
+
+        return $ts ? \Illuminate\Support\Carbon::parse($ts)->toDateString() : null;
     }
 }
