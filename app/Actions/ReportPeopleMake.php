2845334bb58736d587616f457ba09eef1ac44bfb
setiadha <setiadha@unc.edu>
2025-09-08 15:44:04 -0400

    Updated to stream/chunk to avoid OOM

--- app/Actions/ReportPeopleMake.php
+++ app/Actions/ReportPeopleMake.php
@@ -1,70 +1,147 @@
 <?php
 
 namespace App\Actions;
+
 use Illuminate\Console\Command;
 use App\Modules\Person\Models\Person;
+use Illuminate\Support\Facades\DB;
 
 class ReportPeopleMake extends ReportMakeAbstract
 {
-    public $commandSignature = 'report:people';
+    public $commandSignature = 'report:people {--csv=}';
 
-    public function handle()
+    public function handle(string $csvPath = null)
     {
-        $people = Person::with([
-                    'memberships' => function ($q) {
-                        $q->isActive();
-                    },
-                    'memberships.group',
-                    'memberships.group.type',
-                    'memberships.roles',
-                    'country',
-                    'credentials',
-                    'expertises',
-                ])
-                ->get();
+        DB::connection()->disableQueryLog();
+        $fh = null;
+        if ($csvPath) {
+            if (!str_starts_with($csvPath, '/')) {
+                $csvPath = storage_path($csvPath);
+            }
+            @mkdir(dirname($csvPath), 0775, true);
+            $fh = fopen($csvPath, 'w');
+            fputcsv($fh, [
+                'id',
+                'name',
+                'email',
+                'institution',
+                'state',
+                'country',
+                'timezone',
+                'phone',
+                'credentials',
+                'expertises',
+                'active_groups',
+                'biography',
+                'orcid_id',
+                'hypothesis_id',
+                'has_registered',
+                'date created',
+                'last_updated'
+            ]);
+        }
+        
+        $rows = [];
+        $writeRow = function(array $data) use (&$rows, $fh) {            
+            foreach ($data as $k => $v) {
+                if (is_string($v)) {
+                    $data[$k] = preg_replace('/\r?\n/', '; ', $v);
+                }
+            }
+            if ($fh) {
+                fputcsv($fh, [
+                    $data['id'],
+                    $data['name'],
+                    $data['email'],
+                    $data['institution'],
+                    $data['state'],
+                    $data['country'],
+                    $data['timezone'],
+                    $data['phone'],
+                    $data['credentials'],
+                    $data['expertises'],
+                    $data['active_groups'],
+                    $data['biography'],
+                    $data['orcid_id'],
+                    $data['hypothesis_id'],
+                    $data['has_registered'],
+                    $data['date created'],
+                    $data['last_updated'],
+                ]);
+            } else {
+                $rows[] = $data;
+            }
+        };
 
-            $people = $people->map(function ($person) {
+        Person::query()
+            ->whereNull('deleted_at')
+            ->select([
+                'id','first_name','last_name','email','institution_id','state','country_id', 'timezone','phone','biography','orcid_id','hypothesis_id','user_id', 'created_at','updated_at'
+            ])
+            ->with([
+                'memberships' => function ($q) {
+                    $q->isActive()
+                      ->select(['id','group_id','person_id'])
+                      ->with([
+                          'group:id,group_type_id,name',
+                          'group.type:id,name',
+                          'roles:id,name',
+                      ]);
+                },
+                'country:id,name',
+                'credentials:id,name',
+                'expertises:id,name',
+                'institution:id,name',
+            ])
+            ->orderBy('id')
+            ->chunkById(1000, function ($people) use ($writeRow) {
+
+                foreach ($people as $person) {
+                    $activeGroups = $person->memberships->map(function ($m) {
+                        $label = trim(($m->group->name ?? '').' '.strtoupper(optional($m->group->type)->name));
+                        $roles = $m->roles->pluck('name')->filter()->implode(', ');
+                        return $roles ? "{$label} ({$roles})" : $label;
+                    })->filter()->implode('; ');
+
                     $data = [];
-                    $data['id'] = $person->id;
-                    $data['name'] = $person->first_name.' '.$person->last_name;
-                    $data['email'] = $person->email;
-                    $data['institution'] = $person->institution ? $person->institution->name : null;
-                    // $data['address'] = implode(', ', collect([$person->street1, $person->street2, $person->city, $person->state, $person->zip])->filter()->toArray());
-                    $data['state'] = $person->state ? $person->state : null;
-                    $data['country'] = $person->country ? $person->country->name : null;
-                    $data['timezone'] = $person->timezone;
-                    $data['phone'] = $person->phone;
-                    $data['credentials'] = $person->credentialsAsString;
-                    $data['expertises'] = preg_replace('/\n/', '; ', $person->expertisesAsString);
-                    $data['active_groups'] = $person->memberships->map(function ($i) {
-                                                return $i->group->displayName
-                                                    .(
-                                                        $i->rolesAsString
-                                                        ? ' ('.$i->rolesAsString.')'
-                                                        : ''
-                                                    );
-                                            })->filter()->join("; ");
-                    $data['biography'] = $person->biograghy;
-                    $data['orcid_id'] = $person->orcid_id;
-                    $data['hypothesis_id'] = $person->hypothesis_id;
+                    $data['id']             = $person->id;
+                    $data['name']           = trim($person->first_name.' '.$person->last_name);
+                    $data['email']          = $person->email;
+                    $data['institution']    = optional($person->institution)->name;
+                    $data['state']          = $person->state ?: null;
+                    $data['country']        = optional($person->country)->name;
+                    $data['timezone']       = $person->timezone;
+                    $data['phone']          = $person->phone;
+                    $data['credentials']    = $person->credentialsAsString ?? '';
+                    $data['expertises']     = preg_replace('/\r?\n/', '; ', $person->expertisesAsString ?? '');
+                    $data['active_groups']  = $activeGroups;
+                    $data['biography']      = $person->biography;
+                    $data['orcid_id']       = $person->orcid_id;
+                    $data['hypothesis_id']  = $person->hypothesis_id;
                     $data['has_registered'] = $person->user_id ? 'yes' : 'no';
-                    $data['date created'] = $person->created_at->format('Y-m-d');
-                    $data['last_updated'] = $person->updated_at->format('Y-m-d');
+                    $data['date created']   = optional($person->created_at)?->format('Y-m-d');
+                    $data['last_updated']   = optional($person->updated_at)?->format('Y-m-d');
 
-                    foreach ($data as $key => $val) {
-                        $data[$key] = preg_replace('/\n/', '; ', $val);
-                    }
+                    $writeRow($data);
+                }
 
-                    return $data;
-                });
+                $people->each->unsetRelations();
+                gc_collect_cycles();
+            });
 
-        return $people->toArray();
+        if ($fh) {
+            fclose($fh);
+            if (method_exists($this, 'info')) {
+                $this->info("Wrote {$csvPath}");
+            }
+            return $csvPath;
+        }
+        return $rows;
     }
-
 
     public function asCommand(Command $command)
     {
-        dd($this->handle());
+        $csv = $command->option('csv');
+        return $this->handle($csv);
     }
-
 }
