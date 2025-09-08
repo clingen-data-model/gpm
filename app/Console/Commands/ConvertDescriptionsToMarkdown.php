1e0244af612399f3d1c72d071c0e345efd46d7d8
Bradford Powell <bpow@drpowell.org>
2025-09-08 13:38:13 -0400

    convert descriptions back to markdown at the request of website team

--- app/Console/Commands/ConvertDescriptionsToMarkdown.php
+++ app/Console/Commands/ConvertDescriptionsToMarkdown.php
@@ -8,36 +8,37 @@ use App\Modules\Group\Events\GroupDescriptionUpdated;
 use App\Modules\Group\Models\Group;
 use Illuminate\Console\Command;
 use League\CommonMark\CommonMarkConverter;
+use League\HTMLToMarkdown\HtmlConverter;
 
-class ConvertDescriptionsToHtml extends Command
+class ConvertDescriptionsToMarkdown extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'fixup:convert-descriptions-to-html';
+    protected $signature = 'fixup:convert-descriptions-to-markdown';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Processes all website descriptions, scope descriptions, and membership descriptions, converting from markdown to HTML.';
+    protected $description = 'Processes all website descriptions, scope descriptions, and membership descriptions, converting from HTML to MarkDown.';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $converter = new CommonMarkConverter();
+        $converter = new HtmlConverter();
         ExpertPanel::all()->each(function ($ep) use ($converter) {
             $toUpdate = [];
             if ($ep->scope_description) {
-                $toUpdate['scope_description'] = $converter->convertToHtml($ep->scope_description);
+                $toUpdate['scope_description'] = $converter->convert($ep->scope_description);
             }
             if ($ep->membership_description) {
-                $toUpdate['membership_description'] = $converter->convertToHtml($ep->membership_description);
+                $toUpdate['membership_description'] = $converter->convert($ep->membership_description);
             }
             if (!empty($toUpdate)) {
                 $ep->update($toUpdate);
@@ -46,7 +47,7 @@ class ConvertDescriptionsToHtml extends Command
         });
 
         Group::whereNotNull('description')->each(function ($group) use ($converter) {
-            $newDescription = $converter->convertToHtml($group->description);
+            $newDescription = $converter->convert($group->description);
             $group->update($toUpdate = ['description' => $newDescription]);
             event(new GroupDescriptionUpdated($group, $newDescription, $group->description));
         });
