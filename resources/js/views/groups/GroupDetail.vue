f826b2547cc4aa1052fa3e51ad269aec7234e9c8
setiadha <setiadha@unc.edu>
2025-09-22 09:54:40 -0400

    Ini publication tab on group detail page

--- resources/js/views/groups/GroupDetail.vue
+++ resources/js/views/groups/GroupDetail.vue
@@ -32,6 +32,7 @@ import WGCaptionIconForm from '@/components/groups/WGCaptionIconForm.vue';
 import ClinvarForm from '@/components/expert_panels/ClinvarForm.vue';
 import GroupPublications from "./GroupPublications.vue";
 import FundingAwardsTab from '@/components/expert_panels/FundingAwardsTab.vue'
+import GroupPublications from "./GroupPublications.vue";
 
 import { api, isValidationError } from "../../http";
 
