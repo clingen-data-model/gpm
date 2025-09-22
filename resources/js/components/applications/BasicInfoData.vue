473028c7c8dea5893348ac6d190d0013ca2b0ec4
setiadha <setiadha@unc.edu>
2025-09-22 13:52:14 -0400

    Added vreate button on application form

--- resources/js/components/applications/BasicInfoData.vue
+++ resources/js/components/applications/BasicInfoData.vue
@@ -31,11 +31,9 @@ export default {
 			this.affilError = null
 
 			try {				
-				const { data } = await api.post(`/api/applications/${this.group.expert_panel.uuid}/affiliation`)
-				const affiliationID = data?.affiliation_id
-				if (! affiliationID) { throw new Error(data?.message || 'Request failed to create Affiliation ID') }
-				this.application.affiliation_id = affiliationID
-				this.$store.commit('pushSuccess', `Affiliation ID set to ${affiliationID}.`)
+        const epUuid = this.group?.expert_panel?.uuid
+        const id = await this.$store.dispatch('groups/createAffiliationId', { epUuid })
+        this.$store.commit('pushSuccess', `Affiliation ID set to ${id}.`)
 			} catch (e) {
 				const msg = e?.response?.data?.message || e?.message || 'Request failed';
 				this.affilError = msg
@@ -65,14 +63,8 @@ export default {
 
     <dictionary-row label-class="font-bold w-40" label="Affiliation ID">
       <span>{{ application.affiliation_id || '--' }}</span>
-      <button
-        v-if="canRequestAffil"
-        class="btn btn-xs ml-2"
-        :disabled="creatingAffil"
-        @click="createAffiliationId"
-      >
-        <span v-if="creatingAffil">Creating…</span>
-        <span v-else>Create</span>
+      <button v-if="canRequestAffil" class="btn btn-xs ml-2" :disabled="creatingAffil" @click="createAffiliationId">
+        <span v-if="creatingAffil">Creating…</span><span v-else>Create</span>
       </button>
       <note v-if="affilError" class="ml-2 text-red-600">{{ affilError }}</note>
     </dictionary-row>
