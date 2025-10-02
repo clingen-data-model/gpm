bdd02a42c2f739be437ff1fb30138a8ba3b03db3
setiadha <setiadha@unc.edu>
2025-10-02 02:56:53 -0400

    Application log, event: PublicationAdded, removed update.

---
+++ app/Modules/Group/Events/PublicationAdded.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Modules\Group\Events;
+
+use App\Modules\Group\Models\Group;
+use App\Modules\Group\Models\Publication;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
+
+class PublicationAdded extends GroupEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public Group $group, public Publication $publication) { }
+
+    public function getLogEntry(): string
+    {
+        $submitterName = Auth::user() ? Auth::user()->name : 'system';
+        return "Publication added by " . $submitterName . ".";
+    }
+
+    public function getProperties(): array
+    {
+        return [
+            'publication_id' => $this->publication->id,
+            'source' => $this->publication->source,
+            'identifier' => $this->publication->identifier,
+            'pub_type' => $this->publication->pub_type,
+            'published_at' => optional($this->publication->published_at)->toDateString(),            
+            // 'meta_keys' => array_slice(array_keys($this->publication->meta ?? []), 0, 20),
+            // 'has_meta' => ! empty($this->publication->meta),
+        ];
+    }
+}
