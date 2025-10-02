2b85754809d1b9ca20bf20e10ba714fe18275da9
setiadha <setiadha@unc.edu>
2025-10-02 02:56:53 -0400

    Application log, event: PublicationAdded, removed update.

--- app/Modules/Group/Models/Publication.php
+++ app/Modules/Group/Models/Publication.php
@@ -5,6 +5,7 @@ namespace App\Modules\Group\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Publication extends Model
 {
@@ -38,6 +39,30 @@ class Publication extends Model
 
     public function group()
     {
-        return $this->belongsTo(\App\Modules\Group\Models\Group::class);
+        return $this->belongsTo(Group::class);
     }
+
+    protected function displayTitle(): Attribute
+    {
+        return Attribute::get(function () {
+            $raw = $this->meta['title'] ?? null;
+
+            if (!$raw) { $raw = $this->meta['titleText'] ?? null; }
+            $raw = $raw ?: '(untitled)';
+            $clean = strip_tags((string) $raw);
+            return Str::limit($clean, 120);
+        });
+    }
+
+    protected function displayLink(): Attribute
+    {
+        return Attribute::get(function () {
+            return $this->link
+                ?? ($this->meta['url'] ?? null)
+                ?? (isset($this->meta['pmid'])  ? "https://pubmed.ncbi.nlm.nih.gov/{$this->meta['pmid']}/" : null)
+                ?? (isset($this->meta['pmcid']) ? "https://www.ncbi.nlm.nih.gov/pmc/articles/".strtoupper($this->meta['pmcid'])."/" : null)
+                ?? (isset($this->meta['doi'])   ? "https://doi.org/{$this->meta['doi']}" : null);
+        });
+    }
+
 }
