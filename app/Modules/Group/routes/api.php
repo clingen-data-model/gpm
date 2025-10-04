dc6ab5b905af6f84a80cedd99f2ed6e42a1d8bb2
setiadha <setiadha@unc.edu>
2025-10-03 23:13:54 -0400

    added publications to checkpoint. Fix GPM-470

--- app/Modules/Group/routes/api.php
+++ app/Modules/Group/routes/api.php
@@ -62,7 +62,6 @@ use App\Modules\Group\Http\Controllers\Api\GroupSubmissionsController;
 use App\Modules\Group\Actions\EmitGroupCheckpoints;
 use App\Modules\Group\Http\Controllers\Api\GroupPublicationsController;
 use App\Modules\Group\Actions\PublicationStore;
-use App\Modules\Group\Actions\PublicationUpdate;
 use App\Modules\Group\Actions\PublicationDelete;
 
 Route::group([
