d4d5a0454b8568f96f02e96446419312ef9e2221
setiadha <setiadha@unc.edu>
2025-10-16 06:01:47 -0400

    refine the fetching + DX + update doc. GPM-470

--- app/Modules/Group/routes/api.php
+++ app/Modules/Group/routes/api.php
@@ -63,7 +63,7 @@ use App\Modules\Group\Http\Controllers\Api\EvidenceSummaryController;
 use App\Modules\Group\Http\Controllers\Api\GroupSubmissionsController;
 use App\Modules\Group\Actions\EmitGroupCheckpoints;
 use App\Modules\Group\Http\Controllers\Api\GroupPublicationsController;
-use App\Modules\Group\Actions\PublicationStore;
+use App\Modules\Group\Actions\PublicationAdd;
 use App\Modules\Group\Actions\PublicationDelete;
 
 Route::group([
@@ -222,7 +222,7 @@ Route::group([
         // PUBLICATIONS
         Route::group(['prefix' => '/publications'], function () {
             Route::get('/', [GroupPublicationsController::class, 'index']);
-            Route::post('/', PublicationStore::class);
+            Route::post('/', PublicationAdd::class);
             Route::delete('/{publication}', PublicationDelete::class);
         });
 
