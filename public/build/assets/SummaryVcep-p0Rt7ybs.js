89c73304af21da902a93a2efa5f5a51fb1d5c00e
Bradford Powell <bpow@drpowell.org>
2025-10-17 15:49:09 -0400

    js build from prior commit

---
+++ public/build/assets/SummaryVcep-p0Rt7ybs.js
@@ -0,0 +1,2 @@
+import{A as o}from"./ApplicationsSummaryTable-DdnqiZ1T.js";import{_ as a,a as t,c as n,b as p,f as c}from"./app-BJASbhl0.js";import"./router_aware_sort_and_filter-B6Pdwc-L.js";const r={name:"GcepsSummary",components:{ApplicationsSummaryTable:o}};function s(m,i,l,_,u,f){const e=t("ApplicationsSummaryTable");return p(),n("div",null,[c(e,{"ep-type-id":2})])}const b=a(r,[["render",s]]);export{b as default};
+//# sourceMappingURL=SummaryVcep-p0Rt7ybs.js.map
