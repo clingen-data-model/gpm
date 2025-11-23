cf9a98b06558241827321416b2f129eca17fb6b1
setiadha <setiadha@unc.edu>
2025-11-22 20:48:37 -0500

    Missed one file. Fixed CGSP-800

--- app/Modules/Group/Actions/GroupCreate.php
+++ app/Modules/Group/Actions/GroupCreate.php
@@ -24,11 +24,14 @@ class GroupCreate
 
     public function handle($data): Group
     {
+        $visibilityId = $data['group_visibility_id'] ?? config('groups.visibility.public.id') ?? 1;
+
         $group = Group::create([
             'uuid' => isset($data['uuid']) ? $data['uuid'] : Uuid::uuid4(),
             'name' => $data['name'],
             'group_type_id' => $data['group_type_id'],
             'group_status_id' => $data['group_status_id'],
+            'group_visibility_id' => $visibilityId,
             'coi_code' => $this->makeCoiCode->handle(),
             'parent_id' => $this->resolveParentId($data),
         ]);
@@ -54,7 +57,7 @@ class GroupCreate
 
     public function asController(ActionRequest $request)
     {
-        $data = $request->only('name', 'group_type_id', 'group_status_id', 'parent_id', 'long_base_name', 'short_base_name');
+        $data = $request->only('name', 'group_type_id', 'group_status_id', 'group_visibility_id', 'parent_id', 'long_base_name', 'short_base_name');
 
         $group = $this->handle($data);
         $group->load('expertPanel');
@@ -70,6 +73,7 @@ class GroupCreate
             'short_base_name' => 'max:16',
             'group_type_id' => 'required', // TODO: should check for existence when we merge vcep/gcep into group
             'group_status_id' => 'required|exists:group_statuses,id',
+            'group_visibility_id' => 'nullable|exists:group_visibilities,id',
             'parent_id' => [
                 'nullable',
                 function ($attribute, $value, $fail) {
