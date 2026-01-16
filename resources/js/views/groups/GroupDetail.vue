3d411bc6c7471c69dce39e866f59b01350e6c183
setiadha-unc <setiadha@unc.edu>
2026-01-15 20:13:05 -0500

    Funding Awards

--- resources/js/views/groups/GroupDetail.vue
+++ resources/js/views/groups/GroupDetail.vue
@@ -32,6 +32,7 @@ import WGCaptionIconForm from '@/components/groups/WGCaptionIconForm.vue';
 import ClinvarForm from '@/components/expert_panels/ClinvarForm.vue';
 import FundingAwardsTab from '@/components/expert_panels/FundingAwardsTab.vue'
 import GroupPublications from "./GroupPublications.vue";
+import FundingAwardsTab from '@/components/expert_panels/FundingAwardsTab.vue'
 
 import { api, isValidationError } from "../../http";
 
@@ -67,6 +68,7 @@ export default {
     GroupPublications,
     FundingAwardsTab,
     GroupPublications,
+    FundingAwardsTab,
   },
   props: {
     uuid: {
