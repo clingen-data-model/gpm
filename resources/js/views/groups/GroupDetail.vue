e0fb4f5dd9f5694d8ed3cfe927070a2e80f8b065
Bradford Powell <bpow@drpowell.org>
2026-01-16 11:10:50 -0500

    WIP consolidating to europepmc

--- resources/js/views/groups/GroupDetail.vue
+++ resources/js/views/groups/GroupDetail.vue
@@ -31,6 +31,7 @@ import SubgroupList from '@/components/groups/SubgroupList.vue'
 import WGCaptionIconForm from '@/components/groups/WGCaptionIconForm.vue';
 import ClinvarForm from '@/components/expert_panels/ClinvarForm.vue';
 import GroupPublications from "./GroupPublications.vue";
+import PublicationsForm from "@/components/forms/PublicationForm.vue";
 
 import { api, isValidationError } from "../../http";
 
@@ -64,6 +65,7 @@ export default {
     WGCaptionIconForm,
     ClinvarForm,
     GroupPublications,
+    PublicationsForm,
   },
   props: {
     uuid: {
@@ -478,6 +480,7 @@ export default {
           <tab-item v-if="userInGroup(group) || hasPermission('groups-manage')" label="Publications">
             <GroupPublications :key="group.uuid" :group="group" />
             <note>Publications are visible to members of this group (and admins).</note>
+            <PublicationsForm />
           </tab-item>
 
           <tab-item label="Attestations" :visible="group.is_ep">
