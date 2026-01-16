546a6c88632288819cac69f8156bdba266f65fcb
Bradford Powell <bpow@drpowell.org>
2026-01-16 11:10:50 -0500

    WIP consolidating to europepmc

--- resources/js/views/groups/GroupDetail.vue
+++ resources/js/views/groups/GroupDetail.vue
@@ -33,6 +33,7 @@ import WGCaptionIconForm from '@/components/groups/WGCaptionIconForm.vue';
 import ClinvarForm from '@/components/expert_panels/ClinvarForm.vue';
 import FundingAwardsTab from '@/components/expert_panels/FundingAwardsTab.vue'
 import GroupPublications from "./GroupPublications.vue";
+import PublicationsForm from "@/components/forms/PublicationForm.vue";
 
 import { api, isValidationError } from "../../http";
 
@@ -68,6 +69,7 @@ export default {
     ClinvarForm,
     FundingAwardsTab,
     GroupPublications,
+    PublicationsForm,
   },
   props: {
     uuid: {
@@ -510,6 +512,7 @@ export default {
           <tab-item v-if="userInGroup(group) || hasPermission('groups-manage')" label="Publications">
             <GroupPublications :key="group.uuid" :group="group" />
             <note>Publications are visible to members of this group (and admins).</note>
+            <PublicationsForm />
           </tab-item>
 
           <tab-item label="Attestations" :visible="group.is_ep">
