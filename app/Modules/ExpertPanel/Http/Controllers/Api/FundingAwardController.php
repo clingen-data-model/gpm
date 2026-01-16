a7596400159aad45a6153f9a41e208d1fcfa1617
setiadha-unc <setiadha@unc.edu>
2026-01-15 22:37:48 -0500

    Added Contact PIs. Fix CGS{-771

--- app/Modules/ExpertPanel/Http/Controllers/Api/FundingAwardController.php
+++ app/Modules/ExpertPanel/Http/Controllers/Api/FundingAwardController.php
@@ -12,10 +12,28 @@ class FundingAwardController extends Controller
     {
         return FundingAward::query()
             ->where('expert_panel_id', $expertPanel->id)
-            ->with(['fundingSource.fundingType'])
+            ->with(['fundingSource.fundingType', 'contactPis'])
             ->orderByRaw('start_date IS NULL') // nulls last
             ->orderByDesc('start_date')
             ->orderByDesc('id')
             ->get();
     }
+    
+    public function piOptions(ExpertPanel $expertPanel)
+    {
+        $members = $expertPanel->group->activeMemberships()->with('person:id,uuid,first_name,last_name,email,phone')->get();
+        $people = $members->pluck('person')->filter()
+                    ->unique('id')
+                    ->sortBy(fn ($p) => strtolower(($p->last_name ?? '').' '.($p->first_name ?? '')))
+                    ->values()->map(fn ($p) => [
+                            'id'    => $p->id,
+                            'uuid'  => $p->uuid,
+                            'name'  => trim(($p->first_name ?? '').' '.($p->last_name ?? '')),
+                            'email' => $p->email,
+                            'phone' => $p->phone,
+                        ]);
+
+        return $people;
+    }
+
 }
