8fa4126295d41186540a8e4ba8cd7fc43683e04d
Bradford Powell <bpow@drpowell.org>
2026-01-16 11:10:50 -0500

    WIP consolidating to europepmc

--- resources/js/components/forms/PublicationForm.vue
+++ resources/js/components/forms/PublicationForm.vue
@@ -1,29 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { watchDebounced } from '@vueuse/core';
 import usePublicationLookup from "../../composables/usePublicationLookup";
 
-const publicationClient = usePublicationLookup();
+const query = ref('');
+const { citation, execute, pending, error } = usePublicationLookup(query);
 
-const pmid = ref('');
-const publicationData = ref(null as any);
-
-const fetchPublicationData = async () => {
-    if (pmid.value) {
-        publicationData.value = await publicationClient.fetchFromUrl(pmid.value);
+watchDebounced(query, (newVal) => {
+    console.log('query changed to', newVal);
+    if (newVal && newVal.length > 4) {
+        execute();
     }
-};
-
+}, { debounce: 2000 });
 
 </script>
 
 <template>
   <div>
-    <input v-model="pmid" placeholder="Enter PubMed ID" />
-    <button @click="fetchPublicationData">Fetch Publication</button>
+    <input v-model="query" placeholder="Enter PubMed ID" />
 
-    <div v-if="publicationData">
+    <div v-if="citation">
       <h2>Publication Details</h2>
-      <pre>{{ publicationData }}</pre>
+      <pre>{{ citation }}</pre>
     </div>
+    <div v-if="pending">Loading...</div>
+    <div v-if="error">Error: {{ error.message }}</div>
+    <pre>{{ pending }}</pre>
+    <pre>{{ error }}</pre>
   </div>
 </template>
