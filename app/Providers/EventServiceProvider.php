fadff86fbcd7fecf10c90775b65237b42eaa2e22
setiadha-unc <setiadha@unc.edu>
2026-01-22 15:12:45 -0500

    Added event listener for snapshot, remove snapshot when gene removed, rename table genes to scope_genes. GPM-GT-Epam-Integration

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -46,7 +46,6 @@ class EventServiceProvider extends ServiceProvider
         JudgementDeleted::class => [JudgementNotifyAboutEvent::class],
 
         ApplicationStepSubmitted::class => [ CaptureScopeGeneSnapshots::class ],
-        StepApproved::class => [SendGenesToGtOnGcepApproval::class],
     ];
 
     /**
