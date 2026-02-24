bfedf58a660093a387e69af06b459d62dbca526a
setiadha-unc <setiadha@unc.edu>
2026-02-24 12:53:15 -0500

    Update the credentials schema as in checkpoint. CGSP-962

--- app/Modules/Person/Events/Traits/PublishesEvent.php
+++ app/Modules/Person/Events/Traits/PublishesEvent.php
@@ -39,7 +39,9 @@ trait PublishesEvent
                 'last_name' => $this->person->last_name,
                 'email' => $this->person->email,
                 'institution' => $this->getInstitutionMessage(),
-                'credentials' => $this->person->credentials,
+                'credentials' => $this->person->credentials->map(function ($credential) {
+                                return $credential->name;
+                            })->toArray(),
                 'biography' => $this->person->biography,
                 'profile_photo' => $this->person->ProfilePhotoUrl,
                 'orcid_id' => $this->person->orcid_id,
