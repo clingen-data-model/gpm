aeb67ab24d4c63a60201f1baf4b77fddf3a3e5cc
Bradford Powell <bpow@drpowell.org>
2026-02-28 16:58:34 -0500

    avoid referring to CocService in so many places

--- app/Modules/Group/Events/GroupMemberEvent.php
+++ app/Modules/Group/Events/GroupMemberEvent.php
@@ -4,7 +4,6 @@ namespace App\Modules\Group\Events;
 
 use App\Modules\Group\Events\Traits\IsPublishableGroupEvent;
 use App\Modules\Group\Models\GroupMember;
-use App\Services\CocService;
 
 abstract class GroupMemberEvent extends GroupEvent
 {
@@ -20,9 +19,8 @@ abstract class GroupMemberEvent extends GroupEvent
         $this->groupMember->loadMissing([
             'person.latestCocAttestation',
         ]);
-        $cocService = app(CocService::class);
-        
-        return ['members' => [$this->mapMemberForMessage($this->groupMember, false, $cocService)]];
+
+        return ['members' => [$this->mapMemberForMessage($this->groupMember, false)]];
     }
 
 }
