513a28589d8a1f7adec54c80258905055c89b33b
setiadha-unc <setiadha@unc.edu>
2026-03-17 10:52:16 -0400

    Removed the Publication Form. GPM-470

--- resources/js/views/groups/GroupDetail.vue
+++ resources/js/views/groups/GroupDetail.vue
@@ -33,7 +33,6 @@ import WGCaptionIconForm from '@/components/groups/WGCaptionIconForm.vue';
 import ClinvarForm from '@/components/expert_panels/ClinvarForm.vue';
 import FundingAwardsTab from '@/components/expert_panels/FundingAwardsTab.vue'
 import GroupPublications from "./GroupPublications.vue";
-import PublicationsForm from "@/components/forms/PublicationForm.vue";
 
 import { api, isValidationError } from "../../http";
 
@@ -69,7 +68,6 @@ export default {
     ClinvarForm,
     FundingAwardsTab,
     GroupPublications,
-    PublicationsForm,
   },
   props: {
     uuid: {
@@ -512,7 +510,6 @@ export default {
           <tab-item v-if="userInGroup(group) || hasPermission('groups-manage')" label="Publications">
             <GroupPublications :key="group.uuid" :group="group" />
             <note>Publications are visible to members of this group (and admins).</note>
-            <PublicationsForm />
           </tab-item>
 
           <tab-item label="Attestations" :visible="group.is_ep">
