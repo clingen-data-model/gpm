a1a019abf7c79664c4bc75cb518084aa78ed1f87
setiadha-unc <setiadha@unc.edu>
2026-05-30 20:34:04 -0400

    js build from previous commits (GPM-GT Integration)

---
+++ public/build/assets/SummaryGcep-Cgpo_rlc.js
@@ -0,0 +1,2 @@
+import{A as a}from"./ApplicationsSummaryTable-B5nNZ99c.js";import{_ as o,a as t,c as n,b as p,f as r}from"./app-BZVXCG-4.js";import"./router_aware_sort_and_filter-BMzUIc_0.js";const c={name:"GcepsSummary",components:{ApplicationsSummaryTable:a}};function s(m,i,l,_,u,f){const e=t("ApplicationsSummaryTable");return p(),n("div",null,[r(e,{"expert-panel-type-id":1})])}const b=o(c,[["render",s]]);export{b as default};
+//# sourceMappingURL=SummaryGcep-Cgpo_rlc.js.map
