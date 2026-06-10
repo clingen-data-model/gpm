6d57b5ab73b94f77c636e15eedaf8b926a296247
setiadha-unc <setiadha@unc.edu>
2026-06-10 17:40:44 -0400

    js builds

---
+++ public/build/assets/GcepsList-Dn68hHNX.js
@@ -0,0 +1,2 @@
+import{A as o}from"./ApplicationsTable-Dya830Lt.js";import{_ as t,c as p,f as s,a,b as n}from"./app-DNiFiOTe.js";import"./router_aware_sort_and_filter-Fpd5VJRs.js";const c={components:{ApplicationsTable:o},props:{},data(){return{}}};function r(i,l,_,m,d,f){const e=a("ApplicationsTable");return n(),p("div",null,[s(e,{"ep-type-id":1})])}const $=t(c,[["render",r]]);export{$ as default};
+//# sourceMappingURL=GcepsList-Dn68hHNX.js.map
