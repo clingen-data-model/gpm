dbf6c3e598892283fec9adb23625a55f34450eb0
setiadha-unc <setiadha@unc.edu>
2026-06-22 09:55:23 -0400

    Fixed redirect CAM bug. GPM Small Enhancements

--- resources/js/views/PendingCoreMemberAttestation.vue
+++ resources/js/views/PendingCoreMemberAttestation.vue
@@ -5,8 +5,13 @@ import CoreMemberAttestationForm from '@/components/people/CoreMemberAttestation
 const store = useStore()
 const personUUID = store.getters.currentUser?.person?.uuid
 
-function backOrHome() {
-    history.length > 1 ? window.history.back() : location.assign('/')
+async function continueAfterAttestation() {
+    const redirectTo = route.query.redirect
+    if (typeof redirectTo === 'string' && redirectTo.startsWith('/') && !redirectTo.startsWith('/onboarding/') && redirectTo !== '/core-approval-member-attestation') {
+        await router.replace(redirectTo)
+        return
+    }
+    await router.replace({ name: 'Dashboard' })
 }
 </script>
 
@@ -14,6 +19,6 @@ function backOrHome() {
     <card title="Core Approval Member Attestation">
         <p class="mb-4">You've been designated as a Core Approval member. Please complete this one-time attestation to continue.</p>
 
-        <CoreMemberAttestationForm v-if="personUUID" :person-uuid="personUUID" :after-submit="backOrHome" />
+        <CoreMemberAttestationForm v-if="personUUID" :person-uuid="personUUID" :after-submit="continueAfterAttestation" />
     </card>
 </template>
