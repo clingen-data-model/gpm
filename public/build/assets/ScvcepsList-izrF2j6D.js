00f54e47a03bd5d6614f761da911c3b19374a493
setiadha-unc <setiadha@unc.edu>
2026-07-14 17:08:19 -0400

    js builds from previous commmits (GPM-567)

--- public/build/assets/ScvcepsList-izrF2j6D.js
+++
@@ -1,2 +0,0 @@
-import{A as o}from"./ApplicationsTable-Bu71UWgS.js";import{_ as t,c as s,f as c,a as n,b as p}from"./app-BpNrf6eg.js";import"./router_aware_sort_and_filter-BjnPu6AE.js";const a={components:{ApplicationsTable:o}};function r(i,l,_,m,f,d){const e=n("ApplicationsTable");return p(),s("div",null,[c(e,{"ep-type-id":3})])}const $=t(a,[["render",r]]);export{$ as default};
-//# sourceMappingURL=ScvcepsList-izrF2j6D.js.map
---
+++ public/build/assets/SummaryScvcep-knpaFKXG.js
@@ -0,0 +1,2 @@
+import{A as o}from"./ApplicationsSummaryTable-ZSnfp_Ed.js";import{_ as a,a as t,c,b as n,f as p}from"./app-BAHG7jTh.js";import"./router_aware_sort_and_filter-5vgX7NKg.js";const r={name:"ScvcepsSummary",components:{ApplicationsSummaryTable:o}};function s(m,i,l,_,u,f){const e=t("ApplicationsSummaryTable");return n(),c("div",null,[p(e,{"ep-type-id":3})])}const b=a(r,[["render",s]]);export{b as default};
+//# sourceMappingURL=SummaryScvcep-knpaFKXG.js.map
